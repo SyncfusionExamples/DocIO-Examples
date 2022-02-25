eed8bbd277826369d483ef48c74146e8115efe43
LokeshBaskar <lokesh.baskar@syncfusion.com>
2022-02-25 11:41:23 +0530

    WF-62631-Add GitHub samples for split by bookmark, split by placeholder text and retrieve commented word

---
+++ Comments/Retrieve-commented-word/.NET-Standard/Retrieve-commented-word/Program.cs
@@ -0,0 +1,35 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System;
+using System.IO;
+
+namespace Retrieve_commented_word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Load the existing Word document.
+            FileStream fileStreamPath = new FileStream(Path.GetFullPath(@"../../../Template.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            using (WordDocument document = new WordDocument(fileStreamPath, FormatType.Docx))
+            {
+                //Iterate the comments in the Word document.
+                foreach (WComment comment in document.Comments)
+                {
+                    //Get the commented word or part of a particular comment.
+                    if (comment.TextBody.LastParagraph.Text == "This is the second comment.")
+                    {
+                        ParagraphItemCollection paragraphItem = comment.CommentedItems;
+                    }
+
+                }
+                //Save the Word document to file stream.
+                using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Result.docx"), FileMode.Create, FileAccess.ReadWrite))
+                {
+                    document.Save(outputFileStream, FormatType.Docx);
+                }
+            }
+            
+        }
+    }
+}
---
+++ Word-document/Split-by-placeholder-text/.NET-Standard/Split-by-placeholder-text/Program.cs
@@ -0,0 +1,47 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System;
+using System.IO;
+
+namespace Split_a_document_by_placeholder_text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Load an existing Word document into DocIO instance.
+            FileStream fileStreamPath = new FileStream(Path.GetFullPath(@"../../../Template.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            using (WordDocument document = new WordDocument(fileStreamPath, FormatType.Docx))
+            {
+                String[] findPlaceHolderWord = new string[] { "[First Content Start]", "[Second Content Start]", "[Third Content Start]" };
+                for (int i = 0; i < findPlaceHolderWord.Length; i++)
+                {
+                    //Get the start placeholder paragraph in the document.
+                    WParagraph startParagraph = document.Find(findPlaceHolderWord[i], true, true).GetAsOneRange().OwnerParagraph;
+                    //Get the end placeholder paragraph in the document.
+                    WParagraph endParagraph = document.Find(findPlaceHolderWord[i].Replace("Start", "End"), true, true).GetAsOneRange().OwnerParagraph;
+                    //Get the text body.
+                    WTextBody textBody = startParagraph.OwnerTextBody;
+                    //Get the start PlaceHolder index.
+                    int startPlaceHolderIndex = textBody.ChildEntities.IndexOf(startParagraph);
+                    //Get the end PlaceHolder index.
+                    int endPlaceHolderIndex = textBody.ChildEntities.IndexOf(endParagraph);
+
+                    //Create a new Word document.
+                    WordDocument newDocument = new WordDocument();
+                    newDocument.AddSection();
+                    //Add the retrieved content into another new document.
+                    for (int j = startPlaceHolderIndex + 1; j < endPlaceHolderIndex; j++)
+                        newDocument.LastSection.Body.ChildEntities.Add(textBody.ChildEntities[j].Clone());
+                    //Save the Word document to file stream.
+                    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Result" + i + ".docx"), FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        
+                        newDocument.Save(outputFileStream, FormatType.Docx);
+                    }
+                }
+            }
+
+        }
+    }
+}
