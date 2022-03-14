f5dbe5ad8d7fb41986009524a373af282a2c2ce1
Suriya Balamurugan <suriya.balamurugan@syncfusion.com>
2022-03-14 16:55:42 +0530

    Removed old MAUI sample

--- Getting-Started/.NET-MAUI/Create-Word-document/ISave.cs
+++
@@ -1,7 +0,0 @@
-using System.IO;
-using System.Threading.Tasks;
-public interface ISave
-{
-    //Method to save document as a file and view the saved document
-	Task SaveAndView(string filename, string contentType, MemoryStream stream);
-}
