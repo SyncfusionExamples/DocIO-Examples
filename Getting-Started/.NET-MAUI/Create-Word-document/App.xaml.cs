ebf7c3d0a8e949064638f003da7ad3ca11e027da
Suriya Balamurugan <suriya.balamurugan@syncfusion.com>
2022-03-14 16:57:53 +0530

    Updated .NET MAUI Sample

---
+++ Getting-Started/.NET-MAUI/Create-Word-document/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Create_Word_document;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Getting-Started/.NET-MAUI/Create-Word-document/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Create_Word_document;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
