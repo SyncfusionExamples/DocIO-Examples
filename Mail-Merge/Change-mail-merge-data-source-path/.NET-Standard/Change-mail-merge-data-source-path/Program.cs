fb7d84e1b1aad50f0d52a86007a975ed2a3cfc89
Suriya Balamurugan <suriya.balamurugan@syncfusion.com>
2022-03-15 19:27:08 +0530

    Added samples of Mail-Merge

---
+++ Mail-Merge/Change-mail-merge-data-source-path/.NET-Standard/Change-mail-merge-data-source-path/Program.cs
@@ -0,0 +1,28 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.IO;
+
+namespace Change_mail_merge_data_source_path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fileStream = new FileStream(Path.GetFullPath(@"../../../Data/Template.docx"), FileMode.Open, FileAccess.ReadWrite))
+            {
+                //Opens the template document.
+                using (WordDocument document = new WordDocument(fileStream, FormatType.Docx))
+                {
+                    //Change the data source file path.
+                    document.MailMerge.Settings.DataSource = Path.GetFullPath(@"../../../Data/DataSource.mdb");
+                    //Creates file stream.
+                    using (FileStream outputStream = new FileStream(Path.GetFullPath(@"../../../Result.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        //Saves the Word document to file stream.
+                        document.Save(outputStream, FormatType.Docx);
+                    }
+                }
+            }
+        }
+    }
+}
