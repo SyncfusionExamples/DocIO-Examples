5946636abc8009fa509cdc2363a0dd00fa648d66
Anto Nihil Sahaya Raj <antonihil.sahayaraj@syncfusion.com>
2022-08-24 09:28:12 +0530

    Added sample of Replace-text-in-Word-with-HTML

---
+++ Find-and-Replace/Replace-text-in-Word-with-HTML/.NET-Framework/Replace-text-in-Word-with-HTML/Program.cs
@@ -0,0 +1,38 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Replace_text_in_Word_with_HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Loads an existing Word document.
+            using (WordDocument document = new WordDocument(Path.GetFullPath(@"../../Data/Sample.docx"), FormatType.Docx))
+            {
+                //Create the temporary word document for html.
+                using (WordDocument replaceDoc = new WordDocument())
+                {
+                    //Add section for html document. 
+                    IWSection htmlsection = replaceDoc.AddSection();
+                    //Reads HTML string from the file.
+                    string htmlString = File.ReadAllText(Path.GetFullPath(@"../../Data/File.html"));
+                    //Validates the Html string.
+                    bool isValidHtml = htmlsection.Body.IsValidXHTML(htmlString, XHTMLValidationType.None);
+                    //When the Html string passes validation, it is inserted to the document.
+                    if (isValidHtml)
+                    {
+                        //Appends Html string in the temporary word document.
+                        htmlsection.Body.InsertXHTML(htmlString);
+                    }
+                    //Replaces the content placeholder text with desired Word document.
+                    document.Replace(new Regex("«([a-zA-Z0-9 ]*:*[a-zA-Z0-9 ]+)»"), replaceDoc, true);
+                }
+                //Saves the Word document to file stream.
+                document.Save(Path.GetFullPath(@"../../Result.docx"), FormatType.Docx);
+            }
+        }
+    }
+}
