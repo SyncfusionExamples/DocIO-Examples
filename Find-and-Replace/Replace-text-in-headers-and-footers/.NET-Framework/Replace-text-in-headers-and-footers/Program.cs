6e5872fc7507a5765d1b94b0aa96a9408260e595
elizabethatienoo <elizabethatienoo@syncfusion.com>
2022-09-02 09:57:33 +0300

    Cleaned-code

--- Find-and-Replace/Replace-text-in-headers-and-footers/.NET-Framework/Replace-text-in-headers-and-footers/Program.cs
+++ Find-and-Replace/Replace-text-in-headers-and-footers/.NET-Framework/Replace-text-in-headers-and-footers/Program.cs
@@ -10,44 +10,45 @@ namespace Replace_text_in_headers_and_footers
     {
         static void Main(string[] args)
         {
-            
-                //Loads the template document
-                using (WordDocument document = new WordDocument(Path.GetFullPath(@"C:\Users\ElizabethAtienoOdhia\Downloads\Input (2).docx"), FormatType.Docx))
-                {
-                    //Replaces the header placeholder text with desired image     
-                    WParagraph headerParagraph = new WParagraph(document);
-                    //Aligns the paragraph horizontally to the right
-                    headerParagraph.ParagraphFormat.HorizontalAlignment = HorizontalAlignment.Right;
-                    WPicture picture = headerParagraph.AppendPicture(Image.FromFile(@"C:\Users\ElizabethAtienoOdhia\Downloads\AdventureCycle.jpg")) as WPicture;
-                    //Sets width and height for the paragraph
-                    picture.Height = 65f;
-                    picture.Width = 200f;
-                    //Represent the part of the textbody item in Header
-                    TextBodyPart headerBodyPart = new TextBodyPart(document);
-                    headerBodyPart.BodyItems.Add(headerParagraph);
-                    //Replaces all entries of a given regular expression with the text body part along with its formatting in header
-                    document.Replace(new Regex("^<<(.*)>>"), headerBodyPart, false);
 
+            //Load the template document.
+            using (WordDocument document = new WordDocument(Path.GetFullPath(@"../../Template.docx"), FormatType.Docx))
+            {
+                //Create paragraph for header.  
+                WParagraph headerParagraph = new WParagraph(document);
+                //Align paragraph horizontally to the right.
+                headerParagraph.ParagraphFormat.HorizontalAlignment = HorizontalAlignment.Right;
+                //Append picture in the paragraph.
+                WPicture picture = headerParagraph.AppendPicture(Image.FromFile(@"../../AdventureCycle.jpg")) as WPicture;
+                //Set width and height for the picture.
+                picture.Height = 65f;
+                picture.Width = 200f;
+                //Create text body part.
+                TextBodyPart headerBodyPart = new TextBodyPart(document);
+                headerBodyPart.BodyItems.Add(headerParagraph);
+                //Replace all entries of a given regular expression with the text body part along with its formatting in header.
+                document.Replace(new Regex("^<<(.*)>>"), headerBodyPart, false);
 
-                    WParagraph footerParagraph = new WParagraph(document);
-                    //Aligns the paragraph horizontally to the right
-                    footerParagraph.ParagraphFormat.HorizontalAlignment = HorizontalAlignment.Right;
-                    //Adds the text
-                    footerParagraph.AppendText(" Page ");
-                    //Adds page number field to the document
-                    footerParagraph.AppendField(" CurrentPageNumber", FieldType.FieldPage);
-                    // Adds the text
-                    footerParagraph.AppendText(" of ");
-                    //Adds number of page field to the document
-                    footerParagraph.AppendField(" TotalNumberOfPages ", FieldType.FieldNumPages);
-                    //Represent the part of the textbody item in Footer
-                    TextBodyPart footerBodyPart = new TextBodyPart(document);
-                    footerBodyPart.BodyItems.Add(footerParagraph);
-                    //replaces all entries of a given regular expression with the text body part along with its formatting in footer
-                    document.Replace(new Regex("^//(.*)"), footerBodyPart, false);
-                    //Saves and closes the document.
-                    document.Save(Path.GetFullPath("Sample.docx"), FormatType.Docx);              
-                }
+                //Create paragraph for footer.
+                WParagraph footerParagraph = new WParagraph(document);
+                //Align the paragraph horizontally to the right.
+                footerParagraph.ParagraphFormat.HorizontalAlignment = HorizontalAlignment.Right;
+                //Add the text.
+                footerParagraph.AppendText(" Page ");
+                //Add page number field.
+                footerParagraph.AppendField(" CurrentPageNumber", FieldType.FieldPage);
+                //Add the text.
+                footerParagraph.AppendText(" of ");
+                //Add number of page field.
+                footerParagraph.AppendField(" TotalNumberOfPages ", FieldType.FieldNumPages);
+                //Create text body part.
+                TextBodyPart footerBodyPart = new TextBodyPart(document);
+                footerBodyPart.BodyItems.Add(footerParagraph);
+                //Replace all entries of a given regular expression with the text body part along with its formatting in footer.
+                document.Replace(new Regex("^//(.*)"), footerBodyPart, false);
+                //Save the document.
+                document.Save(Path.GetFullPath("../../Sample.docx"), FormatType.Docx);
+            }
         }
     }
 }
--- Find-and-Replace/Replace-text-in-headers-and-footers/.NET-Standard/Replace-text-in-headers-and-footers/Program.cs
+++ Find-and-Replace/Replace-text-in-headers-and-footers/.NET-Standard/Replace-text-in-headers-and-footers/Program.cs
@@ -9,45 +9,47 @@ namespace Replace_text_in_headers_and_footers
     {
         static void Main(string[] args)
         {
-            using (FileStream fileStreamPath = new FileStream(Path.GetFullPath(@"C:\Users\ElizabethAtienoOdhia\Downloads\Input (2).docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (FileStream fileStreamPath = new FileStream(Path.GetFullPath(@"../../../Template.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             {
                 //Loads the template document
                 using (WordDocument document = new WordDocument(fileStreamPath, FormatType.Docx))
                 {
-                    //Replaces the header placeholder text with desired image     
+                    //Create paragraph for header.     
                     WParagraph headerParagraph = new WParagraph(document);
-                    //Aligns the paragraph horizontally to the right
+                    //Align paragraph horizontally to the right.
                     headerParagraph.ParagraphFormat.HorizontalAlignment = HorizontalAlignment.Right;                   
-                    FileStream imageStream = new FileStream(Path.GetFullPath(@"C:\Users\ElizabethAtienoOdhia\Downloads\AdventureCycle.jpg"), FileMode.Open, FileAccess.ReadWrite);
+                    FileStream imageStream = new FileStream(Path.GetFullPath(@"../../../AdventureCycle.jpg"), FileMode.Open, FileAccess.ReadWrite);
+                    //Append picture in the paragraph.
                     WPicture picture = headerParagraph.AppendPicture(imageStream) as WPicture;
-                    //Sets width and height for the paragraph
+                    //Set width and height for the picture.
                     picture.Height = 65f;
                     picture.Width = 200f;
-                    //Represent the part of the textbody item in Header
+                    //Create text body part.
                     TextBodyPart headerBodyPart = new TextBodyPart(document);
                     headerBodyPart.BodyItems.Add(headerParagraph);
-                    //Replaces all entries of a given regular expression with the text body part along with its formatting in header
+                    //Replace all entries of a given regular expression with the text body part along with its formatting in header.
                     document.Replace(new Regex("^<<(.*)>>"), headerBodyPart, false);
 
+                    //Create paragraph for footer.
                     WParagraph footerParagraph = new WParagraph(document);
-                    //Aligns the paragraph horizontally to the right
+                    //Align the paragraph horizontally to the right.
                     footerParagraph.ParagraphFormat.HorizontalAlignment = HorizontalAlignment.Right;
-                    //Adds the text
+                    //Add the text.
                     footerParagraph.AppendText(" Page ");
-                    //Adds page number field to the document
+                    //Add page number field.
                     footerParagraph.AppendField(" CurrentPageNumber", FieldType.FieldPage);
-                    // Adds the text
+                    //Add the text.
                     footerParagraph.AppendText(" of ");
-                    //Adds number of page field to the document
+                    //Add number of page field.
                     footerParagraph.AppendField(" TotalNumberOfPages ", FieldType.FieldNumPages);
-                    //Represent the part of the textbody item in Footer
+                    //Create text body part.
                     TextBodyPart footerBodyPart = new TextBodyPart(document);
                     footerBodyPart.BodyItems.Add(footerParagraph);
-                    //replaces all entries of a given regular expression with the text body part along with its formatting in footer
+                    //Replace all entries of a given regular expression with the text body part along with its formatting in footer.
                     document.Replace(new Regex("^//(.*)"), footerBodyPart, false);
-                    using (FileStream outputFileStream = new FileStream(Path.GetFullPath("Sample.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    using (FileStream outputFileStream = new FileStream(Path.GetFullPath("../../../Sample.docx"), FileMode.Create, FileAccess.ReadWrite))
                     {
-                        //Saves the Word document to file stream.
+                        //Save the document.
                         document.Save(outputFileStream, FormatType.Docx);
                     }
                 }
