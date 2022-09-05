440035bd55cdf12f141b19c4dc2b26ce65cd663c
Anto Nihil Sahaya Raj <antonihil.sahayaraj@syncfusion.com>
2022-09-05 09:07:28 +0530

    Added Sample of Insert-Row-with-same-formatting

---
+++ Tables/Insert-Row-with-same-number-of-cells/.NET-Standard/Insert-Row-with-same-formatting/Program.cs
@@ -0,0 +1,43 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.IO;
+
+namespace Insert_Row_with_same_formatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fileStream = new FileStream(Path.GetFullPath(@"../../../Data/Input.docx"), FileMode.Open, FileAccess.ReadWrite))
+            {
+                //Open an existing Word document.
+                using (WordDocument document = new WordDocument(fileStream, FormatType.Automatic))
+                {
+                    //Get the table from the Word document.
+                    WTable table = document.Sections[0].Tables[0] as WTable;
+                    //Clone the row.
+                    WTableRow row = table.Rows[0].Clone();
+
+                    //Iterate all cells in row and clear the contents.
+                    for (int i = 0; i < row.Cells.Count; i++)
+                    {
+                        WTableCell tableCell = row.Cells[i];
+                        tableCell.ChildEntities.Clear();
+                    }
+                    //Insert new paragraph to the first cell.
+                    WParagraph cellParagraph = row.Cells[0].AddParagraph() as WParagraph;
+                    //Set text and character format.
+                    IWTextRange textRange = cellParagraph.AppendText("New row's first cell");
+                    //Insert a row into the table in specific index.
+                    table.Rows.Insert(2, row);
+                    //Creates file stream.
+                    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Result.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        //Saves and close the Word document.
+                        document.Save(outputFileStream, FormatType.Docx);
+                    }
+                }
+            }
+        }
+    }
+}
