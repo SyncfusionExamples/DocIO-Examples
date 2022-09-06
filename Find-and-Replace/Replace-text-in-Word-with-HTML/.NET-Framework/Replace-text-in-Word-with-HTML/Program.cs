83bdbe45a577880941a9f0c05788ae4ea712cda0
Anto Nihil Sahaya Raj <antonihil.sahayaraj@syncfusion.com>
2022-09-06 15:08:12 +0530

    Added Sample of Insert-Row-with-same-formatting

--- Find-and-Replace/Replace-text-in-Word-with-HTML/.NET-Framework/Replace-text-in-Word-with-HTML/Program.cs
+++ Find-and-Replace/Replace-text-in-Word-with-HTML/.NET-Framework/Replace-text-in-Word-with-HTML/Program.cs
@@ -9,28 +9,28 @@ namespace Replace_text_in_Word_with_HTML
     {
         static void Main(string[] args)
         {
-            //Loads an existing Word document.
+            //Open an existing Word document.
             using (WordDocument document = new WordDocument(Path.GetFullPath(@"../../Data/Sample.docx"), FormatType.Docx))
             {
-                //Create the temporary word document for html.
+                //Create the temporary word document for HTML.
                 using (WordDocument replaceDoc = new WordDocument())
                 {
-                    //Add section for html document. 
+                    //Add section for HTML document. 
                     IWSection htmlsection = replaceDoc.AddSection();
-                    //Reads HTML string from the file.
+                    //Read HTML string from the file.
                     string htmlString = File.ReadAllText(Path.GetFullPath(@"../../Data/File.html"));
-                    //Validates the Html string.
+                    //Validate the HTML string.
                     bool isValidHtml = htmlsection.Body.IsValidXHTML(htmlString, XHTMLValidationType.None);
-                    //When the Html string passes validation, it is inserted to the document.
+                    //When the HTML string passes validation, it is inserted to the document.
                     if (isValidHtml)
                     {
-                        //Appends Html string in the temporary word document.
+                        //Append HTML string in the temporary word document.
                         htmlsection.Body.InsertXHTML(htmlString);
                     }
-                    //Replaces the content placeholder text with desired Word document.
+                    //Replace the content placeholder text with desired Word document.
                     document.Replace(new Regex("«([a-zA-Z0-9 ]*:*[a-zA-Z0-9 ]+)»"), replaceDoc, true);
                 }
-                //Saves the Word document to file stream.
+                //Save the Word document to file stream.
                 document.Save(Path.GetFullPath(@"../../Result.docx"), FormatType.Docx);
             }
         }
