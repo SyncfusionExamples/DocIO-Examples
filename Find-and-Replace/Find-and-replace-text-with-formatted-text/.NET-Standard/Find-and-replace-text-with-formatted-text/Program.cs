5ae88f733e1a59db7afb6ecd9aacbc5acf2270ec
Suriya Balamurugan <suriya.balamurugan@syncfusion.com>
2022-09-13 22:40:30 +0530

    Added sample for find and replace

---
+++ Find-and-Replace/Find-and-replace-text-with-formatted-text/.NET-Standard/Find-and-replace-text-with-formatted-text/Program.cs
@@ -0,0 +1,31 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Find_and_replace_text_with_formatted_text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fileStream = new FileStream(Path.GetFullPath(@"../../../Template.docx"), FileMode.Open, FileAccess.ReadWrite))
+            {
+                //Open an existing Word document.
+                using (WordDocument document = new WordDocument(fileStream, FormatType.Automatic))
+                {
+                    //Find the first occurrence of particular text in the document.
+                    TextSelection selection = document.Find(new Regex ("^«(.*)»"));
+                    //Replace a particular text with the selected text along with formatting.
+                    document.Replace("Bear", selection, false, false, true);
+                    //Create file stream.
+                    using (FileStream outputStream = new FileStream(Path.GetFullPath(@"../../../Result.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        //Save the Word document to file stream.
+                        document.Save(outputStream, FormatType.Docx);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Find-and-Replace/Replace-multiline-text-with-single-line/.NET-Standard/Replace-multiline-text-with-single-line/Program.cs
@@ -0,0 +1,29 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Replace_multiline_text_with_single_line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fileStream = new FileStream(Path.GetFullPath(@"../../../Template.docx"), FileMode.Open, FileAccess.ReadWrite))
+            {
+                //Open an existing Word document.
+                using (WordDocument document = new WordDocument(fileStream, FormatType.Automatic))
+                {
+                    //Replace the text extended to several paragraphs with simple text.
+                    document.ReplaceSingleLine(new Regex("«(.*)»"), "Replaced paragraph");
+                    //Create file stream.
+                    using (FileStream outputStream = new FileStream(Path.GetFullPath(@"../../../Result.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        //Save the Word document to file stream.
+                        document.Save(outputStream, FormatType.Docx);
+                    }
+                }
+            }
+        }
+    }
+}
