6036ee6720cc8da9110540b15588015e20a6d778
elizabethatienoo <elizabethatienoo@syncfusion.com>
2022-09-14 14:30:03 +0300

    Made code change for logic adjustment

--- Find-and-Replace/Find-and-replace-text-within-table/.NET-Framework/Find-and-replace-text-within-table/Program.cs
+++ Find-and-Replace/Find-and-replace-text-within-table/.NET-Framework/Find-and-replace-text-within-table/Program.cs
@@ -9,43 +9,34 @@ namespace Find_and_replace_text_within_table
     {
         static void Main(string[] args)
         {
-            //Loads an existing Word document.
+            //Load the Input document.
             using (WordDocument document = new WordDocument(Path.GetFullPath(@"../../Input.docx"), FormatType.Docx))
             {
-                //Iterate through the document sections.
-                foreach (WSection section in document.Sections)
+                //Access table in Word document.
+                WTable table = document.Sections[0].Body.ChildEntities[0] as WTable;
+                //Iterate through the rows of table.
+                foreach (WTableRow row in table.Rows)
                 {
-                    foreach (Entity entity in section.Body.ChildEntities)
+                    //Iterate through the cells of rows.
+                    foreach (WTableCell cell in row.Cells)
                     {
-                        if (entity.EntityType == EntityType.Table)
+                        //Iterates through the paragraphs of the cell.
+                        foreach (Entity ent in cell.ChildEntities)
                         {
-                            WTable table = (WTable)entity;
-                            //Iterate through the rows of table.
-                            foreach (WTableRow row in table.Rows)
+                            if (ent.EntityType == EntityType.Paragraph)
                             {
-                                //Iterate through the cells of rows.
-                                foreach (WTableCell cell in row.Cells)
+                                WParagraph paragraph = ent as WParagraph;
+                                //Find the selection of text inside the paragraph.
+                                TextSelection[] textSelections = document.FindAll("Suppliers", false, true);
+                                for (int i = 0; i < textSelections.Length; i++)
                                 {
-                                    //Iterates through the paragraphs of the cell.
-                                    foreach (Entity ent in cell.ChildEntities)
-                                    {
-                                        if (ent.EntityType == EntityType.Paragraph)
-                                        {
-                                            WParagraph paragraph = ent as WParagraph;
-                                            //Find the selection of text inside the paragraph.
-                                            TextSelection[] textSelections = document.FindAll("Suppliers", false, true);
-                                            for (int i = 0; i < textSelections.Length; i++)
-                                            {
-                                                //Replace the specified regular expression with a TextSelection in the paragraph.
-                                                paragraph.Replace(new Regex("^//(.*)"), textSelections[i]);
-                                            }
-                                        }
-                                    }
+                                    //Replace the specified regular expression with a TextSelection in the paragraph.
+                                    paragraph.Replace(new Regex("^//(.*)"), textSelections[i]);
                                 }
                             }
                         }
                     }
-                }
+                }  
                 //Save the document.
                 document.Save(Path.GetFullPath("../../Sample.docx"), FormatType.Docx);
             }
