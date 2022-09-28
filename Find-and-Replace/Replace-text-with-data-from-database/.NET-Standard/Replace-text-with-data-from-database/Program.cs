a2f58bd9d28f9f338230c90b68879f9853f9103d
sharonaumao <sharonaumao@syncfusion.com>
2022-09-28 16:34:22 +0300

    Adding Sample of Find-and-Replace text with data from database

---
+++ Find-and-Replace/Replace-text-with-data-from-database/.NET-Standard/Replace-text-with-data-from-database/Program.cs
@@ -0,0 +1,49 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.Drawing;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace Replace_text_with_data_from_database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream inputStream = new FileStream(Path.GetFullPath(@"../../../Input.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                //Set datasource and database values.
+                string datasource = "";
+                string database = "";
+                //Load the file stream into a Word document.
+                using (WordDocument document = new WordDocument(inputStream, FormatType.Automatic))
+                {
+                    SqlConnection SqlConn = new SqlConnection("Data Source=" + datasource + ";Initial Catalog=" + database + ";Integrated Security=True");
+                    //Retrive data from the table 'FindReplace' using SqlCommand.
+                    SqlCommand sqlCommand = new SqlCommand("Select * from FindReplace");
+                    sqlCommand.Connection = SqlConn;
+
+                    //Load the data into DataTable using SqlDataAdapter.
+                    SqlDataAdapter da = new SqlDataAdapter(sqlCommand);
+                    da.SelectCommand.CommandTimeout = 0;
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    //Find and replace text with other text from SQL server.
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        //First column contains text to find.
+                        //Second column contains replacement text.
+                        document.Replace(row[dt.Columns[0]] as string, row[dt.Columns[1]] as string, false, false);
+                    }
+                    //Create a file stream.
+                    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Sample.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        //Save the Word document to the file stream.
+                        document.Save(outputFileStream, FormatType.Docx);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Find-and-Replace/Replace-text-with-data-from-database/README.md
@@ -0,0 +1,18 @@
+# How to replace text with data from database
+This example shows how to find a text in a Word document and replace it with a text from the database.
+
+# How to run this Sample
+
+1. Download this project to a location in your disk.
+
+2. Create database with column "TextToFind" and "ReplacementText" as the screenshot shown below.
+<img src="/assets/images/Database_Screenshot.png" alt=""/> 
+
+3. Open the solution file using Visual Studio.
+
+4. Add datasource and database values in Program.cs file
+
+5. Rebuild the solution to install the required NuGet packages.
+
+5. Run the application.
+ 
