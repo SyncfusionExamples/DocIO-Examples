71865d63377dd694fbe982604c7d0fa8772ca98a
sharonaumao <sharonaumao@syncfusion.com>
2022-10-19 15:45:59 +0300

    Adding proper comments

--- Sections/Add-multiple-page-number-formarts/.NET-Standard/Add-multiple-page-number-formarts/Program.cs
+++ Sections/Add-multiple-page-number-formarts/.NET-Standard/Add-multiple-page-number-formarts/Program.cs
@@ -13,13 +13,16 @@ namespace Add_multiple_page_number_formarts
             {
                 //Add a section to the document.
                 IWSection section = document.AddSection();
+                //Add a page number to the document.
                 section.PageSetup.PageStartingNumber = 1;
                 section.PageSetup.PageNumberStyle = PageNumberStyle.Arabic;
                 //Add a footer paragraph to the document.
                 IWParagraph paragraph = section.HeadersFooters.Footer.AddParagraph();
-                paragraph.ParagraphFormat.Tabs.AddTab(523f, TabJustification.Left, TabLeader.NoLeader);
+                paragraph.ParagraphFormat.Tabs.AddTab(523f, TabJustification.Right, TabLeader.NoLeader);
+                //Add a text for the footer paragraph.
+                paragraph.AppendText("Copyright Northwind Inc. 2001 - 2015");
                 //Add page number field to the document.
-                paragraph.AppendText("Page ");
+                paragraph.AppendText("\tPage ");
                 paragraph.AppendField("Page", FieldType.FieldPage);
                 //Add a paragraph to a section.
                 paragraph = section.AddParagraph();
@@ -32,6 +35,7 @@ namespace Add_multiple_page_number_formarts
                 section.PageSetup.PageNumberStyle = PageNumberStyle.LetterUpper;
                 //Add a paragraph to a section.
                 paragraph = section.AddParagraph();
+                //Append the text to the created paragraph.
                 paragraph.AppendText("AdventureWorks Cycles, the fictitious company on which the AdventureWorks sample databases are based, is a large, multinational manufacturing company.");
                 //Create a file stream.
                 using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Sample.docx"), FileMode.Create, FileAccess.ReadWrite))
