9e9bb56b0a7ce7af394c5ae6cb7011addb796499
sharonaumao <sharonaumao@syncfusion.com>
2022-10-25 11:50:17 +0300

    Adding sample of modufying hyperlink in Paragraphs

---
+++ Paragraphs/Find-and-modify-hyperlink-address/.NET-Standard/Find-and-modify-hyperlink-address/Program.cs
@@ -0,0 +1,36 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.IO;
+
+namespace Find_and_modify_hyperlink_address
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Open the file as a stream.
+            using (FileStream inputStream = new FileStream(Path.GetFullPath(@"../../../Input.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                //Load the file stream into a Word document.
+                using (WordDocument document = new WordDocument(inputStream, FormatType.Docx))
+                {
+                    //Access paragraph in a Word document.
+                    WParagraph paragraph = document.Sections[0].Paragraphs[1];
+                    WField field = paragraph.ChildEntities[0] as WField;
+                    //Create an instance of hyperlink.
+                    Hyperlink hyperlink = new Hyperlink(field);
+                    //Set the hyperlink type, URL and the text to display.
+                    hyperlink.Type = HyperlinkType.WebLink;
+                    hyperlink.Uri = "http://www.google.com";
+                    hyperlink.TextToDisplay = "Google";
+                    //Create a file stream.
+                    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Sample.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        //Save the Word document to the file stream.
+                        document.Save(outputFileStream, FormatType.Docx);
+                    }
+                }
+            }
+        }       
+    }
+}
