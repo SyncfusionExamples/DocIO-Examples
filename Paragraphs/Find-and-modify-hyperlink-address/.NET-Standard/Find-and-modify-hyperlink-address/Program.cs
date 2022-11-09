41304ea24f64bd8edc38bd65d4bdde82dfa6d169
sharonaumao <sharonaumao@syncfusion.com>
2022-11-09 11:35:32 +0300

    Adding sample of modifying hyperlink

---
+++ Paragraphs/Find-and-modify-hyperlink-address/.NET-Standard/Find-and-modify-hyperlink-address/Program.cs
@@ -0,0 +1,54 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.IO;
+
+namespace Find_and_modify_hyperlink_address
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Open the file as a stream.
+            using (FileStream inputStream = new FileStream(Path.GetFullPath(@"../../../Input.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                //Load the file stream into a Word document.
+                using (WordDocument document = new WordDocument(inputStream, FormatType.Docx))
+                {
+                    for (int i = 0; i < document.Sections[0].Paragraphs.Count; i++)
+                    {
+                        WParagraph paragraph = document.Sections[0].Paragraphs[i];
+                        //Iterate through the paragraph items.
+                        foreach (ParagraphItem item in paragraph.ChildEntities)
+                        {
+                            if (item is WField)
+                            {
+                                if ((item as WField).FieldType == FieldType.FieldHyperlink)
+                                {
+                                    //Get the hyperlink field.
+                                    Hyperlink link = new Hyperlink(item as WField);
+                                    if (link.Type == HyperlinkType.WebLink)
+                                    {
+                                        //Modify the url of the hyperlink.
+                                        link.Uri = "http://www.google.com";
+                                    }
+                                    else if (link.Type == HyperlinkType.EMailLink)
+                                    {
+                                        //Modify the url of the hyperlink.
+                                        link.Uri = "sales@syncfusion.com";
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    //Create a file stream.
+                    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Sample.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        //Save the Word document to the file stream.
+                        document.Save(outputFileStream, FormatType.Docx);
+                    }
+                }
+            }
+        }
+    }
+}
