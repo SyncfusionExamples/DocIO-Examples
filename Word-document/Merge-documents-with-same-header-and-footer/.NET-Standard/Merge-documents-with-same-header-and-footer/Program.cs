0cc0d978f2c50fb47dac0dd6c57100d99b6bfce4
sharonaumao <sharonaumao@syncfusion.com>
2022-11-29 16:26:40 +0300

    Adding sample of merging documents with same header and footer

---
+++ Word-document/Merge-documents-with-same-header-and-footer/.NET-Standard/Merge-documents-with-same-header-and-footer/Program.cs
@@ -0,0 +1,54 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.IO;
+
+namespace Merge_documents_with_same_header_and_footer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Open the file as a stream.
+            using (FileStream sourceStreamPath = new FileStream(Path.GetFullPath(@"../../../Data/SourceDocument.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                //Load the file stream into a Word document.
+                using (WordDocument sourceDocument = new WordDocument(sourceStreamPath, FormatType.Automatic))
+                {
+                    using (FileStream destinationStreamPath = new FileStream(Path.GetFullPath(@"../../../Data/DestinationDocument.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                    {
+                        //Open the destination document.
+                        using (WordDocument destinationDocument = new WordDocument(destinationStreamPath, FormatType.Automatic))
+                        {
+                            //Get the default header and footer in the destination document.
+                            EntityCollection firstDocumentHeader = destinationDocument.Sections[0].HeadersFooters.Header.ChildEntities;
+                            EntityCollection firstDocumentFooter = destinationDocument.Sections[0].HeadersFooters.Footer.ChildEntities;
+                            //Iterate source document.
+                            foreach (WSection sourceSection in sourceDocument.Sections)
+                            {
+                                //Clear the header and footer of the source document.
+                                sourceSection.HeadersFooters.Header.ChildEntities.Clear();
+                                sourceSection.HeadersFooters.Footer.ChildEntities.Clear();
+                                foreach (Entity entity in firstDocumentHeader)
+                                {
+                                    sourceSection.HeadersFooters.Header.ChildEntities.Add(entity.Clone());
+                                }
+                                foreach (Entity entity in firstDocumentFooter)
+                                {
+                                    sourceSection.HeadersFooters.Footer.ChildEntities.Add(entity.Clone());
+                                }
+                                //Clone and merge the source document sections to the destination document.
+                                destinationDocument.Sections.Add(sourceSection.Clone());
+                            }
+                            //Create a file stream.
+                            using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Sample.docx"), FileMode.Create, FileAccess.ReadWrite))
+                            {
+                                //Save the Word document to the file stream.
+                                destinationDocument.Save(outputFileStream, FormatType.Docx);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
