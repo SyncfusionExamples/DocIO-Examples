b15b7a11b72c4e01c9ed4acbb9788e1baf881b17
chrispojunior <99657142+chrispojunior@users.noreply.github.com>
2022-12-13 16:09:10 +0300

    Modified the code to address feedback.

--- Find-and-Replace/Replace-text-within-bookmark-content/.NET-Standard/Replace-text-within-bookmark-content/Program.cs
+++ Find-and-Replace/Replace-text-within-bookmark-content/.NET-Standard/Replace-text-within-bookmark-content/Program.cs
@@ -16,8 +16,8 @@ namespace Replace_text_within_bookmark_content
                 using (WordDocument document = new WordDocument(inputStream, FormatType.Docx))
                 {
                     //Replace a text within the bookmark.
+                    ReplaceBookmarkText(document, "Description");
                     ReplaceBookmarkText(document, "Adventure");
-                    ReplaceBookmarkText(document, "Test");
                     //Create a file stream.
                     using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Sample.docx"), FileMode.Create, FileAccess.ReadWrite))
                     {
@@ -38,14 +38,41 @@ namespace Replace_text_within_bookmark_content
             //Replace the bookmark content with new text.
             TextBodyPart textBodyPart = bookmarksNavigator.GetBookmarkContent();
             //Get paragraph from the textBody part.
-            foreach (WParagraph paragraph in textBodyPart.BodyItems)
+            foreach (TextBodyItem item in textBodyPart.BodyItems)
             {
-                //Replace a text in the bookmark content.
-                paragraph.Replace(new System.Text.RegularExpressions.Regex("two thousand"), "2000");
-                paragraph.Replace(new System.Text.RegularExpressions.Regex("Washington"), "USA");
+                IterateTextBody(item);
             }
             //Replace the bookmark content with text body part.
             bookmarksNavigator.ReplaceBookmarkContent(textBodyPart);
+        }
+        /// <summary>
+        /// Iterate text body items.
+        /// </summary>
+        public static void IterateTextBody(TextBodyItem item)
+        {
+            switch (item.EntityType)
+            {
+                case EntityType.Paragraph:
+                    WParagraph paragraph = (WParagraph)item;
+                    //Replace a text in the bookmark content.
+                    paragraph.Replace(new System.Text.RegularExpressions.Regex("Price"), "Amount");
+                    paragraph.Replace(new System.Text.RegularExpressions.Regex("2000"), "two thousand");
+                    break;
+                case EntityType.Table:
+                    WTable table = (WTable)item;
+                    foreach (WTableRow row in table.Rows)
+                    {
+                        foreach (WTableCell cell in row.Cells)
+                        {
+                            foreach (TextBodyItem bodyitem in cell.ChildEntities)
+                            {
+                                IterateTextBody(bodyitem);
+                            }
+                        }
+                    }
+                    break;
+
+            }
         }
     }
 }
