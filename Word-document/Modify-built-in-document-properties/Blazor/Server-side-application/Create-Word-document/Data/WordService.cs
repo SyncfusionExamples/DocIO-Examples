e9da6890419a07163b20dc1808d3c92a4575ea53
AkashArul26 <95226047+AkashArul26@users.noreply.github.com>
2023-01-27 15:57:02 +0530

    Committing the Blazor samples

---
+++ Word-document/Modify-built-in-document-properties/Blazor/Server-side-application/Create-Word-document/Data/WordService.cs
@@ -0,0 +1,32 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System;
+using System.IO;
+
+
+namespace Create_Word_document.Data
+{
+    public class WordService
+    {
+        public MemoryStream CreateWord()
+        {
+            FileStream sourceStreamPath = new FileStream(@"wwwroot/data/Template.docx", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            //Creating a new document.
+            using (WordDocument document = new WordDocument(sourceStreamPath, FormatType.Automatic))
+            {
+                //Accesses the built-in document properties
+                Console.WriteLine("Title - {0}", document.BuiltinDocumentProperties.Title);
+                Console.WriteLine("Author - {0}", document.BuiltinDocumentProperties.Author);
+                //Modifies or sets the category and company Built-in document properties
+                document.BuiltinDocumentProperties.Category = "Sales reports";
+                document.BuiltinDocumentProperties.Company = "Northwind traders";
+
+                //Saves the Word document to MemoryStream.
+                MemoryStream stream = new MemoryStream();
+                document.Save(stream, FormatType.Docx);
+                stream.Position = 0;
+                return stream;
+            }
+        }
+    }
+}
