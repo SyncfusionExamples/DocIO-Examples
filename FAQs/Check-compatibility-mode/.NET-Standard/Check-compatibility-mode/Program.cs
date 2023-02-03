15cb580286521589a91964cf2ae08b72ebacb2ce
sharonaumao <sharonaumao@syncfusion.com>
2023-02-03 14:41:17 +0300

    Adding sample of FAQ

---
+++ FAQs/Check-compatibility-mode/.NET-Standard/Check-compatibility-mode/Program.cs
@@ -0,0 +1,29 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.IO;
+
+namespace Check_compatibility_mode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fileStreamPath = new FileStream(Path.GetFullPath(@"../../../Input.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                //Open the Word document.
+                using (WordDocument document = new WordDocument(fileStreamPath, FormatType.Automatic))
+                {
+                    //Set the compatibility mode.
+                    document.Settings.CompatibilityMode = CompatibilityMode.Word2003;
+
+                    //Create a file stream.
+                    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Sample.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        //Save the Word document to the file stream.
+                        document.Save(outputFileStream, FormatType.Docx);
+                    }
+                }
+            }
+        }
+    }
+}
