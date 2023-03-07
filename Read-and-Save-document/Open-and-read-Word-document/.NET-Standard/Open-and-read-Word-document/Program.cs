2458c0f3a45e467d86157d6d91d2d82904542040
sharonaumao <sharonaumao@syncfusion.com>
2023-03-07 12:12:46 +0300

    Adding samples of opening and reading Word document

---
+++ Read-and-Save-document/Open-and-read-Word-document/.NET-Standard/Open-and-read-Word-document/Program.cs
@@ -0,0 +1,27 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System;
+using System.IO;
+
+namespace Open_and_read_Word_document
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Open the file as a stream.
+            using (FileStream inputFileStream = new FileStream(Path.GetFullPath(@"../../../Template.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                //Load the file stream into a Word document.
+                using (WordDocument document = new WordDocument(inputFileStream, FormatType.Automatic))
+                {
+                    //Get the Word document text.
+                    string text = document.GetText();
+                    //Display Word document's text content.
+                    Console.WriteLine(text);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
