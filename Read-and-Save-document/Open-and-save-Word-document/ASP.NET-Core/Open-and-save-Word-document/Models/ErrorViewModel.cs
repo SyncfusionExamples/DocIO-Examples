573bb748846b5b3bef2b98396e65b06f3d598804
elizabethatienoo <elizabethatienoo@syncfusion.com>
2023-03-22 16:21:42 +0300

    Added sample to open and save Word document in ASP.NET-Core

---
+++ Read-and-Save-document/Open-and-save-Word-document/ASP.NET-Core/Open-and-save-Word-document/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Open_and_save_Word_document.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
