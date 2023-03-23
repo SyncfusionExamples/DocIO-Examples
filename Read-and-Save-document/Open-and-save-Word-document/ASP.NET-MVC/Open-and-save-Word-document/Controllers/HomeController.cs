abe3c346e9c62a3a6d4864bec49a7be1f5b9ebcb
sharonaumao <sharonaumao@syncfusion.com>
2023-03-23 12:49:58 +0300

    Adding samples of Opening and saving Word document in ASP.NET MVC

---
+++ Read-and-Save-document/Open-and-save-Word-document/ASP.NET-MVC/Open-and-save-Word-document/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.IO;
+using System.Drawing;
+using System.Web.Mvc;
+
+namespace Open_and_save_Word_document.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult OpenAndSaveDocument()
+        {
+            //Open an existing Word document.
+            using (WordDocument document = new WordDocument(Server.MapPath("~/App_Start/Input.docx")))
+            {
+                //Access the section in a Word document.
+                IWSection section = document.Sections[0];
+                //Add a new paragraph to the section.
+                IWParagraph paragraph = section.AddParagraph();
+                paragraph.ParagraphFormat.FirstLineIndent = 36;
+                paragraph.BreakCharacterFormat.FontSize = 12f;
+                IWTextRange text = paragraph.AppendText("In 2000, Adventure Works Cycles bought a small manufacturing plant, Importadores Neptuno, located in Mexico. Importadores Neptuno manufactures several critical subcomponents for the Adventure Works Cycles product line. These subcomponents are shipped to the Bothell location for final product assembly. In 2001, Importadores Neptuno, became the sole manufacturer and distributor of the touring bicycle product group.");
+                text.CharacterFormat.FontSize = 12f;
+
+                //Save the Word document to the disk in a DOCX format.
+                document.Save("Sample.docx", FormatType.Docx, HttpContext.ApplicationInstance.Response, HttpContentDisposition.Attachment);
+            }
+
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
