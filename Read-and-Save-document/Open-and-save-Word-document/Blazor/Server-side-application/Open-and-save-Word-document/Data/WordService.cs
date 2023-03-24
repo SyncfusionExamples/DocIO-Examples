39677306ebbb1b5aba7a5a2500210584e3ac0c3d
elizabethatienoo <elizabethatienoo@syncfusion.com>
2023-03-24 15:59:11 +0300

    Made code changes

--- Read-and-Save-document/Open-and-save-Word-document/Blazor/Server-side-application/Open-and-save-Word-document/Data/WordService.cs
+++ Read-and-Save-document/Open-and-save-Word-document/Blazor/Server-side-application/Open-and-save-Word-document/Data/WordService.cs
@@ -8,15 +8,22 @@ namespace Open_and_save_Word_document.Data
 {
     public class WordService
     {
-        public MemoryStream CreateWord()
+        public MemoryStream OpenAndSaveWord()
         {
-            using (FileStream sourceStreamPath = new FileStream(@"wwwroot/data/HelloWorld.docx", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (FileStream sourceStreamPath = new FileStream(@"wwwroot/data/Input.docx", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             {
                 //Opening a document.
                 using (WordDocument document = new WordDocument(sourceStreamPath, FormatType.Automatic))
                 {
-                    //Appends text to the last paragraph of the document.
-                    document.LastParagraph.AppendText("Hello World");
+                    //Access the section in a Word document.
+                    IWSection section = document.Sections[0];
+                    //Add new paragraph to the section.
+                    IWParagraph paragraph = section.AddParagraph();
+                    paragraph.ParagraphFormat.FirstLineIndent = 36;
+                    paragraph.BreakCharacterFormat.FontSize = 12f;
+                    //Add new text to the paragraph.
+                    IWTextRange textRange = paragraph.AppendText("In 2000, AdventureWorks Cycles bought a small manufacturing plant, Importadores Neptuno, located in Mexico. Importadores Neptuno manufactures several critical subcomponents for the AdventureWorks Cycles product line. These subcomponents are shipped to the Bothell location for final product assembly. In 2001, Importadores Neptuno, became the sole manufacturer and distributor of the touring bicycle product group.") as IWTextRange;
+                    textRange.CharacterFormat.FontSize = 12f;
 
                     //Saves the Word document to MemoryStream.
                     MemoryStream stream = new MemoryStream();
