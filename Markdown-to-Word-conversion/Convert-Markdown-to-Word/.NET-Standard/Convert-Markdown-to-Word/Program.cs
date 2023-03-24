9da3ca91214b43363681f1d9099cdbd8fd774e35
sharonaumao <sharonaumao@syncfusion.com>
2023-03-24 09:43:47 +0300

    Adding samples of Markdown to Word conversion

---
+++ Markdown-to-Word-conversion/Convert-Markdown-to-Word/.NET-Standard/Convert-Markdown-to-Word/Program.cs
@@ -0,0 +1,27 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.IO;
+
+namespace Convert_Markdown_to_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Open a file as a stream.
+            using (FileStream fileStreamPath = new FileStream(Path.GetFullPath(@"../../../Input.md"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                //Load the file stream into a Markdown file.
+                using (WordDocument document = new WordDocument(fileStreamPath, FormatType.Markdown))
+                {
+                    //Create a file stream.
+                    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../MarkdownToWord.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        //Save a Word document to the file stream.
+                        document.Save(outputFileStream, FormatType.Docx);
+                    }
+                }
+            }
+        }
+    }
+}
