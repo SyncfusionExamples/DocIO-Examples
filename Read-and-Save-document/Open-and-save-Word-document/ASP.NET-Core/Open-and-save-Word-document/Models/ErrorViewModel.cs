1c27e5b71826ed548efde9a6cea703b4a7f3422d
elizabethatienoo <elizabethatienoo@syncfusion.com>
2023-03-24 14:47:15 +0300

    removed this file

--- Read-and-Save-document/Open-and-save-Word-document/ASP.NET-Core/Open-and-save-Word-document/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Open_and_save_Word_document.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
