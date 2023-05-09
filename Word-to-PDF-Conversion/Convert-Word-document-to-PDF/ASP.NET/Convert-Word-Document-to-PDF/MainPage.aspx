9e370a62b3f82f83eb17a83e91f36c4b03e0a05b
AzureAD\VenkateshMurugananda <venkatesh.m@syncfusion.com>
2023-05-09 19:21:04 +0530

    Sample Added

---
+++ Word-to-PDF-Conversion/Convert-Word-document-to-PDF/ASP.NET/Convert-Word-Document-to-PDF/MainPage.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainPage.aspx.cs" Inherits="Convert_Word_Document_to_PDF.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <asp:Button ID="Button1" runat="server" Text="Convert Word to PDF" OnClick="OnButtonClicked" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Word-to-PDF-Conversion/Convert-Word-document-to-PDF/ASP.NET/Convert-Word-Document-to-PDF/MainPage.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Web;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.IO;
+using Syncfusion.DocToPDFConverter;
+using Syncfusion.Pdf;
+using System.Web.Configuration;
+
+namespace Convert_Word_Document_to_PDF
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void OnButtonClicked(object sender, EventArgs e)
+        {   
+            //Open an existing Word document.        
+            string filePath = Server.MapPath("~/App_Data/Template.docx");
+
+            //Loads file into Word document
+            using (WordDocument document = new WordDocument(filePath))
+            {
+                //Instantiation of DocToPDFConverter for Word to PDF conversion
+                using (DocToPDFConverter converter = new DocToPDFConverter())
+                {
+                    //Converts Word document into PDF document
+                    using (PdfDocument pdfDocument = converter.ConvertToPDF(document))
+                    {
+                        //Saves the PDF document to MemoryStream.
+                        MemoryStream stream = new MemoryStream();
+                        pdfDocument.Save("sample.pdf", HttpContext.Current.Response, HttpReadType.Save);
+                        stream.Position = 0;
+                    }
+                    
+                }
+            }
+        }
+    }
+}
