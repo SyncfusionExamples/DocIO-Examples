f51d93e5ffbd043135ff3046d8dbd70dc4131200
AzureAD\VenkateshMurugananda <venkatesh.m@syncfusion.com>
2023-05-11 20:24:18 +0530

    Sample Added

---
+++ Word-to-PDF-Conversion/Convert-Word-document-to-PDF/WPF/Convert-Word-Document-to-PDF/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Syncfusion.DocIO.DLS;
+using System.IO;
+using Syncfusion.DocIO;
+using Syncfusion.DocToPDFConverter;
+using Syncfusion.Pdf;
+using System.ComponentModel;
+using System.Windows;
+
+
+namespace Convert_Word_Document_to_PDF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnConvert_Click(object sender, RoutedEventArgs e)
+        {
+            //Open an existing Word document.
+            using (WordDocument document = new WordDocument(Path.GetFullPath(@"../../Data/Input.docx"), FormatType.Automatic))
+            {
+                //Instantiation of DocToPDFConverter for Word to PDF conversion
+                using (DocToPDFConverter converter = new DocToPDFConverter())
+                {
+                    //Converts Word document into PDF document
+                    using (PdfDocument pdfDocument = converter.ConvertToPDF(document))
+                    {
+                        //Saves the PDF document
+                        pdfDocument.Save(Path.GetFullPath(@"../../Sample.pdf"));
+                    }
+                };
+            }
+            MessageBox.Show("PDF converted successfully");
+        }                    
+    }
+}
