55b0f7a870a1151c3a8c3d8d52dd0a5f0dc57336
AzureAD\VenkateshMurugananda <venkatesh.m@syncfusion.com>
2023-05-12 19:07:05 +0530

    Modified SaveandLaumch Code

--- Word-to-PDF-Conversion/Convert-Word-document-to-PDF/WinUI/Convert-Word-Document-to-PDF/SaveHelper.cs
+++ Word-to-PDF-Conversion/Convert-Word-document-to-PDF/WinUI/Convert-Word-Document-to-PDF/SaveHelper.cs
@@ -27,28 +27,7 @@ namespace Create_Word_document
             if (!Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons"))
             {
                 FileSavePicker savePicker = new();
-                if (extension == ".docx")
-                {
-                    savePicker.DefaultFileExtension = ".docx";
-                    savePicker.SuggestedFileName = filename;
-                    //Saves the file as Docx file.
-                    savePicker.FileTypeChoices.Add("DOCX", new List<string>() { ".docx" });
-                }
-                else if (extension == ".doc")
-                {
-                    savePicker.DefaultFileExtension = ".doc";
-                    savePicker.SuggestedFileName = filename;
-                    //Saves the file as Doc file.
-                    savePicker.FileTypeChoices.Add("DOC", new List<string>() { ".doc" });
-                }
-                else if (extension == ".rtf")
-                {
-                    savePicker.DefaultFileExtension = ".rtf";
-                    savePicker.SuggestedFileName = filename;
-                    //Saves the file as Xml file.
-                    savePicker.FileTypeChoices.Add("RTF", new List<string>() { ".rtf" });
-                }
-                else if (extension == ".pdf")
+                if (extension == ".pdf")
                 {
                     savePicker.DefaultFileExtension = ".pdf";
                     savePicker.SuggestedFileName = filename;
@@ -66,31 +45,54 @@ namespace Create_Word_document
             }
             if (storageFile != null)
             {
-                using (IRandomAccessStream zipStream = await storageFile.OpenAsync(FileAccessMode.ReadWrite))
+                try
                 {
-                    //Writes compressed data from memory to file.
-                    using Stream outstream = zipStream.AsStreamForWrite();
-                    outstream.SetLength(0);
-                    byte[] buffer = stream.ToArray();
-                    outstream.Write(buffer, 0, buffer.Length);
-                    outstream.Flush();
+                    using (IRandomAccessStream zipStream = await storageFile.OpenAsync(FileAccessMode.ReadWrite))
+                    {
+                        //Writes compressed data from memory to file.
+                        using Stream outstream = zipStream.AsStreamForWrite();
+                        outstream.SetLength(0);
+                        byte[] buffer = stream.ToArray();
+                        outstream.Write(buffer, 0, buffer.Length);
+                        outstream.Flush();
+                    }
+                    //Creates message dialog box. 
+                    MessageDialog msgDialog = new("Do you want to view the Document?", "File has been created successfully");
+                    UICommand yesCmd = new("Yes");
+                    msgDialog.Commands.Add(yesCmd);
+                    UICommand noCmd = new("No");
+                    msgDialog.Commands.Add(noCmd);
+
+                    WinRT.Interop.InitializeWithWindow.Initialize(msgDialog, windowHandle);
+
+                    //Showing a dialog box. 
+                    IUICommand cmd = await msgDialog.ShowAsync();
+                    if (cmd.Label == yesCmd.Label)
+                    {
+                        if (extension == ".md")
+                        {
+                            Windows.System.LauncherOptions options = new Windows.System.LauncherOptions();
+                            options.DisplayApplicationPicker = true;
+                            // Launch the file with "Open with" option.
+                            await Windows.System.Launcher.LaunchFileAsync(storageFile, options);
+                        }
+                        else
+                            //Launch the saved file. 
+                            await Windows.System.Launcher.LaunchFileAsync(storageFile);
+                    }
                 }
-
-                //Creates message dialog box. 
-                MessageDialog msgDialog = new("Do you want to view the Document?", "File has been created successfully");
-                UICommand yesCmd = new("Yes");
-                msgDialog.Commands.Add(yesCmd);
-                UICommand noCmd = new("No");
-                msgDialog.Commands.Add(noCmd);
-
-                WinRT.Interop.InitializeWithWindow.Initialize(msgDialog, windowHandle);
-
-                //Showing a dialog box. 
-                IUICommand cmd = await msgDialog.ShowAsync();
-                if (cmd.Label == yesCmd.Label)
+                catch (Exception ex)
                 {
-                    //Launch the saved file. 
-                    await Windows.System.Launcher.LaunchFileAsync(storageFile);
+                    if (ex.Message.Contains("Access is denied."))
+                    {
+                        //Creates message dialog box.
+                        MessageDialog msgDialogBox = new("Access to the given path is denied. Please enable permission to save the file in that folder or save the file in another location.", "Access Denied");
+                        UICommand okCmd = new("Ok");
+                        msgDialogBox.Commands.Add(okCmd);
+                        WinRT.Interop.InitializeWithWindow.Initialize(msgDialogBox, windowHandle);
+                        //Showing a dialog box. 
+                        IUICommand msgCmd = await msgDialogBox.ShowAsync();
+                    }
                 }
             }
         }
