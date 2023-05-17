892ede750dd43d41d4757b0168e6c157db6d8a5c
AzureAD\VenkateshMurugananda <venkatesh.m@syncfusion.com>
2023-05-17 10:31:02 +0530

    Sample Added

---
+++ Word-to-PDF-Conversion/Convert-Word-document-to-PDF/Xamarin/Convert-Word-Document-to-PDF/Convert-Word-Document-to-PDF.UWP/SaveWindows.cs
@@ -0,0 +1,77 @@
+﻿#region Copyright Syncfusion Inc. 2001-2016.
+// Copyright Syncfusion Inc. 2001-2016. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: Dependency(typeof(SaveWindows))]
+
+class SaveWindows : ISave
+{
+
+    public async Task SaveAndView(string filename, string contentType, MemoryStream stream)
+    {
+        stream.Position = 0;
+        //save the stream into the file. 
+        if (Device.Idiom != TargetIdiom.Desktop)
+        {
+            StorageFolder local = Windows.Storage.ApplicationData.Current.LocalFolder;
+            StorageFile outFile = await local.CreateFileAsync(filename, CreationCollisionOption.ReplaceExisting);
+            using (Stream outStream = await outFile.OpenStreamForWriteAsync())
+            {
+                outStream.Write(stream.ToArray(), 0, (int)stream.Length);
+            }
+            if (contentType != "application/html")
+                await Windows.System.Launcher.LaunchFileAsync(outFile);
+        }
+        else
+        {
+            StorageFile storageFile = null;
+            FileSavePicker savePicker = new FileSavePicker();
+            savePicker.SuggestedStartLocation = PickerLocationId.Desktop;
+            savePicker.SuggestedFileName = filename;
+            switch (contentType)
+            {
+                case "application/vnd.openxmlformats-officedocument.presentationml.presentation":
+                    savePicker.FileTypeChoices.Add("PowerPoint Presentation", new List<string>() { ".pptx", });
+                    break;
+
+                case "application/msexcel":
+                    savePicker.FileTypeChoices.Add("Excel Files", new List<string>() { ".xlsx", });
+                    break;
+
+                case "application/msword":
+                    savePicker.FileTypeChoices.Add("Word Document", new List<string>() { ".docx" });
+                    break;
+
+                case "application/pdf":
+                    savePicker.FileTypeChoices.Add("Adobe PDF Document", new List<string>() { ".pdf" });
+                    break;
+                case "application/html":
+                    savePicker.FileTypeChoices.Add("HTML Files", new List<string>() { ".html" });
+                    break;
+            }
+            storageFile = await savePicker.PickSaveFileAsync();
+
+            using (Stream outStream = await storageFile.OpenStreamForWriteAsync())
+            {
+                outStream.Write(stream.ToArray(), 0, (int)stream.Length);
+            }
+
+            //Invoke the saved file for Viewing.
+            await Windows.System.Launcher.LaunchFileAsync(storageFile);
+        }
+    }
+}
---
+++ Word-to-PDF-Conversion/Convert-Word-document-to-PDF/Xamarin/Convert-Word-Document-to-PDF/Convert-Word-Document-to-PDF/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.Reflection;
+using System.IO;
+using Syncfusion.DocIORenderer;
+using Syncfusion.Pdf;
+
+namespace Convert_Word_Document_to_PDF
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            //Loading an existing Word document
+            Assembly assembly = typeof(App).GetTypeInfo().Assembly;
+            Stream streams = assembly.GetManifestResourceStream("Convert-Word-Document-to-PDF.Template.Input.docx");
+            using (WordDocument document = new WordDocument(assembly.GetManifestResourceStream("Convert-Word-Document-to-PDF.Template.Input.docx"), FormatType.Docx))
+            {
+                //Instantiation of DocIORenderer for Word to PDF conversion
+                using (DocIORenderer render = new DocIORenderer())
+                {
+                    //Converts Word document into PDF document
+                    using (PdfDocument pdfDocument = render.ConvertToPDF(document))
+                    {
+                        //Saves the Word document to MemoryStream.
+                        MemoryStream stream = new MemoryStream();
+                        pdfDocument.Save(stream);
+
+                        //Save the stream as a file in the device and invoke it for viewing.
+                        Xamarin.Forms.DependencyService.Get<ISave>().SaveAndView("Sample.pdf", "application/pdf", stream);
+                    }
+                }               
+            }                         
+        }       
+    }
+}
