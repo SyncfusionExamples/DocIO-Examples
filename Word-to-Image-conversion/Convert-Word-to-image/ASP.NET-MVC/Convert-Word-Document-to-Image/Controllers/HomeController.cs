206164db94cb6a94e4b2088629880498fdb24cf0
AzureAD\VenkateshMurugananda <venkatesh.m@syncfusion.com>
2023-06-09 17:51:39 +0530

    Sample Added

---
+++ Word-to-Image-conversion/Convert-Word-to-image/ASP.NET-MVC/Convert-Word-Document-to-Image/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIO;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Convert_Word_Document_to_Image.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void ConvertWordtoImage()
+        {
+            //Open the file as Stream
+            using (FileStream docStream = new FileStream(Server.MapPath("~/App_Data/Input.docx"), FileMode.Open, FileAccess.Read))
+            {
+                //Loads file stream into Word document
+                using (WordDocument wordDocument = new WordDocument(docStream, FormatType.Docx))
+                {                 
+                    //Convert the first page of the Word document into an image.
+                    Image image = wordDocument.RenderAsImages(0, ImageType.Bitmap);
+                    //Save the image as jpeg.           
+                    ExportAsImage(image, "wordtoimage.jpeg", ImageFormat.Jpeg, HttpContext.ApplicationInstance.Response);
+                }
+            }
+        }
+        //To download the image file
+        protected void ExportAsImage(Image image, string fileName, ImageFormat imageFormat, HttpResponse response)
+        {
+            if (ControllerContext == null)
+                throw new ArgumentNullException("Context");
+            string disposition = "content-disposition";
+            response.AddHeader(disposition, "attachment; filename=" + fileName);
+            if (imageFormat != ImageFormat.Emf)
+                image.Save(Response.OutputStream, imageFormat);
+            Response.End();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Word-to-Image-conversion/Convert-Word-to-image/ASP.NET-MVC/Convert-Word-Document-to-Image/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Convert_Word_Document_to_Image")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Convert_Word_Document_to_Image")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d99323e1-29e3-4874-8d05-f1b037ee202f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
