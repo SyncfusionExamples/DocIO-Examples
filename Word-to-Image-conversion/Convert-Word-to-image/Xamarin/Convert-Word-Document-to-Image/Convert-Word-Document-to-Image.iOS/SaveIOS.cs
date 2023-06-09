b1910fd1d9d9039e1dacae284f1aa434fcd9a149
AzureAD\VenkateshMurugananda <venkatesh.m@syncfusion.com>
2023-06-09 17:11:42 +0530

    Xamarin sample

---
+++ Word-to-Image-conversion/Convert-Word-to-image/Xamarin/Convert-Word-Document-to-Image/Convert-Word-Document-to-Image.iOS/SaveIOS.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+using Xamarin.Forms;
+using Convert_Word_Document_to_Image.iOS;
+using UIKit;
+using QuickLook;
+
+[assembly: Dependency(typeof(SaveIOS))]
+
+class SaveIOS : ISave
+{
+    //Method to save document as a file and view the saved document
+    public async Task SaveAndView(string filename, string contentType, MemoryStream stream)
+    {
+        //Get the root path in iOS device.
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        string filePath = Path.Combine(path, filename);
+
+        //Create a file and write the stream into it.
+        FileStream fileStream = File.Open(filePath, FileMode.Create);
+        stream.Position = 0;
+        stream.CopyTo(fileStream);
+        fileStream.Flush();
+        fileStream.Close();
+
+        //Invoke the saved document for viewing
+        UIViewController currentController = UIApplication.SharedApplication.KeyWindow.RootViewController;
+        while (currentController.PresentedViewController != null)
+            currentController = currentController.PresentedViewController;
+        UIView currentView = currentController.View;
+
+        QLPreviewController qlPreview = new QLPreviewController();
+        QLPreviewItem item = new QLPreviewItemBundle(filename, filePath);
+        qlPreview.DataSource = new PreviewControllerDS(item);
+
+        currentController.PresentViewController(qlPreview, true, null);
+    }
+}
