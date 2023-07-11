714b4f031885263f272463da84a7d2a4c4494172
AzureAD\VenkateshMurugananda <venkatesh.m@syncfusion.com>
2023-07-11 13:43:46 +0530

    Sample Removed

--- Getting-Started/Azure/Azure_App_Service/Create-Word-Document/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Create_Word_Document.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
