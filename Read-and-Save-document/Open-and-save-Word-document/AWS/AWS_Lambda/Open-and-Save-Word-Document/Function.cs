d5331d241254ef5d571e84945c49bcd4307b7503
AzureAD\VenkateshMurugananda <venkatesh.m@syncfusion.com>
2023-07-12 13:37:07 +0530

    Nuget added

--- Read-and-Save-document/Open-and-save-Word-document/AWS/AWS_Lambda/Open-and-Save-Word-Document/Function.cs
+++ Read-and-Save-document/Open-and-save-Word-document/AWS/AWS_Lambda/Open-and-Save-Word-Document/Function.cs
@@ -20,21 +20,21 @@ public class Function
     public string FunctionHandler(string input, ILambdaContext context)
     {
         string filePath = Path.GetFullPath(@"Data/Input.docx");
-        //Load the file from the disk
+        //Load the file from the disk.
         using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
         {
             using (WordDocument document = new WordDocument(fileStream, FormatType.Docx))
             {
-                //Access the section in a Word document
+                //Access the section in a Word document.
                 IWSection section = document.Sections[0];
-                //Add new paragraph to the section
+                //Add new paragraph to the section.
                 IWParagraph paragraph = section.AddParagraph();
                 paragraph.ParagraphFormat.FirstLineIndent = 36;
                 paragraph.BreakCharacterFormat.FontSize = 12f;
-                //Add new text to the paragraph
+                //Add new text to the paragraph.
                 IWTextRange textRange = paragraph.AppendText("In 2000, AdventureWorks Cycles bought a small manufacturing plant, Importadores Neptuno, located in Mexico. Importadores Neptuno manufactures several critical subcomponents for the AdventureWorks Cycles product line. These subcomponents are shipped to the Bothell location for final product assembly. In 2001, Importadores Neptuno, became the sole manufacturer and distributor of the touring bicycle product group.") as IWTextRange;
                 textRange.CharacterFormat.FontSize = 12f;
-                //Save the Word document into stream
+                //Save the Word document.
                 MemoryStream stream = new MemoryStream();
                 document.Save(stream,FormatType.Docx);
                 return Convert.ToBase64String(stream.ToArray());
