32b94933592ff9831989750faab279591d17420e
AzureAD\VenkateshMurugananda <venkatesh.m@syncfusion.com>
2023-07-12 12:49:03 +0530

    Sample Added

---
+++ Read-and-Save-document/Open-and-save-Word-document/Azure/Azure_Functions/Azure_Functions_v1/Open-and-Save-Word-Document/Function1.cs
@@ -0,0 +1,55 @@
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+
+namespace Open_and_Save_Word_Document
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            //Gets the input Word document as stream from request
+            Stream stream = req.Content.ReadAsStreamAsync().Result;
+            //Loads an existing Word document
+            using (WordDocument document = new WordDocument(stream))
+            {
+                //Access the section in a Word document.
+                IWSection section = document.Sections[0];
+                //Add a new paragraph to the section.
+                IWParagraph paragraph = section.AddParagraph();
+                paragraph.ParagraphFormat.FirstLineIndent = 36;
+                paragraph.BreakCharacterFormat.FontSize = 12f;
+                IWTextRange text = paragraph.AppendText("In 2000, Adventure Works Cycles bought a small manufacturing plant, Importadores Neptuno, located in Mexico. Importadores Neptuno manufactures several critical subcomponents for the Adventure Works Cycles product line. These subcomponents are shipped to the Bothell location for final product assembly. In 2001, Importadores Neptuno, became the sole manufacturer and distributor of the touring bicycle product group.");
+                text.CharacterFormat.FontSize = 12f;
+
+                MemoryStream memoryStream = new MemoryStream();
+                //Saves the Word document file.
+                document.Save(memoryStream, FormatType.Docx);
+                //Reset the memory stream position.
+                memoryStream.Position = 0;
+                //Create the response to return.
+                HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+                //Set the Word document saved stream as content of response.
+                response.Content = new ByteArrayContent(memoryStream.ToArray());
+                //Set the contentDisposition as attachment.
+                response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+                {
+                    FileName = "Sample.docx"
+                };
+                //Set the content type as Word document mime type.
+                response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/docx");
+                //Return the response with output Word document stream.
+                return response;
+            }
+        }
+    }
+}
