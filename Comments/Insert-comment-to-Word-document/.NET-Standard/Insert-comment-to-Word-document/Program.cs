5c3410597e48c9ef54c362f2be9c96c5f5ebe799
AkashArul26 <95226047+AkashArul26@users.noreply.github.com>
2023-08-09 16:07:41 +0530

    Committing Insert-comment-to-Word-document sample

---
+++ Comments/Insert-comment-to-Word-document/.NET-Standard/Insert-comment-to-Word-document/Program.cs
@@ -0,0 +1,57 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Insert_comment_to_Word_document
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fileStream = new FileStream(Path.GetFullPath(@"../../../Data/Input.docx"), FileMode.Open, FileAccess.ReadWrite))
+            {
+                //Open the existing Word document.
+                using (WordDocument document = new WordDocument(fileStream, FormatType.Docx))
+                {
+                    //Find all occurrence of a particular text end with comma in the document using regex.
+                    TextSelection[] textSelection = document.FindAll(new Regex("\\w+,"));
+                    if (textSelection != null)
+                    {
+                        //Iterates through each occurrence and comment it.
+                        for (int i = 0; i < textSelection.Count(); i++)
+                        {
+                            //Get the found text as single text range.
+                            WTextRange textRange = textSelection[i].GetAsOneRange();
+                            //Get the owner paragraph of the found text.
+                            WParagraph paragraph = textRange.OwnerParagraph;
+                            //Get the index of the found text.
+                            int textIndex = paragraph.ChildEntities.IndexOf(textRange);
+                            //Adds comment to a paragraph.
+                            WComment comment = paragraph.AppendComment("comment test_" + i);
+                            //Specify the author of the comment.
+                            comment.Format.User = "Peter";
+                            //Specify the initial of the author.
+                            comment.Format.UserInitials = "St";
+                            //Set the date and time for comment.
+                            comment.Format.DateTime = DateTime.Now;
+                            //Insert the comment next to the textrange.
+                            paragraph.ChildEntities.Insert(textIndex + 1, comment);
+                            //Add the paragraph items to the commented items.
+                            comment.AddCommentedItem(textRange);
+                        }
+                    }
+                    //Creates file stream.
+                    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Result.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        //Saves the Word document to file stream.
+                        document.Save(outputFileStream, FormatType.Docx);
+                    }
+                }
+            }
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(Path.GetFullPath(@"../../../Result.docx")) { UseShellExecute = true });
+        }
+    }
+}
