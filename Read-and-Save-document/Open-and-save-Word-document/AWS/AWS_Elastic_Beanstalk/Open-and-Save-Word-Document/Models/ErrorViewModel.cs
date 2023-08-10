60b9285c7f23aa60b363ccc2ebaf16e060d33981
Venkateshmuruganandam <venkatesh.m@syncfusion.com>
2023-08-10 14:23:05 +0530

    remove

--- Read-and-Save-document/Open-and-save-Word-document/AWS/AWS_Elastic_Beanstalk/Open-and-Save-Word-Document/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Open_and_Save_Word_Document.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
