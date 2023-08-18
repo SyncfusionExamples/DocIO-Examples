2b2863d3cbf822adc1147b84565789a6557b3b9c
Venkateshmuruganandam <venkatesh.m@syncfusion.com>
2023-08-18 12:09:15 +0530

    AWS Elastic Beanstalk

---
+++ Word-to-Image-conversion/Convert-Word-to-image/AWS/AWS_Elastic_Beanstalk/Convert-Word-Document-to-Image/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Convert_Word_Document_to_Image.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
