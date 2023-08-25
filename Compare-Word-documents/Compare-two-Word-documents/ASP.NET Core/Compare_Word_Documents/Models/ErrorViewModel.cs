e37b6d6f37601b7e6295119d813e85c04d5e4690
Venkateshmuruganandam <venkatesh.m@syncfusion.com>
2023-08-25 15:26:14 +0530

    Removed

--- Compare-Word-documents/Compare-two-Word-documents/ASP.NET Core/Compare_Word_Documents/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Compare_Word_Documents.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
