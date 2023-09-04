a8ca90879eea8afeb80c27c344b9e23bcaceafc0
Venkateshmuruganandam <venkatesh.m@syncfusion.com>
2023-09-04 16:21:52 +0530

    Sample added

---
+++ Compare-Word-documents/Compare-two-Word-documents/.NET/Compare-Word-documents/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Open the Original file as Stream.
+            using (FileStream originalDocStream = new FileStream(Path.GetFullPath(@"../../../Data/OriginalDocument.docx"), FileMode.Open, FileAccess.Read))
+            {
+                //Open the Revised file as Stream
+                using (FileStream revisedDocStream = new FileStream(Path.GetFullPath(@"../../../Data/RevisedDocument.docx"), FileMode.Open, FileAccess.Read))
+                {
+                    //Loads Original file stream into Word document.
+                    using (WordDocument originalWordDocument = new WordDocument(originalDocStream, FormatType.Docx))
+                    {
+                        //Loads Revised file stream into Word document.
+                        using (WordDocument revisedWordDocument = new WordDocument(revisedDocStream, FormatType.Docx))
+                        {
+                            // Create a memory stream to store the comparison result.
+                            MemoryStream stream = new MemoryStream();
+
+                            // Compare the original and revised Word documents.
+                            originalWordDocument.Compare(revisedWordDocument);
+
+                            //Save the stream as file.
+                            using (FileStream fileStreamOutput = File.Create("Result.docx"))
+                            {
+                                stream.CopyTo(fileStreamOutput);
+                            }
+                        }                      
+                    }
+                }              
+            }
+        }       
+    }
+}
---
+++ Compare-Word-documents/Compare-two-Word-documents/ASP.NET Core/Compare_Word_Documents/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Compare_Word_Documents.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.IO;
+
+namespace Compare_Word_Documents.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CompareWordDocuments()
+        {
+            string originalFilePath = Path.GetFullPath("Data/OriginalDocument.docx");
+            string revisedFilePath = Path.GetFullPath("Data/RevisedDocument.docx");
+
+            using (FileStream orgDocStream = new FileStream(originalFilePath, FileMode.Open, FileAccess.Read))
+            using (FileStream revisedStream = new FileStream(revisedFilePath, FileMode.Open, FileAccess.Read))
+            //Open the original Word document
+            using (WordDocument originalDocument = new WordDocument(orgDocStream, FormatType.Docx))
+            //Open the revised Word document
+            using (WordDocument revisedDocument = new WordDocument(revisedStream, FormatType.Docx))
+            {
+                //Compare original document with revised document
+                originalDocument.Compare(revisedDocument, "Andrew Fuller", DateTime.Now);
+                // Create a memory stream to store the comparison result.
+                MemoryStream stream = new MemoryStream();
+
+                // Save the compared document into the MemoryStream.
+                originalDocument.Save(stream, FormatType.Docx);
+
+                //Download Word document in the browser.
+                return File(stream, "application/docx", "Result.docx");
+           
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
