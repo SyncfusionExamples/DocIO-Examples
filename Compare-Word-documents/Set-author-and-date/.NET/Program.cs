7fd38804cafa90b1df96be58a32bb5a54d896436
Venkateshmuruganandam <venkatesh.m@syncfusion.com>
2023-09-05 11:37:43 +0530

    sample added

---
+++ Compare-Word-documents/Set-author-and-date/.NET/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+
+namespace Set_author_and_date
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Loads the original document.
+            using (FileStream originalDocumentStreamPath = new FileStream(Path.GetFullPath(@"../../../Data/OriginalDocument.docx"), FileMode.Open, FileAccess.Read))
+            {
+                using (WordDocument originalDocument = new WordDocument(originalDocumentStreamPath, FormatType.Docx))
+                {
+                    //Loads the revised document
+                    using (FileStream revisedDocumentStreamPath = new FileStream(Path.GetFullPath(@"../../../Data/RevisedDocument.docx"), FileMode.Open, FileAccess.Read))
+                    {
+                        using (WordDocument revisedDocument = new WordDocument(revisedDocumentStreamPath, FormatType.Docx))
+                        {
+                            // Compare the original and revised Word documents.
+                            originalDocument.Compare(revisedDocument, "Nancy Davolio", DateTime.Now.AddDays(-1));
+
+                            //Saves the Word document to MemoryStream
+                            MemoryStream stream = new MemoryStream();
+                            originalDocument.Save(stream, FormatType.Docx);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
