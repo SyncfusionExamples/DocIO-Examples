b1fbade8d7d44d0718ea6cd4be18f6cfbda8afcc
Lokesh Baskar <lokesh.baskar@syncfusion.com>
2023-12-13 13:22:28 +0530

    Addressed the feedback in comments and sample name

--- Word-to-Image-conversion/Fallback-fonts-for-Unicode-range/.NET/Fallback-fonts-for-Unicode-range/Program.cs
+++ Word-to-Image-conversion/Fallback-fonts-for-Unicode-range/.NET/Fallback-fonts-for-Unicode-range/Program.cs
@@ -2,7 +2,7 @@
 using Syncfusion.DocIORenderer;
 using Syncfusion.Office;
 
-namespace Fallback_fonts_for_custom_range
+namespace Fallback_fonts_for_Unicode_range
 {
     internal class Program
     {
@@ -12,19 +12,19 @@ namespace Fallback_fonts_for_custom_range
             using FileStream inputStream = new FileStream(@"../../../Template.docx", FileMode.Open, FileAccess.Read);
             //Loads an existing Word document file stream.
             using WordDocument wordDocument = new WordDocument(inputStream, Syncfusion.DocIO.FormatType.Docx);
-            //Adds fallback font for "Arabic" custom unicode range.
+            //Adds fallback font for "Arabic" specific unicode range.
             wordDocument.FontSettings.FallbackFonts.Add(new FallbackFont(0x0600, 0x06ff, "Arial"));
-            //Adds fallback font for "Hebrew" custom unicode range.
+            //Adds fallback font for "Hebrew" specific unicode range.
             wordDocument.FontSettings.FallbackFonts.Add(new FallbackFont(0x0590, 0x05ff, "Times New Roman"));
-            //Adds fallback font for "Hindi" custom unicode range.
+            //Adds fallback font for "Hindi" specific unicode range.
             wordDocument.FontSettings.FallbackFonts.Add(new FallbackFont(0x0900, 0x097F, "Nirmala UI"));
-            //Adds fallback font for "Chinese" custom unicode range.
+            //Adds fallback font for "Chinese" specific unicode range.
             wordDocument.FontSettings.FallbackFonts.Add(new FallbackFont(0x4E00, 0x9FFF, "DengXian"));
-            //Adds fallback font for "Japanese" custom unicode range.
+            //Adds fallback font for "Japanese" specific unicode range.
             wordDocument.FontSettings.FallbackFonts.Add(new FallbackFont(0x3040, 0x309F, "MS Gothic"));
-            //Adds fallback font for "Thai" custom unicode range.
+            //Adds fallback font for "Thai" specific unicode range.
             wordDocument.FontSettings.FallbackFonts.Add(new FallbackFont(0x0E00, 0x0E7F, "Tahoma"));
-            //Adds fallback font for "Korean" custom unicode range.
+            //Adds fallback font for "Korean" specific unicode range.
             wordDocument.FontSettings.FallbackFonts.Add(new FallbackFont(0xAC00, 0xD7A3, "Malgun Gothic"));
             //Instantiation of DocIORenderer for Word to Image conversion.
             using DocIORenderer render = new DocIORenderer();
