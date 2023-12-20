2992e919d9602b14adb4969ecfa27832c03498eb
AzureAD\VenkateshMurugananda <venkatesh.m@syncfusion.com>
2023-12-20 14:20:46 +0530

    Sample added

---
+++ Mathematical-Equation/LaTeX-equations/Border-Box/.NET/Border-Box/Program.cs
@@ -0,0 +1,16 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+
+
+// Create a new Word document.
+using WordDocument document = new WordDocument();
+
+//Add one section and one paragraph to the document.
+document.EnsureMinimal();
+
+//Append an border box equation using LaTeX.
+document.LastParagraph.AppendMath(@"\boxed{x^2 + y^2 = z^2}");
+
+//Save the Word document.
+using (FileStream outputStream = new FileStream(Path.GetFullPath(@"../../../Result.docx"), FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite))
+document.Save(outputStream, FormatType.Docx);
