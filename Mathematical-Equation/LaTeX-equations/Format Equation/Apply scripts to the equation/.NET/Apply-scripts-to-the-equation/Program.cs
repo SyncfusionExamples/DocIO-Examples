1262ab5edf3f1f80adc839cb1f6cc65167dbc1f1
sneha.biju <sneha.biju@syncfusion.com>
2024-01-23 12:09:14 +0530

    Sample for format equation using LaTeX

---
+++ Mathematical-Equation/LaTeX-equations/Format Equation/Apply scripts to the equation/.NET/Apply-scripts-to-the-equation/Program.cs
@@ -0,0 +1,28 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+
+
+// Create a new Word document.
+using (WordDocument document = new WordDocument())
+{
+    //Add one section and one paragraph to the document.
+    document.EnsureMinimal();
+
+    //Append an accent equation with Double-Struck font using LaTeX.
+    document.LastParagraph.AppendMath(@"\dot{\mathbb{a}}");
+    //Append an accent equation with Fraktur font using LaTeX.
+    document.LastSection.AddParagraph().AppendMath(@"\dot{\mathfrak{a}}");
+    //Append an accent equation with Sans Serif font using LaTeX.
+    document.LastSection.AddParagraph().AppendMath(@"\dot{\mathsf{a}}");
+    //Append an accent equation with Script using LaTeX.
+    document.LastSection.AddParagraph().AppendMath(@"\dot{\mathcal{a}}");
+    //Append an accent equation with Script using LaTeX.
+    document.LastSection.AddParagraph().AppendMath(@"\dot{\mathscr{a}}");
+
+    //Save a Word document
+    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Result.docx"), FileMode.Create, FileAccess.ReadWrite))
+    {
+        //Saves the Word document to file stream.
+        document.Save(outputFileStream, FormatType.Docx);
+    }
+}
