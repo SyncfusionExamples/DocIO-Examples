6eeb01112abad451e9909210f3e5fc8b07fe0f31
AkashArul26 <95226047+AkashArul26@users.noreply.github.com>
2024-01-24 17:35:48 +0530

    Revert unwanted changes

--- Charts/Create-chart-from-scratch/.NET-Standard/Create-chart-from-scratch/Program.cs
+++ Charts/Create-chart-from-scratch/.NET-Standard/Create-chart-from-scratch/Program.cs
@@ -22,7 +22,9 @@ namespace Create_chart_from_scratch
                 //Sets chart type.
                 chart.ChartType = OfficeChartType.Pie;
                 //Sets chart title.
-                chart.ChartTitle = string.Empty;
+                chart.ChartTitle = "Best Selling Products";
+                chart.ChartTitleArea.FontName = "Calibri";
+                chart.ChartTitleArea.Size = 14;
                 //Sets data for chart.
                 chart.ChartData.SetValue(1, 1, "");
                 chart.ChartData.SetValue(1, 2, "Sales");
@@ -30,11 +32,34 @@ namespace Create_chart_from_scratch
                 chart.ChartData.SetValue(2, 2, 141.396);
                 chart.ChartData.SetValue(3, 1, "Stanley Hudson");
                 chart.ChartData.SetValue(3, 2, 80.368);
+                chart.ChartData.SetValue(4, 1, "Bernard Shah");
+                chart.ChartData.SetValue(4, 2, 71.155);
+                chart.ChartData.SetValue(5, 1, "Patricia Lincoln");
+                chart.ChartData.SetValue(5, 2, 47.234);
+                chart.ChartData.SetValue(6, 1, "Camembert Pierrot");
+                chart.ChartData.SetValue(6, 2, 46.825);
+                chart.ChartData.SetValue(7, 1, "Thomas Hardy");
+                chart.ChartData.SetValue(7, 2, 42.593);
+                chart.ChartData.SetValue(8, 1, "Hanna Moos");
+                chart.ChartData.SetValue(8, 2, 41.819);
+                chart.ChartData.SetValue(9, 1, "Alice Mutton");
+                chart.ChartData.SetValue(9, 2, 32.698);
+                chart.ChartData.SetValue(10, 1, "Christina Berglund");
+                chart.ChartData.SetValue(10, 2, 29.171);
+                chart.ChartData.SetValue(11, 1, "Elizabeth Lincoln");
+                chart.ChartData.SetValue(11, 2, 25.696);
                 //Creates a new chart series with the name “Sales”.
                 IOfficeChartSerie pieSeries = chart.Series.Add("Sales");
-                pieSeries.Values = chart.ChartData[2, 2, 3, 2];
+                pieSeries.Values = chart.ChartData[2, 2, 11, 2];
+                //Sets data label.
+                pieSeries.DataPoints.DefaultDataPoint.DataLabels.IsValue = true;
+                pieSeries.DataPoints.DefaultDataPoint.DataLabels.Position = OfficeDataLabelPosition.Outside;
+                //Sets background color.
+                chart.ChartArea.Fill.ForeColor = Syncfusion.Drawing.Color.FromArgb(242, 242, 242);
+                chart.PlotArea.Fill.ForeColor = Syncfusion.Drawing.Color.FromArgb(242, 242, 242);
+                chart.ChartArea.Border.LinePattern = OfficeChartLinePattern.None;
                 //Sets category labels.
-                chart.PrimaryCategoryAxis.CategoryLabels = chart.ChartData[2, 1, 3, 1];
+                chart.PrimaryCategoryAxis.CategoryLabels = chart.ChartData[2, 1, 11, 1];
                 //Creates file stream.
                 using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Result.docx"), FileMode.Create, FileAccess.ReadWrite))
                 {
