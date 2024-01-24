b797ade7b222693765ee7303a0a926266fb2dd3b
sneha.biju <sneha.biju@syncfusion.com>
2024-01-24 15:46:45 +0530

    Added sample for showing warning for unsupported items during Word to PDF

---
+++ Word-to-PDF-Conversion/Show-Warning-for-unsupported-elements/Show-Warning-for-unsupported-elements/Program.cs
@@ -0,0 +1,71 @@
+﻿using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIORenderer;
+using Syncfusion.Pdf;
+
+namespace Show_Warning_for_unsupported_elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fileStream = new FileStream(@"../../../Smart Art.docx", FileMode.Open))
+            {
+                //Loads an existing Word document.
+                using (WordDocument wordDocument = new WordDocument(fileStream, Syncfusion.DocIO.FormatType.Automatic))
+                {
+                    //Creates an instance of DocIORenderer.
+                    using (DocIORenderer renderer = new DocIORenderer())
+                    {
+                        renderer.Settings.Warning = new DocumentWarning();
+                        //Converts Word document into PDF document.
+                        using (PdfDocument pdfDocument = renderer.ConvertToPDF(wordDocument))
+                        {
+                            if (!renderer.IsCanceled)
+                            {     //Saves the PDF file
+                                FileStream outputFile = new FileStream("Output.pdf", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                                pdfDocument.Save(outputFile);
+                                outputFile.Dispose();
+
+                                PdfDocument.ClearFontCache();
+
+                                Console.WriteLine("Success");
+
+                                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                                process.StartInfo = new System.Diagnostics.ProcessStartInfo("Output.pdf")
+                                {
+                                    UseShellExecute = true
+                                };
+                                process.Start();
+                            }
+                            else
+                            {
+                                Console.WriteLine("The execution stops due to the input document contains SmartArt");
+                                Console.ReadKey();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// DocumentWarning class implements the IWarning interface
+    /// </summary>
+    /// <seealso cref="IWarning" />
+    public class DocumentWarning : IWarning
+    {
+        public bool ShowWarnings(List<WarningInfo> warningInfo)
+        {
+            bool isContinueConversion = true;
+            foreach (WarningInfo warning in warningInfo)
+            {
+                //Based on WarningType enumeration, you can do your manipulation.
+                //Skips the Word to PDF conversion by setting isContinueConversion value as false
+                if (warning.WarningType == WarningType.SmartArt)
+                    isContinueConversion = false;
+            }
+            return isContinueConversion;
+        }
+    }
+}
