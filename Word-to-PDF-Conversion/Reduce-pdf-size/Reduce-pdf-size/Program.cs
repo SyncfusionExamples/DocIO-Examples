80d8682bf4d9cf526e855e6e9924041171a56a54
sneha.biju <sneha.biju@syncfusion.com>
2024-02-05 20:10:18 +0530

    Added sample for reduce PDF size

---
+++ Word-to-PDF-Conversion/Reduce-pdf-size/Reduce-pdf-size/Program.cs
@@ -0,0 +1,36 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocToPDFConverter;
+using Syncfusion.Pdf;
+
+namespace Reduce_pdf_size
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WordDocument wordDocument = new WordDocument(@"../../Data/Template.docx", FormatType.Docx);
+            DocToPDFConverter converter = new DocToPDFConverter();
+
+            // Enable fast rendering
+            converter.Settings.EnableFastRendering = true;
+
+            // Adjust image quality and resolution
+            converter.Settings.ImageQuality = 50;
+            converter.Settings.ImageResolution = 640;
+
+            // Convert Word document to PDF
+            PdfDocument pdfDocument = converter.ConvertToPDF(wordDocument);
+
+            // Set PDF compression level
+            pdfDocument.Compression = PdfCompressionLevel.Best;
+
+            // Save the PDF document
+            pdfDocument.Save("../../Data/Output.pdf");
+
+            // Close document instances
+            wordDocument.Close();
+            pdfDocument.Close();
+        }
+    }
+}
