ff78f2bc9906809fa9efb772eb72f0a780d29092
AkashArul26 <95226047+AkashArul26@users.noreply.github.com>
2024-03-19 19:13:05 +0530

    Committing the Blazor sample

---
+++ Word-to-PDF-Conversion/Use-alternate-font-without-installing/Blazor/WASM-app/Use-alternate-font-without-installing/FileUtils.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.JSInterop;
+
+namespace Use_alternate_font_without_installing
+{
+    public static class FileUtils
+    {
+        public static ValueTask<object> SaveAs(this IJSRuntime js, string filename, byte[] data)
+           => js.InvokeAsync<object>(
+                "saveAsFile",
+                filename,
+                Convert.ToBase64String(data));
+    }
+}
