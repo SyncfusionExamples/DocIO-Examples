8434083eff2ab1209119d168d40338db6cbb1de4
AzureAD\AntoNihilSahayaRaj <antonihil.sahayaraj@syncfusion.com>
2024-04-02 14:43:33 +0530

    Committing the sample for Convert-Word-to-EPUB in WPF

---
+++ Word-to-EPUB-conversion/Convert-Word-to-EPUB/WPF/Convert-Word-to-EPUB/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Windows;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+
+namespace Convert_Word_to_EPUB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnWordToEPUB_Click(object sender, RoutedEventArgs e)
+        {
+            //Open an existing Word document.
+            using (WordDocument document = new WordDocument(Path.GetFullPath(@"../../../Data/Input.docx"), FormatType.Automatic))
+            {
+                //Exports the fonts used in the document
+                document.SaveOptions.EPubExportFont = true;
+                //Exports header and footer
+                document.SaveOptions.HtmlExportHeadersFooters = true;
+
+                //Save the Word document.
+                document.Save(@"../../../WordToEPUB.epub", FormatType.EPub);
+            }
+            MessageBox.Show("Word document generated successfully");
+        }
+    }
+}
