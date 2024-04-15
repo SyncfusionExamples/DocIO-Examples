89019e7a97f9e1ba7e3e98e9d542d540f1a2e237
AzureAD\AntoNihilSahayaRaj <antonihil.sahayaraj@syncfusion.com>
2024-04-15 17:11:29 +0530

    Added sample of the Find-and-replace-with-multiple-list-paragraphs

---
+++ Find-and-Replace/Find-and-replace-with-multiple-list-paragraphs/.NET/Find-and-replace-with-multiple-list-paragraphs/Program.cs
@@ -0,0 +1,70 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+
+//Open the file as Stream.
+using (FileStream destInput = new FileStream("../../../Data/Input.docx", FileMode.Open, FileAccess.Read))
+{
+    //Open an existing Word document.
+    using (WordDocument document = new WordDocument(destInput, FormatType.Docx))
+    {
+        for (int documentIndex = 1; documentIndex <= 2; documentIndex++)
+        {
+            //String to be found.
+            string findText = "<<Heading"+ documentIndex + "Items>>";
+            //Find the selection in the document.
+            TextSelection selection = document.Find(findText, false, false);
+            //Get the owner paragraph.
+            WParagraph ownerPara = selection.GetAsOneRange().OwnerParagraph;
+            //Open the file as Stream.
+            using (FileStream subDocumentStream = new FileStream("../../../Data/Heading"+ documentIndex + "Items.docx", FileMode.Open, FileAccess.Read))
+            {
+                //Open an sub Word document.
+                using (WordDocument subDocument = new WordDocument(subDocumentStream, FormatType.Docx))
+                {
+                    //Create a text body part to be replaced.
+                    TextBodyPart textBodyPart = CreateBodyPart(subDocument, ownerPara);
+                    //Replace the text with the created text body part.
+                    document.Replace(findText, textBodyPart, true, true);
+                }
+            }
+        }
+
+        //Save the Word document.
+        using (FileStream output = new FileStream("../../../Result.docx", FileMode.Create, FileAccess.Write))
+        {
+            document.Save(output, FormatType.Docx);
+        }
+    }
+}
+
+/// <summary>
+/// Create a text body based on paragraphs on the sub another existing document.
+/// </summary>
+/// <param name="subDocument">Document contains the paragraph which need to be replace</param>
+/// <param name="ownerPara">The paragraph that needs to be copied.</param>
+TextBodyPart CreateBodyPart(WordDocument subDocument, WParagraph ownerPara)
+{
+    //Clone the paragraph.
+    WParagraph paratoInsert = ownerPara.Clone() as WParagraph;
+    //Creates new text body part.
+    TextBodyPart bodyPart = new TextBodyPart(ownerPara.Document);
+    //Iterate the body items of the sub document.
+    for (int bodyItemIndex = 0; bodyItemIndex < subDocument.LastSection.Body.ChildEntities.Count; bodyItemIndex++)
+    {
+        if (subDocument.LastSection.Body.ChildEntities[bodyItemIndex] is WParagraph)
+        {
+            WParagraph paragraph = subDocument.LastSection.Body.ChildEntities[bodyItemIndex] as WParagraph;
+            //Clear the paragraph's items.
+            paratoInsert.ChildEntities.Clear();
+            //Iterate the items of the paragraph.
+            for (int paraItemIndex = 0; paraItemIndex < paragraph.ChildEntities.Count; paraItemIndex++)
+            {
+                //Add the paragraph items.
+                paratoInsert.ChildEntities.Add(paragraph.ChildEntities[paraItemIndex].Clone());
+            }
+            //Add to text body part.
+            bodyPart.BodyItems.Add(paratoInsert.Clone());
+        }
+    }
+    return bodyPart;
+}
