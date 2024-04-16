3115357b4d8b16902d0c7ac9f296d89718508600
sneha.biju <sneha.biju@syncfusion.com>
2024-04-16 18:31:56 +0530

    Extract images from table sample

---
+++ Tables/Extract-images-from-table/.NET Standard/Extract-images-from-table/Program.cs
@@ -0,0 +1,13 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+
+
+//Creates an instance of WordDocument class
+FileStream fileStreamPath = new FileStream("../../../Input.docx", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+WordDocument document = new WordDocument(fileStreamPath, FormatType.Docx);
+
+//Get the list of pictures
+List<Entity> entities = document.FindAllItemsByProperty(EntityType.Picture, "OwnerParagraph.IsInCell", "True");
+
+//Closes the document
+document.Close();
