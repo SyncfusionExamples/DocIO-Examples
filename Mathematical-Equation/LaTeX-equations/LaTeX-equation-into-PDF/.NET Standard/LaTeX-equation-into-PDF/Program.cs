0b1f0d20ec10e6a9030a0eb1d67ee6b4656f9418
sneha.biju <sneha.biju@syncfusion.com>
2024-05-17 15:10:24 +0530

    Sample to create equation using LaTeX and convert it to PDF

---
+++ Mathematical-Equation/LaTeX-equations/LaTeX-equation-into-PDF/.NET Standard/LaTeX-equation-into-PDF/Program.cs
@@ -0,0 +1,31 @@
+﻿using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIORenderer;
+using Syncfusion.Pdf;
+
+// Create a new Word document.
+using (WordDocument document = new WordDocument())
+{
+    //Add one section and one paragraph to the document.
+    document.EnsureMinimal();
+
+    //Append an accent equation using LaTeX.
+    document.LastParagraph.AppendMath(@"f\left(x\right)={a}_{0}+\sum_{n=1}^{\infty}{\left({a}_{n}\cos{\frac{n\pi{x}}{L}}+{b}_{n}\sin{\frac{n\pi{x}}{L}}\right)}");
+    
+    //Instantiation of DocIORenderer for Word to PDF conversion
+    DocIORenderer render = new DocIORenderer();
+    //render.Settings.AutoDetectComplexScript = true;
+    //Converts Word document into PDF document
+    PdfDocument pdfDocument = render.ConvertToPDF(document);
+    //Releases all resources used by the Word document and DocIO Renderer objects
+    render.Dispose();
+    document.Dispose();
+    //Save the Word document.
+    using (FileStream outputStream = new FileStream(Path.GetFullPath(@"../../../Result.pdf"), FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite))
+    {
+        pdfDocument.Save(outputStream);
+    }
+    //Close the Word document
+    document.Close();
+    //Closes the instance of PDF document object
+    pdfDocument.Close();
+}
