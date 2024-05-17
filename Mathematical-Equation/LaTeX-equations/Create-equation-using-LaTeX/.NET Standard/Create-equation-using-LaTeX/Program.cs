b1e039ce30f092b8f81cca7defbecd582fc4d2a0
sneha.biju <sneha.biju@syncfusion.com>
2024-05-17 12:45:28 +0530

    Sample to insert an equation using LaTeX in a Word document

--- Mathematical-Equation/LaTeX-equations/Create-equation-using-LaTeX/.NET Standard/Create-equation-using-LaTeX/Program.cs
+++ Mathematical-Equation/LaTeX-equations/Create-equation-using-LaTeX/.NET Standard/Create-equation-using-LaTeX/Program.cs
@@ -8,7 +8,7 @@ using (WordDocument document = new WordDocument())
     //Add one section and one paragraph to the document.
     document.EnsureMinimal();
 
-    //Append an accent equation using LaTeX.
+    //Append an Fourier series equation using LaTeX.
     document.LastParagraph.AppendMath(@"f\left(x\right)={a}_{0}+\sum_{n=1}^{\infty}{\left({a}_{n}\cos{\frac{n\pi{x}}{L}}+{b}_{n}\sin{\frac{n\pi{x}}{L}}\right)}");
 
     //Save the Word document.
