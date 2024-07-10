dcbe95f625f655bd37493939799abc43c9d88c81
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2024-07-10 16:32:13 +0530

    Extract comments

---
+++ Comments/Extract-comments/.NET/Extract-comments/Program.cs
@@ -0,0 +1,26 @@
+﻿using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIO;
+
+namespace Extract_comments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Open an existing document
+            using (FileStream inputStream = new FileStream(@"../../../Data/Template.docx", FileMode.Open, FileAccess.Read))
+            {
+                using (WordDocument document = new WordDocument(inputStream, FormatType.Docx))
+                {
+                    //Iterate through the comments in the Word document
+                    foreach (WComment comment in document.Comments)
+                    {
+                        //Traverse each paragraph from the comment body items
+                        foreach (WParagraph paragraph in comment.TextBody.Paragraphs)
+                            Console.WriteLine(paragraph.Text);
+                    }
+                }
+            }
+        }
+    }
+}
