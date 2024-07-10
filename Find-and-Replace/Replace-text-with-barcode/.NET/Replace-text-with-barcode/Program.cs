1749982a463d6cf281b57d37ae1e86ac3a06df0d
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2024-07-10 16:37:35 +0530

    Replace text with barcode

---
+++ Find-and-Replace/Replace-text-with-barcode/.NET/Replace-text-with-barcode/Program.cs
@@ -0,0 +1,35 @@
+﻿using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIO;
+
+namespace Replace_text_with_barcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Open an existing document
+            using (FileStream inputStream = new FileStream(@"../../../Data/Template.docx", FileMode.Open, FileAccess.Read))
+            {
+                using (WordDocument document = new WordDocument(inputStream, FormatType.Docx))
+                {
+                    //Find all instances of the target word in the document
+                    TextSelection[] textSelections = document.FindAll("Barcode", false, true);
+
+                    foreach (TextSelection selection in textSelections)
+                    {
+                        //Apply the font style
+                        selection.GetAsOneRange().CharacterFormat.FontName = "Code 128";
+                        selection.GetAsOneRange().CharacterFormat.Bold = true;
+                        selection.GetAsOneRange().CharacterFormat.Italic = true;
+                    }
+
+                    //Save the Word document
+                    using (FileStream outputStream = new FileStream(@"../../../Output.docx", FileMode.Create, FileAccess.Write))
+                    {
+                        document.Save(outputStream, FormatType.Docx);
+                    }
+                }
+            }
+        }
+    }
+}
