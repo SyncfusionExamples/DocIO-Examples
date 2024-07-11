ca386ed9ebb1663cefcbd4654c5a19df95d72d4e
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2024-07-11 17:11:28 +0530

    Orientation

--- Sections/Set-different-orientation/.NET/Set-different-orientation/Program.cs
+++ Sections/Set-different-orientation/.NET/Set-different-orientation/Program.cs
@@ -1,30 +1,15 @@
 ﻿using Syncfusion.DocIO.DLS;
 using Syncfusion.DocIO;
 
-namespace Set_different_orientation
+//Open an existing document
+using (WordDocument document = new WordDocument(new FileStream(@"../../../Data/Template.docx", FileMode.Open, FileAccess.Read), FormatType.Docx))
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            //Open an existing document
-            using (FileStream inputStream = new FileStream(@"../../../Data/Template.docx", FileMode.Open, FileAccess.Read))
-            {
-                using (WordDocument document = new WordDocument(inputStream, FormatType.Docx))
-                {
-                    //Set different orientations
-                    document.Sections[0].PageSetup.Orientation = PageOrientation.Portrait;
-                    document.Sections[1].PageSetup.Orientation = PageOrientation.Landscape;
-                    document.Sections[2].PageSetup.Orientation = PageOrientation.Portrait;
-                    document.Sections[3].PageSetup.Orientation = PageOrientation.Landscape;
+    //Set different orientations
+    document.Sections[0].PageSetup.Orientation = PageOrientation.Portrait;
+    document.Sections[1].PageSetup.Orientation = PageOrientation.Landscape;
+    document.Sections[2].PageSetup.Orientation = PageOrientation.Portrait;
+    document.Sections[3].PageSetup.Orientation = PageOrientation.Landscape;
 
-                    //Save the Word document
-                    using (FileStream outputStream = new FileStream(@"../../../Output.docx", FileMode.Create, FileAccess.Write))
-                    {
-                        document.Save(outputStream, FormatType.Docx);
-                    }
-                }
-            }
-        }
-    }
+    //Save the Word document
+    document.Save(new FileStream(@"../../../Output.docx", FileMode.Create, FileAccess.Write), FormatType.Docx);
 }
