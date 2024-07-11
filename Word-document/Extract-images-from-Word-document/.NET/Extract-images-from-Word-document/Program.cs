f803ddd85f9dc9a44c6d26d641ac26482e3cee45
DharanyaSakthivel-SF4210 <Dharanya.sakthivel@syncfusion.com>
2024-07-11 17:38:43 +0530

    Extract-images-from-Word-document

--- Word-document/Extract-images-from-Word-document/.NET/Extract-images-from-Word-document/Program.cs
+++ Word-document/Extract-images-from-Word-document/.NET/Extract-images-from-Word-document/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Syncfusion.DocIO;
 using Syncfusion.DocIO.DLS;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Extract_images_from_Word_document
 {
@@ -7,60 +10,32 @@ namespace Extract_images_from_Word_document
     {
         static void Main(string[] args)
         {
-            //Open the file as Stream.
+            // Open the file as a stream.
             using (FileStream docStream = new FileStream(Path.GetFullPath(@"../../../Data/Template.docx"), FileMode.Open, FileAccess.Read))
             {
-                //Load the file stream into a Word document.
+                // Load the file stream into a Word document.
                 using (WordDocument document = new WordDocument(docStream, FormatType.Docx))
                 {
-                    int index = 0;
-                    //Visits all document entities.
-                    foreach (var item in document.Visit())
+                    // Find all pictures by EntityType in the Word document.
+                    List<Entity> pictures = document.FindAllItemsByProperty(EntityType.Picture, null, null);
+
+                    // Iterate through the pictures and save each one as an image file.
+                    for (int i = 0; i < pictures.Count; i++)
                     {
-                        switch (item.EntityType)
+                        WPicture image = pictures[i] as WPicture;
+
+                        // Use a MemoryStream to handle the image bytes from the picture.
+                        using (MemoryStream memoryStream = new MemoryStream(image.ImageBytes))
                         {
-                            case EntityType.Picture:
-                                WPicture picture = item as WPicture;
+                            // Define the path where the image will be saved.
+                            string imagePath = Path.GetFullPath(@"../../../Image-" + i + ".jpeg");
 
-                                // Use a MemoryStream to handle the image bytes from the picture
-                                using (MemoryStream memoryStream = new MemoryStream(picture.ImageBytes))
-                                {
-                                    // Define the path where the image will be saved
-                                    string imagePath = Path.GetFullPath(@"../../../Images/Image-" + index + ".jpeg");
-
-                                    // Create a FileStream to write the image to the specified path
-                                    using (FileStream image = new FileStream(imagePath, FileMode.Create, FileAccess.Write))
-                                    {
-                                        // Copy the content of the MemoryStream to the FileStream
-                                        memoryStream.CopyTo(image);
-                                    }
-                                }
-
-                                // Increment the index for the next image
-                                index++;
-                                break;
+                            // Create a FileStream to write the image to the specified path.
+                            using (FileStream filestream = new FileStream(imagePath, FileMode.Create, FileAccess.Write))
+                            {
+                                memoryStream.CopyTo(filestream);
+                            }
                         }
-
-                    }
-                }
-            }
-        }
-    }
-
-    public static class DocIOExtensions
-    {
-        public static IEnumerable<IEntity> Visit(this ICompositeEntity entity)
-        {
-            var entities = new Stack<IEntity>(new IEntity[] { entity });
-            while (entities.Count > 0)
-            {
-                var e = entities.Pop();
-                yield return e;
-                if (e is ICompositeEntity)
-                {
-                    foreach (IEntity childEntity in ((ICompositeEntity)e).ChildEntities)
-                    {
-                        entities.Push(childEntity);
                     }
                 }
             }
