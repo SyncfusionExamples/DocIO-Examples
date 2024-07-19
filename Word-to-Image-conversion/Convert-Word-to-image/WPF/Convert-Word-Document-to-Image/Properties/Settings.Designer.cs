ab46bea6f861777f69138d77a3c831f07f9da5ec
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2024-07-19 17:18:07 +0530

    Word to Image conversion

--- Word-to-Image-conversion/Convert-Word-to-image/WPF/Convert-Word-Document-to-Image/Properties/Settings.Designer.cs
+++ Word-to-Image-conversion/Convert-Word-to-image/WPF/Convert-Word-Document-to-Image/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Convert_Word_Document_to_Image.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
