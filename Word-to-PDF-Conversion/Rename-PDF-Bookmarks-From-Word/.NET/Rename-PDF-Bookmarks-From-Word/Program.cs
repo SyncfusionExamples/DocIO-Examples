c9a9a6efd879c3863a39074e6a8f617207dc46f2
DharanitharanA <97955635+DharanitharanA@users.noreply.github.com>
2024-07-19 18:09:23 +0530

    Added sample for Rename-PDF-Bookmarks-From-Word

---
+++ Word-to-PDF-Conversion/Rename-PDF-Bookmarks-From-Word/.NET/Rename-PDF-Bookmarks-From-Word/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using Syncfusion.DocIO;
+using System.Reflection;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIORenderer;
+using Syncfusion.OfficeChart;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Interactive;
+using Syncfusion.Pdf.Parsing;
+
+namespace Rename_PDF_Bookmarks_From_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fileStream = new FileStream(Path.GetFullPath(@"../../../Data/Template.docx"), FileMode.Open))
+            {
+                //Loads an existing Word document.
+                using (WordDocument wordDocument = new WordDocument(fileStream, Syncfusion.DocIO.FormatType.Automatic))
+                {
+                    //Creates an instance of DocIORenderer.
+                    using (DocIORenderer renderer = new DocIORenderer())
+                    {
+                        //Sets ExportBookmarks for preserving Word document headings as PDF bookmarks.
+                        renderer.Settings.ExportBookmarks = ExportBookmarkType.Bookmarks;
+                        //Converts Word document into PDF document
+                        using (PdfDocument pdfDocument = renderer.ConvertToPDF(wordDocument))
+                        {
+                            //Saves the PDF document to MemoryStream.
+                            using (MemoryStream stream = new MemoryStream())
+                            {
+                                pdfDocument.Save(stream);
+                                stream.Position = 0;
+                                //Load the PDF document.
+                                using (PdfLoadedDocument pdfLoadedDocument = new PdfLoadedDocument(stream))
+                                {
+                                    int i = 1;
+                                    //Get each bookmark and changes the title of the bookmark.
+                                    foreach (PdfBookmark pdfBookmark in pdfLoadedDocument.Bookmarks)
+                                    {
+                                        pdfBookmark.Title = "PdfBookMark" + i++;
+                                    }
+                                    //Saves the PDF file to file system.    
+                                    using (FileStream outputStream = new FileStream(Path.GetFullPath(@"../../../WordToPDF.pdf"), FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite))
+                                    {
+                                        pdfLoadedDocument.Save(outputStream);
+                                    }
+                                }
+                            }
+                        } 
+                    }
+                }
+            }
+        }
+    }
+}
