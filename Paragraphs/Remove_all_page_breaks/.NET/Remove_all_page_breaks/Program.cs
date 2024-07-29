06b99daf2d906a95aea7985ef95bec26428f50ed
Konduru Keerthi Konduru Ravichandra Raju <kondurukeerthik@syncfusion.com>
2024-07-29 18:17:21 +0530

    Paragraphs

---
+++ Paragraphs/Remove_all_page_breaks/.NET/Remove_all_page_breaks/Program.cs
@@ -0,0 +1,22 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+
+using (FileStream inputStream = new FileStream(@"../../../Data/Template.docx", FileMode.Open, FileAccess.Read))
+{
+    //Open the existing Word document.
+    using (WordDocument document = new WordDocument(inputStream, FormatType.Docx))
+    {
+        //Get the list of page breaks
+        List<Entity> entities = document.FindAllItemsByProperty(EntityType.Break, "BreakType", "PageBreak");
+
+        //Iterate through all page breaks and remove it from the owner paragraph
+        foreach (Entity entity in entities)
+            (entity as Break).OwnerParagraph.ChildEntities.Remove(entity);
+
+        //Save the Word document
+        using (FileStream outputStream = new FileStream(@"../../../Output.docx", FileMode.Create, FileAccess.Write))
+        {
+            document.Save(outputStream, FormatType.Docx);
+        }
+    }
+}
