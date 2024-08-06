465bb59184418043bb35407756ba1f6c3ec53de6
DharanitharanA <97955635+DharanitharanA@users.noreply.github.com>
2024-08-06 20:20:44 +0530

    Added ASP.NET Core and WindowsForms samples for Get-missing-fonts-for-PDF-conversion

---
+++ Word-to-PDF-Conversion/Get-missing-fonts-for-pdf-conversion/.NET-Framework/Get-missing-fonts-for-PDF-conversion/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Get_missing_fonts_for_PDF_conversion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.browseButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnconvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // browseButton
+            // 
+            this.browseButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.browseButton.Location = new System.Drawing.Point(360, 199);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(21, 21);
+            this.browseButton.TabIndex = 92;
+            this.browseButton.Text = ". . .";
+            this.browseButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 199);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(348, 29);
+            this.textBox1.TabIndex = 91;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label2.Location = new System.Drawing.Point(3, 178);
+            this.label2.Name = "label9";
+            this.label2.Size = new System.Drawing.Size(177, 20);
+            this.label2.TabIndex = 90;
+            this.label2.Text = "Source Document";
+            // 
+            // btnconvert
+            // 
+            this.btnconvert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnconvert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(158)))), ((int)(((byte)(218)))));
+            this.btnconvert.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnconvert.ForeColor = System.Drawing.Color.White;
+            this.btnconvert.Location = new System.Drawing.Point(281, 300);
+            this.btnconvert.Name = "btnconvert";
+            this.btnconvert.Size = new System.Drawing.Size(100, 22);
+            this.btnconvert.TabIndex = 96;
+            this.btnconvert.Text = "Convert";
+            this.btnconvert.UseVisualStyleBackColor = false;
+            this.btnconvert.Click += new System.EventHandler(this.btnconvert_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Verdana",10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(378, 72);
+            this.label1.TabIndex = 97;
+            this.label1.Text = "This sample will print whether the fonts in Word document are available in machine while converting Word document to PDF using .NET Framework Word library (DocIO) and .NET Framework PDF library (PDF).";
+
+            // 
+            // Form1
+            // 
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(400, 400);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnconvert);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Word to PDF";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnconvert;
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
+
