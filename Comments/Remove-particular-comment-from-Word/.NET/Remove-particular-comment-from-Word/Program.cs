a1739ae5eea88a88fda8905d4e3334237f403f00
VijayadharshiniMathiyalagan <vijayadharshini.m@syncfusion.com>
2024-08-21 12:12:08 +0530

    ES-893973 - docio playground changes for github examples

--- Comments/Remove-particular-comment-from-Word/.NET/Remove-particular-comment-from-Word/Program.cs
+++ Comments/Remove-particular-comment-from-Word/.NET/Remove-particular-comment-from-Word/Program.cs
@@ -8,7 +8,7 @@ namespace Remove_particular_comment_from_Word
     {
         static void Main(string[] args)
         {
-            using (FileStream fileStream = new FileStream(Path.GetFullPath(@"../../../Template.docx"), FileMode.Open, FileAccess.ReadWrite))
+            using (FileStream fileStream = new FileStream(Path.GetFullPath(@"Data/Template.docx"), FileMode.Open, FileAccess.ReadWrite))
             {
                 //Opens the template document.
                 using (WordDocument document = new WordDocument(fileStream, FormatType.Docx))
@@ -16,7 +16,7 @@ namespace Remove_particular_comment_from_Word
                     //Removes second comments from a document.
                     document.Comments.RemoveAt(1);
                     //Creates file stream.
-                    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Result.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output.docx"), FileMode.Create, FileAccess.ReadWrite))
                     {
                         //Saves the Word document to file stream.
                         document.Save(outputFileStream, FormatType.Docx);
