8bf2fb9abc8b87256f6c9805cc989ed2dae95d2b
DharanyaSakthivel-SF4210 <Dharanya.sakthivel@syncfusion.com>
2024-09-03 22:27:56 +0530

    Add-playground-in-sample

--- Table-Of-Contents/Update-table-of-contents/.NET/Update-table-of-contents/Program.cs
+++ Table-Of-Contents/Update-table-of-contents/.NET/Update-table-of-contents/Program.cs
@@ -9,7 +9,7 @@ namespace Update_table_of_contents
     {
         static void Main(string[] args)
         {
-            using (FileStream fileStreamPath = new FileStream(Path.GetFullPath(@"../../../Template.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (FileStream fileStreamPath = new FileStream(Path.GetFullPath(@"Data/Template.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             {
                 //Creates a new Word document.
                 using (WordDocument document = new WordDocument(fileStreamPath, FormatType.Automatic))
@@ -17,7 +17,7 @@ namespace Update_table_of_contents
                     //Updates the table of contents.
                     document.UpdateTableOfContents();
                     //Creates file stream.
-                    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Result.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Result.docx"), FileMode.Create, FileAccess.ReadWrite))
                     {
                         //Saves the Word document to file stream.
                         document.Save(outputFileStream, FormatType.Docx);
