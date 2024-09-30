78e117af97fdf2a857b3b27ef1d06d1359605e2b
VijayadharshiniMathiyalagan <vijayadharshini.m@syncfusion.com>
2024-09-30 11:22:02 +0530

    Added replace excel embedded image to png

---
+++ Word-to-PDF-Conversion/Replace-Excel-embedded-image-to-PNG/.NET/Replace-Excel-embedded-image-to-PNG/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Drawing;
+using Syncfusion.Pdf;
+using Syncfusion.DocIORenderer;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIO;
+using Syncfusion.Drawing;
+using System.Drawing.Imaging;
+using Syncfusion.XlsIO;
+using Syncfusion.XlsIORenderer;
+
+
+// Disable the PDF document cache to prevent caching of rendered pages
+PdfDocument.EnableCache = false;
+// Initialize the DocIORenderer component for converting Word documents to PDF
+using DocIORenderer docIORenderer = new DocIORenderer();
+// Create new DocIORenderer settings
+docIORenderer.Settings = new DocIORendererSettings();
+// Set the PDF conformance level to PDF/A-1B for archival standard compliance
+docIORenderer.Settings.PdfConformanceLevel = PdfConformanceLevel.Pdf_A1B;
+// Set the chart rendering scaling mode to normal to preserve the aspect ratio of charts
+docIORenderer.Settings.ChartRenderingOptions.ScalingMode = Syncfusion.OfficeChart.ScalingMode.Normal;
+// Open the input Word document from a file stream
+FileStream inputStream = new FileStream(Path.GetFullPath(@"Data/Input.docx"), FileMode.Open, FileAccess.Read);
+// Load the Word document into a WordDocument instance
+using var tempDocument = new WordDocument(inputStream, FormatType.Automatic);
+// Call a method to replace embedded Excel objects in the document with images
+ReplaceExcelToImage(tempDocument);
+// Convert the Word document to a PDF using the DocIORenderer component
+using PdfDocument pdf = docIORenderer.ConvertToPDF(tempDocument);
+// Create a file stream to save the output PDF document
+FileStream outputStream = new FileStream(Path.GetFullPath(@"Output/Output.pdf"), FileMode.Create, FileAccess.Write);
+// Save the generated PDF to the specified file stream
+pdf.Save(outputStream);
+
+
+void ReplaceExcelToImage(WordDocument wordDocument)
+{
+    //Get the Ole objects.
+    List<Entity> oleObjects = wordDocument.FindAllItemsByProperty(EntityType.OleObject, null, null);
+    //Iterate through the ole objects.
+    for (int i = 0; i < oleObjects.Count; i++)
+    {
+        WOleObject ole = oleObjects[i] as WOleObject;
+        //Check the type of OLE.
+        string type = ole.ObjectType;
+        //Get the height and width of OLE picture.
+        float height = ole.OlePicture.Height;
+        float width = ole.OlePicture.Width;
+        //If the type contains "Excel", then the OLE object is extracted from Excel.
+        if (type.Contains("Excel"))
+        {
+            //Create a Excel file using the Ole data.
+            MemoryStream excelStream = new MemoryStream();
+            excelStream.Write(ole.NativeData);
+            excelStream.Position = 0;
+
+            //Creates a new instance for ExcelEngine.
+            ExcelEngine excelEngine = new ExcelEngine();
+            //Initialize IApplication.
+            IApplication application = excelEngine.Excel;
+            //Loads or open an existing workbook through Open method of IWorkbooks.
+            IWorkbook workbook = application.Workbooks.Open(excelStream);
+            IWorksheet sheet = workbook.Worksheets[0];
+
+            //Initialize XlsIORenderer.
+            application.XlsIORenderer = new XlsIORenderer();
+
+            //Converts and save as stream.
+            MemoryStream imgStream = new MemoryStream();
+            sheet.ConvertToImage(1, 1, 6, 5, imgStream);
+            imgStream.Position = 0;
+
+            //Load the converted image as OLE picture.
+            ole.OlePicture.LoadImage(imgStream);
+            ole.OlePicture.LockAspectRatio = false;
+            ole.OlePicture.Height = height;
+            ole.OlePicture.Width = width;
+
+            //Close and Dispose.
+            workbook.Close();
+            imgStream.Dispose();
+            excelStream.Dispose();
+        }
+    }
+}
