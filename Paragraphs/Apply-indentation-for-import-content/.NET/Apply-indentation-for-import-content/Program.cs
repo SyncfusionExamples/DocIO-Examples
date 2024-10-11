42aa37c7b11d44231af214ffffb91bc88f746554
VijayadharshiniMathiyalagan <vijayadharshini.m@syncfusion.com>
2024-10-11 13:59:40 +0530

    Use proper naming of the variable. Use proper disposal by using statement. Use proper name for input documents.

--- Paragraphs/Apply-indentation-for-import-content/.NET/Apply-indentation-for-import-content/Program.cs
+++ Paragraphs/Apply-indentation-for-import-content/.NET/Apply-indentation-for-import-content/Program.cs
@@ -1,63 +1,71 @@
 ﻿using Syncfusion.DocIO;
 using Syncfusion.DocIO.DLS;
 
-//Open the existing main document.
-FileStream fileStream1 = new FileStream(Path.GetFullPath(@"Data/SourceDocument.docx"), FileMode.Open);
-WordDocument mainDocument = new WordDocument(fileStream1, FormatType.Docx);
-//Open the existing temporary document.
-FileStream fileStream = new FileStream(Path.GetFullPath(@"Data/DestinationDocument.docx"), FileMode.Open);
-WordDocument tempDoc = new WordDocument(fileStream, FormatType.Docx);
-//Set the first section break.
-tempDoc.Sections[0].BreakCode = SectionBreakCode.NoBreak;
+// Open the existing main document from a file stream.
+using (FileStream destinationStream = new FileStream(Path.GetFullPath(@"Data/DestinationDocument.docx"), FileMode.Open))
+{
+    // Load the main Word document.
+    WordDocument destinationDocument = new WordDocument(destinationStream, FormatType.Docx);
 
-//Get the last section index of main document.
-int secIndex = mainDocument.ChildEntities.IndexOf(mainDocument.LastSection);
-//Get the last paragraph index of main document.
-int paraIndex = mainDocument.LastSection.Body.ChildEntities.IndexOf(mainDocument.LastParagraph);
-//Get the last paragraph style.
-WParagraph lastPara = mainDocument.LastParagraph;
+    // Open the existing source document from a file stream.
+    using (FileStream sourceStream = new FileStream(Path.GetFullPath(@"Data/SourceDocument.docx"), FileMode.Open))
+    {
+        // Load the source Word document.
+        WordDocument sourceDocument = new WordDocument(sourceStream, FormatType.Docx);
 
-//Import the temporary document content to the main document.
-mainDocument.ImportContent(tempDoc, ImportOptions.UseDestinationStyles);
+        // Set the first section break in the source document to "NoBreak" for seamless merging.
+        sourceDocument.Sections[0].BreakCode = SectionBreakCode.NoBreak;
 
-//Modify the paragraph style for the added contents.
-AddLeftIndentation(mainDocument, secIndex, paraIndex + 1, lastPara.ParagraphFormat.LeftIndent);
+        // Get the index of the last section in the destination document.
+        int secIndex = destinationDocument.ChildEntities.IndexOf(destinationDocument.LastSection);
 
-//Save the main document.
-FileStream outputStream = new FileStream(Path.GetFullPath(@"Output/Output.docx"), FileMode.Create, FileAccess.Write);
-mainDocument.Save(outputStream, FormatType.Docx);
+        // Get the index of the last paragraph in the last section of the destination document.
+        int paraIndex = destinationDocument.LastSection.Body.ChildEntities.IndexOf(destinationDocument.LastParagraph);
 
-//Dispose the streams.
-fileStream1.Dispose();
-fileStream.Dispose();
+        // Get the style and formatting of the last paragraph for reference.
+        WParagraph lastPara = destinationDocument.LastParagraph;
+
+        // Import content from the source document into the destination document, using destination styles.
+        destinationDocument.ImportContent(sourceDocument, ImportOptions.UseDestinationStyles);
+
+        // Modify the paragraph style for the newly added contents by applying left indentation.
+        AddLeftIndentation(destinationDocument, secIndex, paraIndex + 1, lastPara.ParagraphFormat.LeftIndent);
+
+        // Save the updated destination document to a new file.
+        using (FileStream outputStream = new FileStream(Path.GetFullPath(@"Output/Output.docx"), FileMode.Create, FileAccess.Write))
+        {
+            destinationDocument.Save(outputStream, FormatType.Docx);
+        };
+    };
+};
 
 /// <summary>
 /// Applies left indentation to paragraphs and tables in a specified section and paragraph range of a Word document.
 /// </summary>
-void AddLeftIndentation(WordDocument document1, int secIndex, int paraIndex, float leftIndent)
+void AddLeftIndentation(WordDocument document, int secIndex, int paraIndex, float leftIndent)
 {
-    //Iterate through the sections added from the temporary document.
-    for (int i = secIndex; i < document1.ChildEntities.IndexOf(document1.LastSection) + 1; i++)
+    // Iterate through the sections added from the source document, starting from secIndex.
+    for (int i = secIndex; i < document.ChildEntities.IndexOf(document.LastSection) + 1; i++)
     {
-        //Iterate through the child entities added from the temporary document.
-        for (int j = paraIndex; j < document1.Sections[i].Body.ChildEntities.Count; j++)
+        // Iterate through the child entities (paragraphs/tables) added from the source document.
+        for (int j = paraIndex; j < document.Sections[i].Body.ChildEntities.Count; j++)
         {
-            //If the child entity is a paragraph then apply the previously taken para left indent.
-            if (document1.Sections[i].Body.ChildEntities[j] is WParagraph)
+            // If the child entity is a paragraph, apply the left indent from the last paragraph.
+            if (document.Sections[i].Body.ChildEntities[j] is WParagraph)
             {
-                WParagraph para = document1.Sections[i].Body.ChildEntities[j] as WParagraph;
-                //Set the left indentation
+                WParagraph para = document.Sections[i].Body.ChildEntities[j] as WParagraph;
+                // Set the left indentation for the paragraph
                 para.ParagraphFormat.LeftIndent = leftIndent;
             }
-            //If the child entity is a table then apply the previously taken para left indent.
-            else if (document1.Sections[i].Body.ChildEntities[j] is WTable)
+            // If the child entity is a table, apply the same left indent to the table.
+            else if (document.Sections[i].Body.ChildEntities[j] is WTable)
             {
-                WTable table = document1.Sections[i].Body.ChildEntities[j] as WTable;
-                //Set the left indentation
+                WTable table = document.Sections[i].Body.ChildEntities[j] as WTable;
+                // Set the left indentation for the table.
                 table.TableFormat.LeftIndent = leftIndent;
             }
         }
-        //Reset the index for next section.
+        // Reset the paragraph index for the next section to start from the beginning.
         paraIndex = 0;
     }
 }
