dcaacfb73be38c3b632f1915557a1544f755db5d
VijayadharshiniMathiyalagan <vijayadharshini.m@syncfusion.com>
2024-10-14 09:56:19 +0530

    Addressed the feedbacks

--- Bookmarks/Maintain-source-list-format-after-replace/.NET/Maintain-source-list-format-after-replace/Program.cs
+++ Bookmarks/Maintain-source-list-format-after-replace/.NET/Maintain-source-list-format-after-replace/Program.cs
@@ -7,27 +7,24 @@ namespace Replace_content_with_bookmark
     {
         static void Main(string[] args)
         {
-            // Open the source Word document for reading using a file stream.
-            using (FileStream sourceStream = new FileStream(Path.GetFullPath("Data/SourceDocument.docx"), FileMode.Open, FileAccess.Read))
+            using (FileStream destinationStream = new FileStream(Path.GetFullPath("Data/DestinationDocument.docx"), FileMode.Open, FileAccess.Read))
             {
-                // Load the source document from the stream.
-                using (WordDocument sourceDocument = new WordDocument(sourceStream, FormatType.Docx))
+                //Open the destination Word document.
+                using (WordDocument destinationDocument = new WordDocument(destinationStream, FormatType.Docx))
                 {
-                    // Open the destination Word document for reading using another file stream.
-                    using (FileStream destinationStream = new FileStream(Path.GetFullPath(@"Data/DestinationDocument.docx"), FileMode.Open, FileAccess.Read))
+                    using (FileStream srcStream = new FileStream(Path.GetFullPath(@"Data/SourceDocument.docx"), FileMode.Open, FileAccess.Read))
                     {
-                        // Load the destination document from the stream.
-                        using (WordDocument destinationDocument = new WordDocument(destinationStream, FormatType.Docx))
+                        //Open the source Word document.
+                        using (WordDocument srcDocument = new WordDocument(srcStream, FormatType.Docx))
                         {
-                            // Replace text "Text one" in the source document with content from the bookmark "bkmk1" in the destination document.
-                            DocxReplaceTextWithDocPart(sourceDocument, destinationDocument, "Text one", "bkmk1");
-                            // Replace text "Text two" in the source document with content from the bookmark "bkmk2" in the destination document.
-                            DocxReplaceTextWithDocPart(sourceDocument, destinationDocument, "Text two", "bkmk2");
-                            // Open a stream to save the modified source document as a new file.
+                            //Replace text "Text one" in the destination document with content from the bookmark "bkmk1" in the source document.
+                            DocxReplaceTextWithDocPart(destinationDocument, srcDocument, "Text one", "bkmk1");
+                            //Replace text "Text two" in the destination document with content from the bookmark "bkmk2" in the source document.
+                            DocxReplaceTextWithDocPart(destinationDocument, srcDocument, "Text two", "bkmk2");
+                            //Save the modified destination document to the output stream.
                             using (FileStream output = new FileStream(Path.GetFullPath("Output/Output.docx"), FileMode.Create, FileAccess.Write))
                             {
-                                // Save the modified source document to the output stream in DOCX format.
-                                sourceDocument.Save(output, FormatType.Docx);
+                                destinationDocument.Save(output, FormatType.Docx);
                             }
                         }
                     }
@@ -42,49 +39,49 @@ namespace Replace_content_with_bookmark
         {
             string bookmarkRef = textBookmark + "_bm";
 
-            // Find the start token in the document.
+            //Find the start token in the document.
             TextSelection start = document.Find(tokenToFind, true, true);
             if (start != null)
             {
                 WTextRange startText = start.GetAsOneRange();
                 WParagraph startParagraph = startText.OwnerParagraph;
-                // Get the index of the start token in the paragraph.
+                //Get the index of the start token in the paragraph.
                 int index = startParagraph.Items.IndexOf(startText);
-                // Remove the start token at the specified index.
+                //Remove the start token at the specified index.
                 startParagraph.Items.Remove(startText);
-                // Create and insert a BookmarkStart at the index of the start token.
+                //Create and insert a BookmarkStart at the index of the start token.
                 BookmarkStart bookmarkStart = new BookmarkStart(document, bookmarkRef);
                 startParagraph.Items.Insert(index, bookmarkStart);
                 startParagraph.AppendBookmarkEnd(bookmarkRef);
 
-                // Check if the bookmark exists in the source document.
+                //Check if the bookmark exists in the source document.
                 if (sourceDoc.Bookmarks.FindByName(textBookmark) != null)
                 {
-                    // Access the bookmark in the source document.
+                    //Access the bookmark in the source document.
                     BookmarksNavigator bookmarksNavigator = new BookmarksNavigator(sourceDoc);
                     bookmarksNavigator.MoveToBookmark(textBookmark);
-                    // Get the bookmark content.
+                    //Get the bookmark content.
                     WordDocumentPart wordDocumentPart = bookmarksNavigator.GetContent();
                     bookmarksNavigator = new BookmarksNavigator(document);
                     bookmarksNavigator.MoveToBookmark(bookmarkRef);
 
-                    // Get the destination paragraph before replacing.
+                    //Get the destination paragraph before replacing.
                     WParagraph destinationPara = bookmarksNavigator.CurrentBookmark.BookmarkStart.OwnerParagraph;
-                    // Store the list style, first line indent, and left indent of the paragraph.
+                    //Store the list style, first line indent, and left indent of the paragraph.
                     string listStyleName = destinationPara.ListFormat.CustomStyleName;
                     float firstLineIndent = destinationPara.ParagraphFormat.FirstLineIndent;
                     float leftIndent = destinationPara.ParagraphFormat.LeftIndent;
 
-                    // Replace the selected text with the bookmark content from the source document.
+                    //Replace the selected text with the bookmark content from the source document.
                     bookmarksNavigator.ReplaceContent(wordDocumentPart);
-                    // Reapply the list style and indent values after replacement.
+                    //Reapply the list style and indent values after replacement.
                     destinationPara.ListFormat.ApplyStyle(listStyleName);
                     destinationPara.ParagraphFormat.FirstLineIndent = firstLineIndent;
                     destinationPara.ParagraphFormat.LeftIndent = leftIndent;
                 }
                 else
                 {
-                    // If the bookmark is not found, replace the bookmark content with an empty string.
+                    //If the bookmark is not found, replace the bookmark content with an empty string.
                     BookmarksNavigator bookmarksNavigator = new BookmarksNavigator(document);
                     bookmarksNavigator.MoveToBookmark(bookmarkRef);
                     bookmarksNavigator.ReplaceBookmarkContent(string.Empty, true);
