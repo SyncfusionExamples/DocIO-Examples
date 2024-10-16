fd5e44f27e73dd776252ce1da7c482463d61c77f
VijayadharshiniMathiyalagan <vijayadharshini.m@syncfusion.com>
2024-10-16 19:38:04 +0530

    Reverted

--- Find-item-in-word-document/Replace-text-heading-paragraphs/.NET/Replace-text-heading-paragraphs/Program.cs
+++ Find-item-in-word-document/Replace-text-heading-paragraphs/.NET/Replace-text-heading-paragraphs/Program.cs
@@ -1,4 +1,7 @@
-﻿using Syncfusion.DocIO;
+﻿
+
+using Microsoft.VisualBasic.FileIO;
+using Syncfusion.DocIO;
 using Syncfusion.DocIO.DLS;
 
 namespace Replace_text_heading_paragraphs
@@ -7,26 +10,24 @@ namespace Replace_text_heading_paragraphs
     {
         static void Main(string[] args)
         {
-            using (FileStream inputFileStream = new FileStream(Path.GetFullPath(@"Data/Input.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (FileStream fileStreamPath = new FileStream(Path.GetFullPath(@"Data/Input.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             {
-                //Opens the input Word document.
-                using (WordDocument document = new WordDocument(inputFileStream, FormatType.Automatic))
+                //Opens an existing Word document.
+                using (WordDocument document = new WordDocument(fileStreamPath, FormatType.Automatic))
                 {
                     for (int headingLevel = 1; headingLevel < 10; headingLevel++)
                     {
                         //Find headings based on the levels and endnote by paragraph in Word document.
                         List<Entity> headings = document.FindAllItemsByProperty(EntityType.Paragraph, "StyleName", "Heading " + headingLevel);
-                        //Iterate through all headings in the list.
+                        //Replace the headings with text.
                         for (int index = 0; index < headings.Count; index++)
                         {
-                            //Cast the current heading to WParagraph.
                             WParagraph paragraph = headings[index] as WParagraph;
-                            //Remove all child elements from the paragraph.
                             paragraph.ChildEntities.Clear();
-                            //Add new text to replace the heading content.
-                            paragraph.AppendText("Replaced Heading" + headingLevel + " text");
+                            paragraph.AppendText("Replaced Heading"+headingLevel+" text");
                         }
                     }
+                    //Creates file stream.
                     using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Result.docx"), FileMode.Create, FileAccess.ReadWrite))
                     {
                         //Saves the Word document to file stream.
