78f5d86b1b0bf689eb3448b4f2351d59e5904f70
VijayadharshiniMathiyalagan <vijayadharshini.m@syncfusion.com>
2024-10-17 14:36:02 +0530

    ES-876652-How-to-change-the-font-of-table-content-in-a-Word-document - Added Sample

---
+++ Tables/Change-font-of-table-content/Change-font-of-table-content/Program.cs
@@ -0,0 +1,46 @@
+﻿using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIO;
+
+//Register Syncfusion license
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2UlhhQlNHfV5DQmBWfFN0QXNYfVRwdF9GYEwgOX1dQl9nSXZTc0VlWndfcXNSQWc=");
+
+using (FileStream inputStream = new FileStream("Data/Adventure.html", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+{
+    // Open the input HTML format document.
+    using (WordDocument document = new WordDocument(inputStream, FormatType.Html))
+    {
+        // Access the first section of the document.
+        WSection section = document.Sections[0];
+        // Iterate through all tables in the section.
+        foreach (WTable table in section.Tables)
+        {
+            // Iterate through each row in the table.
+            foreach (WTableRow row in table.Rows)
+            {
+                // Iterate through each cell in the row.
+                foreach (WTableCell cell in row.Cells)
+                {
+                    // Iterate through each paragraph in the cell.
+                    foreach (WParagraph paragraph in cell.Paragraphs)
+                    {
+                        // Iterate through the child entities of the paragraph.
+                        foreach (Entity entity in paragraph.ChildEntities)
+                        {
+                            // Check if the child entity is a text range.
+                            if (entity is WTextRange)
+                            {
+                                // Apply character format to change the font to Arial for the text range.
+                                (entity as WTextRange).CharacterFormat.FontName = "Algerian";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // Save the modified Word document.
+        using (FileStream outputStream = new FileStream("Output/Output.docx", FileMode.Create, FileAccess.Write))
+        {
+            document.Save(outputStream, FormatType.Docx);
+        }
+    }
+}
