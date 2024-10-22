f9cf8f59e72c7e73dc76d9ce9ed1751d1cfa1b32
DharanitharanA <97955635+DharanitharanA@users.noreply.github.com>
2024-10-22 10:09:10 +0530

    901973-Modified the input Word document

--- Find-and-Replace/Find-and-apply-bold-to-replaced-content/.NET/Find-and-apply-bold-to-replaced-content/Program.cs
+++ Find-and-Replace/Find-and-apply-bold-to-replaced-content/.NET/Find-and-apply-bold-to-replaced-content/Program.cs
@@ -15,41 +15,32 @@ namespace Find_and_apply_bold_to_replaced_content
                 using (WordDocument document = new WordDocument(docStream, FormatType.Docx))
                 {
                     // Find the text pattern using a regular expression.
-                    TextSelection selection = document.Find(new Regex("Adventure Works Cycles"));
-
+                    TextSelection selection = document.Find(new Regex("^<<([^>]*)>>"));
                     // Get the selected text as a single text range.
                     WTextRange textRange = selection.GetAsOneRange();
-
                     // Get the text body and paragraph of selected text.
                     WTextBody body = textRange.OwnerParagraph.OwnerTextBody;
                     WParagraph paragraph = textRange.OwnerParagraph;
-
                     // Get the index of the paragraph within the text body.
                     int paraIndex = body.ChildEntities.IndexOf(paragraph);
-
                     // Replace the selected text with the replace content.
-                    document.Replace("Adventure Works Cycles", "Company name", true, true);
-
+                    document.Replace(selection.SelectedText, "Adventure Works Cycles", true, true);
                     // Iterate the body from the replaced paragraph.
                     for (int i = paraIndex; i < body.Count; i++)
                     {
                         // Access each paragraph in the body.
                         WParagraph para = body.ChildEntities[i] as WParagraph;
-
                         // Search for the word within the current paragraph.
-                        TextSelection replacedSelection = para.Find("Company", true, false);
-
+                        TextSelection replacedSelection = para.Find("Works", true, false);
                         if (replacedSelection != null)
                         {
                             // Get the selected text as a single text range.
                             WTextRange replacedTextRange = replacedSelection.GetAsOneRange();
-
                             // Apply bold formatting to the text range.
                             replacedTextRange.CharacterFormat.Bold = true;
                             break;
                         }
                     }
-
                     // Save the modified document to the specified output path.
                     using (FileStream outputStream = new FileStream(Path.GetFullPath(@"Output/Result.docx"), FileMode.Create, FileAccess.Write))
                     {
@@ -57,7 +48,6 @@ namespace Find_and_apply_bold_to_replaced_content
                     }
                 }
             }
-
         }
     }
 }
