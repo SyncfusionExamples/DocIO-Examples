20260a99705cd26314a7dfa17a7a411a2aedeecc
VijayadharshiniMathiyalagan <vijayadharshini.m@syncfusion.com>
2024-10-29 17:29:44 +0530

    Added sample for add rows with dynamic data into an existing table

---
+++ Tables/Add-rows-with-dynamic-data/Add-rows-with-dynamic-data/Program.cs
@@ -0,0 +1,52 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIORenderer;
+using Syncfusion.Pdf;
+
+using (FileStream inputStream = new FileStream(@"Data/Template.docx", FileMode.Open, FileAccess.Read))
+{
+    // Open an existing Word document.
+    using (WordDocument document = new WordDocument(inputStream, FormatType.Automatic))
+    {
+        // Find the table with title.
+        WTable table = document.FindItemByProperty(EntityType.Table, "Title", "Template") as WTable;
+        // Check if the table with the specified title was found.
+        if (table != null)
+        {
+            // Define new data to add to the table.
+            List<string[]> rowData =
+            [
+                ["3.", "Banana", "20"],
+                ["4.", "Grapes", "70"]
+            ];
+            for (int j = 0; j < rowData.Count; j++)
+            {
+                string[] data = rowData[j];
+                // Add a new row to the table.
+                WTableRow row = table.AddRow(false);
+                for (int i = 0; i < data.Length; i++)
+                {
+                    string cellData = data[i];
+                    // Get the cell at the current index.
+                    WTableCell cell = row.Cells[i];
+                    // Add a paragraph to the cell.
+                    WParagraph cellParagraph = cell.AddParagraph() as WParagraph;
+                    // Add the cell data as text within the cell's paragraph.
+                    cellParagraph.AppendText(cellData);
+                }
+            }
+        }
+        using (DocIORenderer renderer = new DocIORenderer())
+        {
+            // Convert the Word document into a PDF document.
+            using (PdfDocument pdfDocument = renderer.ConvertToPDF(document))
+            {
+                using (FileStream outputStream = new FileStream(@"Output/Result.pdf", FileMode.Create, FileAccess.Write))
+                {
+                    // Save the converted PDF document.
+                    pdfDocument.Save(outputStream);
+                }
+            }
+        }
+    }
+}
