f2fdf59799d8639b1f4cddc95d7554ecac3dc2c6
VijayadharshiniMathiyalagan <vijayadharshini.m@syncfusion.com>
2024-10-30 07:26:51 +0530

    Modified the image, template and code snippet

--- Paragraphs/Replace-image-with-same-size/Replace-image-with-same-size/Program.cs
+++ Paragraphs/Replace-image-with-same-size/Replace-image-with-same-size/Program.cs
@@ -8,32 +8,18 @@ using (FileStream fileStreamPath = new FileStream(Path.GetFullPath(@"Data/Templa
     {
         // Variables to store the original width and height of each picture.
         float pictureWidth, pictureHeight = 0;
-        // Retrieve the body of the first section.
-        WTextBody textbody = document.Sections[0].Body;
-        // Iterates through each paragraph in the document's body.
-        foreach (WParagraph paragraph in textbody.Paragraphs)
-        {
-            // Iterates through each item within the paragraph.
-            foreach (ParagraphItem item in paragraph.ChildEntities)
-            {
-                // Checks if the item is a picture.
-                if (item is WPicture)
-                {
-                    // Casts the item to WPicture to access picture properties.
-                    WPicture picture = item as WPicture;
-                    // Store the original width and height of the picture.
-                    pictureWidth = picture.Width;
-                    pictureHeight = picture.Height;
-                    // Open the new image file to replace the existing picture.
-                    FileStream imageStream = new FileStream(Path.GetFullPath(@"Data/Image.png"), FileMode.Open, FileAccess.ReadWrite);
-                    // Load the new image into the existing picture element.
-                    picture.LoadImage(imageStream);
-                    // Restore the original width and height to maintain the picture's dimensions.
-                    picture.Width = pictureWidth;
-                    picture.Height = pictureHeight;
-                }
-            }
-        }
+        // Find the picture with alternative text.
+        WPicture picture = document.FindItemByProperty(EntityType.Picture, "AlternativeText", "Adventure") as WPicture;
+        // Store the original width and height of the picture.
+        pictureWidth = picture.Width;
+        pictureHeight = picture.Height;
+        // Open the new image file to replace the existing picture.
+        FileStream imageStream = new FileStream(Path.GetFullPath(@"Data/Image.png"), FileMode.Open, FileAccess.ReadWrite);
+        // Load the new image into the existing picture element.
+        picture.LoadImage(imageStream);
+        // Restore the original width and height to maintain the picture's dimensions.
+        picture.Width = pictureWidth;
+        picture.Height = pictureHeight;
         using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Result.docx"), FileMode.Create, FileAccess.ReadWrite))
         {
             // Saves the modified Word document.
