12d239605b336f2fa1409b36686164fd9e97117f
VijayadharshiniMathiyalagan <vijayadharshini.m@syncfusion.com>
2024-11-05 08:46:02 +0530

    Added find and replace samples

---
+++ Find-and-Replace/Find-text-in-Word-document/.NET/Find-text-in-Word-document/Program.cs
@@ -0,0 +1,25 @@
+﻿using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIO;
+using Syncfusion.Drawing;
+
+using (FileStream fileStreamPath = new FileStream(Path.GetFullPath(@"Data/Template.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+{
+    //Opens an existing Word document.
+    using (WordDocument document = new WordDocument(fileStreamPath, FormatType.Automatic))
+    {
+        //Finds the first occurrence of a particular text in the document.
+        TextSelection textSelection = document.Find("Adventure Works Cycles", false, true);
+        //Gets the found text as single text range.
+        WTextRange textRange = textSelection.GetAsOneRange();
+        //Modifies the text.
+        textRange.Text = "Replaced text";
+        //Sets highlight color.
+        textRange.CharacterFormat.HighlightColor = Color.Yellow;
+        //Creates file stream.
+        using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Result.docx"), FileMode.Create, FileAccess.ReadWrite))
+        {
+            //Saves the Word document to file stream.
+            document.Save(outputFileStream, FormatType.Docx);
+        }
+    }
+}
