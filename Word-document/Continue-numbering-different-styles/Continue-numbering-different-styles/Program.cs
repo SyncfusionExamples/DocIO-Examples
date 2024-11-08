a7c2996bc954f8f53f31520bf00caceb9c702021
VijayadharshiniMathiyalagan <vijayadharshini.m@syncfusion.com>
2024-11-08 16:01:30 +0530

    Added sample for apply continuos list numbering

--- Word-document/Continue-numbering-different-styles/Continue-numbering-different-styles/Program.cs
+++ Word-document/Continue-numbering-different-styles/Continue-numbering-different-styles/Program.cs
@@ -3,32 +3,32 @@ using Syncfusion.DocIO.DLS;
 
 using (FileStream destinationStream = new FileStream(Path.GetFullPath("Data/DestinationDocument.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
 {
-    using (FileStream sourceStream = new FileStream(Path.GetFullPath("Data/SourceDocument.docx"), FileMode.Open, FileAccess.ReadWrite))
+    // Open the destination Word document.
+    using (WordDocument destinationDocument = new WordDocument(destinationStream, FormatType.Docx))
     {
-        // Open the destination Word document.
-        using (WordDocument destinationDocument = new WordDocument(destinationStream, FormatType.Docx))
+        using (FileStream sourceStream = new FileStream(Path.GetFullPath("Data/SourceDocument.docx"), FileMode.Open, FileAccess.ReadWrite))
         {
             // Open the source Word document.
             using (WordDocument sourceDocument = new WordDocument(sourceStream, FormatType.Docx))
             {
-                // Set the break code of the source document's first section to prevent page breaks.
+                // Set the break code as no break to prevent page breaks.
                 sourceDocument.Sections[0].BreakCode = SectionBreakCode.NoBreak;
                 // Initialize a variable to hold the list style from the destination document.
                 ListStyle listStyle = null;
                 // Iterate through the paragraphs in the last section of the destination document.
                 foreach (WParagraph paragraph in destinationDocument.LastSection.Paragraphs)
                 {
-                    // Store the current list style if available.
                     if (paragraph.ListFormat.CurrentListStyle != null)
                     {
-                        listStyle = paragraph.ListFormat.CurrentListStyle; 
+                        // Get the current list style.
+                        listStyle = paragraph.ListFormat.CurrentListStyle;
                     }
                     else
                     {
-                        // If no current list style, check the paragraph style for a list style and store it.
-                        WParagraphStyle style1 = destinationDocument.Styles.FindByName(paragraph.StyleName) as WParagraphStyle;
-                        if (style1 != null)
-                            listStyle = style1.ListFormat.CurrentListStyle;
+                        // Check the paragraph style for a list style and store it.
+                        WParagraphStyle style = destinationDocument.Styles.FindByName(paragraph.StyleName) as WParagraphStyle;
+                        if (style != null)
+                            listStyle = style.ListFormat.CurrentListStyle;
                     }
                     // Break the loop if a list style is found.
                     if (listStyle != null)
@@ -36,14 +36,13 @@ using (FileStream destinationStream = new FileStream(Path.GetFullPath("Data/Dest
                 }
                 // Import the content of the source document at the end of the destination document.
                 destinationDocument.ImportContent(sourceDocument, ImportOptions.ListContinueNumbering);
-                // If a list style is found, apply it to the paragraphs in the destination document to maintain continuous numbering.
                 if (listStyle != null)
                 {
+                    // Apply liststyle to the paragraphs in the destination document to maintain continuous numbering.
                     foreach (WParagraph paragraph in destinationDocument.LastSection.Paragraphs)
                     {
-                        // Apply the found list style.
                         if (paragraph.ListFormat.CurrentListStyle != null)
-                            paragraph.ListFormat.ApplyStyle(listStyle.Name); 
+                            paragraph.ListFormat.ApplyStyle(listStyle.Name);
                     }
                 }
                 // Save the merged document.
