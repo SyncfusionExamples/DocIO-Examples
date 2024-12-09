bb5396071430a256dd952d6bc3f34de10851532f
Suriya-Balamurugan <suriya.balamurugan@syncfusion.com>
2024-12-09 20:33:05 +0530

    Added example for Multithreading

---
+++ Word-to-PDF-Conversion/Multithreaded-using-parallel-process/.NET/Multithreaded-using-parallel-process/Program.cs
@@ -0,0 +1,51 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIORenderer;
+using Syncfusion.Pdf;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Multithreaded_using_parallel_process
+{
+    class MultiThreading
+    {
+        static void Main(string[] args)
+        {
+            //Indicates the number of threads to be create.
+            int limit = 5;
+            Console.WriteLine("Parallel For Loop");
+            Parallel.For(0, limit, count =>
+            {
+                Console.WriteLine("Task {0} started", count);
+                //Convert multiple Word document, one document on each thread.
+                ConvertWordToPDF(count);
+                Console.WriteLine("Task {0} is done", count);
+            });
+        }
+        //Convert a Word document to PDF using multi-threading.
+        static void ConvertWordToPDF(int count)
+        {
+            using (FileStream inputStream = new FileStream(Path.GetFullPath(@"Data/Input.docx"), FileMode.Open, FileAccess.Read))
+            {
+                //Load an existing Word document.
+                using (WordDocument document = new WordDocument(inputStream, FormatType.Docx))
+                {
+                    //Create an instance of DocIORenderer.
+                    using (DocIORenderer renderer = new DocIORenderer())
+                    {
+                        //Convert Word document to PDF.
+                        using (PdfDocument pdfDocument = renderer.ConvertToPDF(document))
+                        {
+                            //Save the PDF document.
+                            using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output" + count + ".pdf"), FileMode.Create, FileAccess.Write))
+                            {
+                                pdfDocument.Save(outputFileStream);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Word-to-PDF-Conversion/Multithreaded-using-tasks/.NET/Multithreaded-using-tasks/Program.cs
@@ -0,0 +1,52 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIORenderer;
+using Syncfusion.Pdf;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Multithreaded_using_tasks
+{
+    class MultiThreading
+    {
+        //Indicates the number of threads to be create.
+        private const int TaskCount = 1000;
+        public static async Task Main()
+        {
+            //Create an array of tasks based on the TaskCount.
+            Task[] tasks = new Task[TaskCount];
+            for (int i = 0; i < TaskCount; i++)
+            {
+                tasks[i] = Task.Run(() => ConvertWordToPDF());
+            }
+            //Ensure all tasks complete by waiting on each task.
+            await Task.WhenAll(tasks);
+        }
+
+        //Convert a Word document to PDF using multi-threading.
+        static void ConvertWordToPDF()
+        {
+            using (FileStream inputStream = new FileStream(Path.GetFullPath(@"Data/Input.docx"), FileMode.Open, FileAccess.Read))
+            {
+                //Load an existing Word document.
+                using (WordDocument document = new WordDocument(inputStream, FormatType.Docx))
+                {
+                    //Create an instance of DocIORenderer.
+                    using (DocIORenderer renderer = new DocIORenderer())
+                    {
+                        //Convert Word document to PDF.
+                        using (PdfDocument pdfDocument = renderer.ConvertToPDF(document))
+                        {
+                            //Save the PDF document.
+                            using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output" + Guid.NewGuid().ToString() + ".pdf"), FileMode.Create, FileAccess.Write))
+                            {
+                                pdfDocument.Save(outputFileStream);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
