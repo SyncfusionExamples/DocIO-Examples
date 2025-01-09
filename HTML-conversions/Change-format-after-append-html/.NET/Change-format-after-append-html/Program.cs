8586300fba262103491abddb62b96ff348f5d659
DharanyaSakthivel-SF4210 <Dharanya.sakthivel@syncfusion.com>
2025-01-09 11:36:00 +0530

    898185-change-format-after-append-html

--- HTML-conversions/Change-format-after-append-html/.NET/Change-format-after-append-html/Program.cs
+++ HTML-conversions/Change-format-after-append-html/.NET/Change-format-after-append-html/Program.cs
@@ -19,7 +19,7 @@ namespace Change_format_after_append_html
                     int paraIndex = section.Body.Paragraphs.Count - 1;
 
                     // Append HTML content to the document with formatting in <p> tag.
-                    document.LastParagraph.AppendHTML("<p style='color:blue; font-weight:bold;'>The Giant</p><p style='color:green; font-style:italic;'>Panda</p>");
+                    document.LastParagraph.AppendHTML("<p>The Giant</p><p>Panda</p>");
 
                     // Iterate through the paragraphs in the section's body.
                     for (int i = paraIndex; i < section.Body.ChildEntities.Count; i++)
@@ -31,13 +31,6 @@ namespace Change_format_after_append_html
                             // Set the paragraph formatting spacing to 0.
                             paragraph.ParagraphFormat.BeforeSpacing = 0;
                             paragraph.ParagraphFormat.AfterSpacing = 0;
-
-                            // Iterate through the items in the paragraph to chnage formatting.
-                            foreach (var item in paragraph.ChildEntities)
-                            {
-                                if (item is WTextRange textRange)
-                                    textRange.CharacterFormat.TextColor = Syncfusion.Drawing.Color.DarkGreen; // Change text color
-                            }
                         }
                     }
                     // Save the modified document.
