1b01cfe17a6b957f1be1854470de213abfee7c5c
DharanyaSakthivel-SF4210 <Dharanya.sakthivel@syncfusion.com>
2025-02-11 12:28:53 +0530

    Mail-merge-in-multiple-column

---
+++ Mail-Merge/Mail-merge-in-multiple-column/.NET/Mail-merge-in-multiple-column/Program.cs
@@ -0,0 +1,95 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Mail_merge_in_multiple_column
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fileStream = new FileStream(Path.GetFullPath(@"Data/Template.docx"), FileMode.Open, FileAccess.ReadWrite))
+            {
+                //Loads an existing Word document into DocIO instance.
+                using (WordDocument document = new WordDocument(fileStream, FormatType.Automatic))
+                {
+                    //Gets the employee details as IEnumerable collection.
+                    List<Employee> employeeList = GetEmployees();
+                    //Creates an instance of MailMergeDataTable by specifying MailMerge group name and IEnumerable collection.
+                    MailMergeDataTable dataSource = new MailMergeDataTable("Employees", employeeList);
+                    //Uses the mail merge events handler for image fields.
+                    document.MailMerge.MergeImageField += new MergeImageFieldEventHandler(MergeField_EmployeeImage);
+                    //Performs Mail merge.
+                    document.MailMerge.ExecuteGroup(dataSource);
+                    //Creates file stream.
+                    using (FileStream outputStream = new FileStream(Path.GetFullPath(@"Output/Result.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        //Saves the Word document to file stream.
+                        document.Save(outputStream, FormatType.Docx);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the employee details to perform mail merge.
+        /// </summary>
+        public static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee("Nancy", "Smith", "Sales Representative", "505 - 20th Ave. E. Apt. 2A,", "Seattle", "WA", "USA", "Nancy.png"));
+            employees.Add(new Employee("Andrew", "Fuller", "Vice President, Sales", "908 W. Capital Way", "Tacoma", "WA", "USA", "Andrew.png"));
+            employees.Add(new Employee("Roland", "Mendel", "Sales Representative", "722 Moss Bay Blvd.", "Kirkland", "WA", "USA", "Janet.png"));
+            employees.Add(new Employee("Margaret", "Peacock", "Sales Representative", "4110 Old Redmond Rd.", "Redmond", "WA", "USA", "Margaret.png"));
+            employees.Add(new Employee("Steven", "Buchanan", "Sales Manager", "14 Garrett Hill", "London", string.Empty, "UK", "Steven.png"));
+            return employees;
+        }
+
+        /// <summary>
+        /// Represents the method that handles MergeImageField event.
+        /// </summary>
+        private static void MergeField_EmployeeImage(object sender, MergeImageFieldEventArgs args)
+        {
+            //Binds image from file system during mail merge.
+            if (args.FieldName == "Photo")
+            {
+                string photoFileName = args.FieldValue.ToString();
+                //Gets the image from file system.
+                FileStream imageStream = new FileStream(Path.GetFullPath(@"Data/" + photoFileName), FileMode.Open, FileAccess.Read);
+                args.ImageStream = imageStream;
+                //Gets the picture, to be merged for image merge field
+                WPicture picture = args.Picture;
+                //Resizes the picture
+                picture.Height = 90;
+                picture.Width = 90;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Represents a class to maintain employee details.
+    /// </summary>
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+        public string Title { get; set; }
+        public string Photo { get; set; }
+        public Employee(string firstName, string lastName, string title, string address, string city, string region, string country, string photoFilePath)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Title = title;
+            Address = address;
+            City = city;
+            Region = region;
+            Country = country;
+            Photo = photoFilePath;
+        }
+    }
+}
