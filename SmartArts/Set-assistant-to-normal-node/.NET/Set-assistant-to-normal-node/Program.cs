15d7f970fd704b3dbdb49f3a52fda764cfe240c9
DharanyaSakthivel-SF4210 <Dharanya.sakthivel@syncfusion.com>
2025-03-13 12:19:32 +0530

    Working-with-SmartArts

--- SmartArts/Set-assistant-to-normal-node/.NET/Set-assistant-to-normal-node/Program.cs
+++ SmartArts/Set-assistant-to-normal-node/.NET/Set-assistant-to-normal-node/Program.cs
@@ -5,20 +5,21 @@ using Syncfusion.Office;
 using System.IO;
 
 //Create a new Word document instance.
-WordDocument document = new WordDocument();
-//Add a new section to the document.
-IWSection section = document.AddSection();
-//Add a paragraph to the section.
-IWParagraph paragraph = section.AddParagraph();
+WordDocument wordDocument = new WordDocument();
+//Add one section and one paragraph to the document.
+wordDocument.EnsureMinimal();
 //Add a SmartArt to the paragraph at the specified size.
-WSmartArt smartArt = paragraph.AppendSmartArt(OfficeSmartArtType.OrganizationChart, 432, 252);
+WSmartArt smartArt = wordDocument.LastParagraph.AppendSmartArt(OfficeSmartArtType.OrganizationChart, 640, 426);
 //Traverse through all nodes of the SmartArt.
 foreach (IOfficeSmartArtNode node in smartArt.Nodes)
 {
-    //Check if the node is assistant or not.
-    if (node.IsAssistant)
-        //Set the assistant node to false.
-        node.IsAssistant = false;
+    foreach (IOfficeSmartArtNode childNode in node.ChildNodes)
+    {
+        //Check if the node is assistant or not.
+        if (childNode.IsAssistant)
+            //Set the assistant node to false.
+            childNode.IsAssistant = false;
+    }
 }
 // Create a file stream to save the modified document.
 using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Result.docx"), FileMode.Create, FileAccess.ReadWrite))
