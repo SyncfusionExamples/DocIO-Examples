767feb45aa42ca83cf2643957068d16b0ccb4770
DharanyaSakthivel-SF4210 <Dharanya.sakthivel@syncfusion.com>
2025-03-17 16:24:06 +0530

    ES-944814-DocIO_fileformat_checker

---
+++ Word-document/DocIO-fileformat-checker/.NET/DocIO-fileformat-checker/Program.cs
@@ -0,0 +1,46 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System;
+using System.IO;
+
+namespace DocIO_fileformat_checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get all files from the directory
+            string[] files = Directory.GetFiles(Path.GetFullPath(@"Data/"));
+            // Loop through each file in the directory
+            foreach (string filePath in files)
+            {
+                using (FileStream inputStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    try
+                    {
+                        // Try to open the file using Syncfusion DocIO
+                        using (WordDocument doc = new WordDocument(inputStream, FormatType.Automatic))
+                        {
+                            // Successfully opened the document
+                            Console.WriteLine("Supported format" + filePath);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        // Check if the exception message matches the unsupported format message
+                        if (ex.Message.Contains("This file format is not supported"))
+                        {
+                            // If the file format is not supported, print it to the console
+                            Console.WriteLine("Unsupported format: " + filePath);
+                        }
+                        else
+                        {
+                            // If some other exception occurs, handle it
+                            Console.WriteLine($"Error opening file {filePath}: {ex.Message}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Word-document/DocIO-fileformat-checker/.NET/DocIO-fileformat-checker/README.md
@@ -0,0 +1,13 @@
+Identify file formats not supported by DocIO
+----------------------------------------
+
+DocIO supports Word 97-2003 and later versions, handling major Microsoft Word file formats, including DOC, DOCX, RTF, DOT, DOTX, and DOCM. If you use an older or unsupported format, convert it to a supported format to avoid errors. Otherwise, you will get this exception: **This file format is not supported**.
+
+**To run this example**
+
+*   Download this project to a location in your disk.
+*   Create a folder **Data** parallelly to .sln file.
+*   Add the input documents to the "Data" folder.
+*   Run the application.
+
+The console output will indicate whether the provided input document is supported or unsupported. If the document is unsupported, convert it to a format supported by DocIO.
