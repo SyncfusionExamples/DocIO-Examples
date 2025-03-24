bbf976430bafd5a56fe5540181035cc2fb141e80
DharanyaSakthivel-SF4210 <Dharanya.sakthivel@syncfusion.com>
2025-03-24 15:56:18 +0530

    ES-935026-Get-field-code

---
+++ Fields/Get-field-code/.NET/Get-field-code/Program.cs
@@ -0,0 +1,28 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System;
+using System.IO;
+
+namespace Get_Field_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Open the Word document file stream.
+            using (FileStream fileStreamPath = new FileStream(Path.GetFullPath(@"Data/Template.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                // Load the template document.
+                using (WordDocument document = new WordDocument(fileStreamPath, FormatType.Docx))
+                {
+                    // Access the merge field in the first paragraph of the first section.
+                    WMergeField mergeField = document.Sections[0].Paragraphs[0].ChildEntities[2] as WMergeField;
+
+                    // Get and print the field code of the merge field.
+                    string fieldCode = mergeField.FieldCode;
+                    Console.WriteLine(fieldCode);
+                }
+            }
+        }
+    }
+}
