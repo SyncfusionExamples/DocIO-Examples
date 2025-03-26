849cd491792edf0fbf7181177f25f532dd2681bf
DharanyaSakthivel-SF4210 <149999916+DharanyaSakthivel-SF4210@users.noreply.github.com>
2025-03-26 08:44:04 +0530

    Update Program.cs

--- Mail-Merge/Execute-Mail-Merge-in-columnwise/.NET/Execute-Mail-Merge-in-columnwise/Program.cs
+++ Mail-Merge/Execute-Mail-Merge-in-columnwise/.NET/Execute-Mail-Merge-in-columnwise/Program.cs
@@ -1,73 +1,69 @@
-﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO;
 using Syncfusion.DocIO.DLS;
-using System.Collections.Generic;
+using System.Data;
 using System.IO;
 
-namespace Execute_Mail_Merge_in_columnwise
+namespace Generate_Documents_for_each_record
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            using (FileStream fileStream = new FileStream(Path.GetFullPath(@"Data/Template.docx"), FileMode.Open, FileAccess.ReadWrite))
+            //Open the file as Stream.
+            using (FileStream fileStream = new FileStream(Path.GetFullPath(@"Data/Template.docx"), FileMode.Open))
             {
-                //Loads an existing Word document into DocIO instance.
-                using (WordDocument document = new WordDocument(fileStream, FormatType.Automatic))
+                //Get the data for mail merge.
+                DataTable table = GetDataTable();
+                //Iterate to the each row and generate mail merged document for each rows.
+                for (int i = 0; i < table.Rows.Count; i++)
                 {
-                    //Gets the customer details as “IEnumerable” collection
-                    List<CustomerDetail> customerDetails = GetCustomerDetails();
-                    //Creates an instance of “MailMergeDataTable” by specifying mail merge group name and “IEnumerable” collection.
-                    MailMergeDataTable dataTable = new MailMergeDataTable("CustomerDetails", customerDetails);
-                    //Removes the empty paragraph, if the field not have value.
-                    document.MailMerge.RemoveEmptyParagraphs = true;
-                    //Performs Mail merge
-                    document.MailMerge.ExecuteGroup(dataTable);
-
-                    //Creates file stream.
-                    using (FileStream outputStream = new FileStream(Path.GetFullPath(@"Output/Result.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    //Load file stream into Word document.
+                    using (WordDocument document = new WordDocument(fileStream, FormatType.Docx))
                     {
-                        //Saves the Word document to file stream.
-                        document.Save(outputStream, FormatType.Docx);
+                        //Executes mail merge using the data row.
+                        document.MailMerge.Execute(table.Rows[i]);
+
+                        //Create a file stream.
+                        using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/" + "Record_" + (i + 1) + ".docx"), FileMode.Create, FileAccess.ReadWrite))
+                        {
+                            //Save the Word document to the file stream.
+                            document.Save(outputFileStream, FormatType.Docx);
+                        }
                     }
                 }
             }
         }
-
         /// <summary>
-        /// Create a data to execute mail merge in Word document.
+        /// Get the data for mail merge.
         /// </summary>
         /// <returns></returns>
-        public static List<CustomerDetail> GetCustomerDetails()
+        static DataTable GetDataTable()
         {
-            List<CustomerDetail> customers = new List<CustomerDetail>();
-            customers.Add(new CustomerDetail("Sathish Kumar", "F3, Bharath PST Castle,", "14, Renga Nagar, Srirangam", "Trichy,", "Tamilnadu", string.Empty));
-            customers.Add(new CustomerDetail("Swathi", "No12, DN Aparments", "15, Thillai ganga nagar", "Chennai,", "Tamilnadu", string.Empty));
-            customers.Add(new CustomerDetail("Brent", "#12, London Road", string.Empty, string.Empty, "Oxford,", "United Kingdom"));
-            customers.Add(new CustomerDetail("Mani", "#12, Steve Lane,", string.Empty, string.Empty, "Dublin,", "Ireland"));
-            return customers;
-        }
-    }
-    /// <summary>
-    /// Represents a class to maintain customer details.
-    /// </summary>
-    public class CustomerDetail
-    {
-        public string Name { get; set; }
-        public string Address1 { get; set; }
-        public string Address2 { get; set; }
-        public string City { get; set; }
-        public string Region { get; set; }
-        public string Country { get; set; }
+            DataTable table = new DataTable();
 
-        public CustomerDetail(string name, string address1, string address2, string city, string region, string country)
-        {
-            Name = name;
-            Address1 = address1;
-            Address2 = address2;
-            City = city;
-            Region = region;
-            Country = country;
+            //Defining columns
+            table.Columns.Add("Name");
+            table.Columns.Add("Street");
+            table.Columns.Add("City");
+            table.Columns.Add("ProjectNo");
+
+            //Set values
+            DataRow row;
+            row = table.NewRow();
+            row["Name"] = "Andreas Waning";
+            row["Street"] = "Middelwegg 2";
+            row["City"] = "Vreden";
+            row["ProjectNo"] = "4711";
+            table.Rows.Add(row);
+
+            row = table.NewRow();
+            row["Name"] = "Mike Korf";
+            row["Street"] = "teststreet";
+            row["City"] = "TestCity";
+            row["ProjectNo"] = "4711";
+            table.Rows.Add(row);
+
+            return table;
         }
     }
 }
