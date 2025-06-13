492f2687ec9cb1be254ab58f4f930f25172dc780
DharanyaSakthivel-SF4210 <Dharanya.sakthivel@syncfusion.com>
2025-06-13 20:03:24 +0530

    ES-898646-Python-Wrapper

---
+++ Getting-Started/Python-wrapper/Create-document/Create-document/CreateWordDocument.cs
@@ -0,0 +1,39 @@
+﻿using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIO;
+using System;
+using System.IO;
+using Syncfusion.Licensing;
+
+namespace Create_document
+{
+    public class CreateWordDocument
+    {
+        public string WordDocument(string outputPath)
+        {
+            try
+            {
+                // Create a new Word document.
+                using (WordDocument document = new WordDocument())
+                {
+                    // Add a section and a paragraph with sample text.
+                    IWSection section = document.AddSection();
+                    IWParagraph paragraph = section.AddParagraph();
+                    paragraph.AppendText("In 2000, AdventureWorks Cycles bought a small manufacturing plant, Importadores Neptuno, located in Mexico. Importadores Neptuno manufactures several critical subcomponents for the AdventureWorks Cycles product line. These subcomponents are shipped to the Bothell location for final product assembly. In 2001, Importadores Neptuno, became the sole manufacturer and distributor of the touring bicycle product group.");
+
+                    // Save the document to the specified path.
+                    using (FileStream outputPathStream = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+                    {
+                        document.Save(outputPathStream, FormatType.Docx);
+                    }
+                }
+
+                return "Word document created successfully at: " + outputPath;
+            }
+            catch (Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
+        }
+
+    }
+}
---
+++ Getting-Started/Python-wrapper/Python/CreateWordDocument.py
@@ -0,0 +1,38 @@
+import clr #import clr from pythonnet
+import os
+
+#Get the DLL and input file path in string
+dll_path = r'D:\Create-document\Create-document\bin\Release\netstandard2.0\Create-document.dll' #Dll of application
+docio_dll = r'C:\Users\Admin\.nuget\packages\syncfusion.docio.net.core\version\lib\netstandard2.0\Syncfusion.DocIO.Portable.dll'
+officeChart_dll = r'C:\Users\Admin\.nuget\packages\syncfusion.officechart.net.core\version\lib\netstandard2.0\Syncfusion.OfficeChart.Portable.dll'
+compression_dll =r'C:\Users\Admin\.nuget\packages\syncfusion.compression.net.core\version\lib\netstandard2.0\Syncfusion.Compression.Portable.dll'
+license_dll = r'C:\Users\Admin\.nuget\packages\syncfusion.licensing\version\lib\netstandard2.0\Syncfusion.Licensing.dll'
+
+# Verify and add paths
+for path in [dll_path, docio_dll, officeChart_dll, compression_dll, license_dll]:
+   if path not in os.sys.path:
+       os.sys.path.append(path)
+       
+#load our dll file
+try: 
+   clr.AddReference(dll_path)
+   clr.AddReference(docio_dll)
+   clr.AddReference(officeChart_dll)
+   clr.AddReference(compression_dll)
+   clr.AddReference(license_dll)
+   print ("Load success")
+except Exception as e:
+   print("Fail to load")
+
+#import our DocIO class from Our C# namespace DocIOLibrary
+from Create_document import CreateWordDocument
+
+document = CreateWordDocument() #create our DocIO object
+
+# Define file path
+output_doc = r"Create-Word-document.docx" #path of result document
+
+# Call CompareDocuments method
+result = document.WordDocument(output_doc)
+
+print("Comparison Result:", result) 
