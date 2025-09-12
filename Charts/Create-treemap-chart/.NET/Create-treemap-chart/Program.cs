027cd97596df37a0c2319c6c78c41cb23c91dd97
Sushma-SF4796 <sushma.princeedwin@syncfusion.com>
2025-09-12 16:23:24 +0530

    Added samples for sunburst and treemap

---
+++ Charts/Create-treemap-chart/.NET/Create-treemap-chart/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.OfficeChart;
+
+namespace Create_pie_chart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new Word document.
+            using (WordDocument document = new WordDocument())
+            {
+                
+                using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output.docx"), FileMode.Create, FileAccess.ReadWrite))
+                {
+                    //Save the Word document to the file stream.
+                    document.Save(outputFileStream, FormatType.Docx);
+                }
+            }
+        }
+    }
+}
