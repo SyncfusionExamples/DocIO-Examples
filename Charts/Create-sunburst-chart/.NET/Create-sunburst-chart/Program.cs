f15ccaad1cf45a28c8cd01104661e6dc991cc169
Sushma-SF4796 <sushma.princeedwin@syncfusion.com>
2025-09-12 16:25:36 +0530

    reverted changes of pie from sunburst sample

--- Charts/Create-sunburst-chart/.NET/Create-sunburst-chart/Program.cs
+++ Charts/Create-sunburst-chart/.NET/Create-sunburst-chart/Program.cs
@@ -12,40 +12,7 @@ namespace Create_pie_chart
             //Create a new Word document.
             using (WordDocument document = new WordDocument())
             {
-                //Add a section to the document.
-                IWSection section = document.AddSection();
-                //Add a paragraph to the section.
-                IWParagraph paragraph = section.AddParagraph();
-                //Create and append the chart to the paragraph.
-                WChart chart = paragraph.AppendChart(446, 270);
-                //Set chart type.
-                chart.ChartType = OfficeChartType.Pie;
-                //Set chart data.
-                chart.ChartData.SetValue(2, 1, "Food");
-                chart.ChartData.SetValue(3, 1, "Fruits");
-                chart.ChartData.SetValue(4, 1, "Vegetables");
-                chart.ChartData.SetValue(5, 1, "Dairy");
-                chart.ChartData.SetValue(6, 1, "Protein");
-                chart.ChartData.SetValue(7, 1, "Grains");
-                chart.ChartData.SetValue(2, 2, "Percentage");
-                chart.ChartData.SetValue(3, 2, 36);
-                chart.ChartData.SetValue(4, 2, 14);
-                chart.ChartData.SetValue(5, 2, 13);
-                chart.ChartData.SetValue(6, 2, 28);
-                chart.ChartData.SetValue(7, 2, 9);
-                //Set region of Chart data.
-                chart.DataRange = chart.ChartData[2, 1, 7, 2];
-                //Set chart series in the column for assigned data region.
-                chart.IsSeriesInRows = false;
-                //Set a Chart Title.
-                chart.ChartTitle = "Pie Chart";
-                //Set Datalabels.
-                IOfficeChartSerie serie = chart.Series[0];
-
-                serie.DataPoints.DefaultDataPoint.DataLabels.IsValue = true;
-                //Set legend.
-                chart.HasLegend = true;
-                chart.Legend.Position = OfficeLegendPosition.Bottom;
+                
                 //Create a file stream.
                 using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output.docx"), FileMode.Create, FileAccess.ReadWrite))
                 {
