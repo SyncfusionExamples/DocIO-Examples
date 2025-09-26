94993733d179613bb7108e4ee6adc233dcd0c78a
GOPINATHSF4767 <gopinath.natesan@syncfusion.com>
2025-09-26 23:43:04 +0530

    Added using

--- Paragraphs/Resize_Images_to_fit_Owner_Element/.NET/Resize_Images_to_fit_Owner_Element/Program.cs
+++ Paragraphs/Resize_Images_to_fit_Owner_Element/.NET/Resize_Images_to_fit_Owner_Element/Program.cs
@@ -7,16 +7,23 @@ namespace Resize_Images_to_fit_Owner_Element
     {
         static void Main(string[] args)
         {
-            // Create a new Word document instance
-            WordDocument document = new WordDocument();
-            // Enable automatic image resizing
-            document.Settings.ResizeImageToFitInContainer = true;
-            // Open the template document
-            document.Open(Path.GetFullPath(@"Data/InputDocument.docx"), FormatType.Docx);
-            // Save the modified document
-            document.Save(Path.GetFullPath(@"Output/Result.docx"), FormatType.Docx);
-            // Release resources
-            document.Dispose();
+            // Create a new Word document
+            using (WordDocument document = new WordDocument())
+            {
+                // Enable automatic image resizing
+                document.Settings.ResizeImageToFitInContainer = true;
+                // Open the template document
+                using (FileStream fileStreamPath = new FileStream(Path.GetFullPath(@"Data/InputDocument.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    document.Open(fileStreamPath, FormatType.Automatic);
+                }
+                //Creates file stream.
+                using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Result.docx"), FileMode.Create, FileAccess.ReadWrite))
+                {
+                    //Saves the Word document to file stream.
+                    document.Save(outputFileStream, FormatType.Docx);
+                }
+            }
         }
     }
 }
