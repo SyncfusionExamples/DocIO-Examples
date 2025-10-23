98a2c9971689c59e40711f3cbd61af5781565c12
Karan-SF4772 <karan.nagarathinam@syncfusion.com>
2025-10-23 13:20:52 +0530

    Added Word to PDF sample

---
+++ Word-to-PDF-Conversion/Convert-Word-document-to-PDF/ASP.NET-Core-Web-API/Client-Application/Client-Application/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Create an HttpClient instance
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                // Send a GET request to a URL
+                HttpResponseMessage response = await client.GetAsync("https://localhost:7240/api/Values/api/ConvertWordToPdf");
+
+                // Check if the response is successful
+                if (response.IsSuccessStatusCode)
+                {
+                    // Read the content as a string
+                    Stream responseBody = await response.Content.ReadAsStreamAsync();
+                    FileStream fileStream = File.Create("../../../Output/Output.pdf");
+                    responseBody.CopyTo(fileStream);
+                    fileStream.Close();
+                }
+                else
+                {
+                    Console.WriteLine("HTTP error status code: " + response.StatusCode);
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Request exception: " + e.Message);
+            }
+        }
+    }
+}
