ae43eb7a663d6ddef6a572123e5cb0170ba10bf5
Karan-SF4772 <karan.nagarathinam@syncfusion.com>
2025-10-23 13:08:17 +0530

    Add Open and Save Web API Sample

---
+++ Read-and-Save-document/Open-and-save-Word-document/ASP.NET-Core-Web-API/Open_and_save_Word_document/Open_and_save_Word_document/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Open_and_save_Word_document
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
