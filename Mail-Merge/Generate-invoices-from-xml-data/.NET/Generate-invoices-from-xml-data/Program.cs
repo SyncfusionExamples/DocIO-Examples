923b73da1b941637d41b15557beea7c211a03fea
Karan-SF4772 <karan.nagarathinam@syncfusion.com>
2025-12-15 13:02:08 +0530

    Added the Xml commands.

--- Mail-Merge/Generate-invoices-from-xml-data/.NET/Generate-invoices-from-xml-data/Program.cs
+++ Mail-Merge/Generate-invoices-from-xml-data/.NET/Generate-invoices-from-xml-data/Program.cs
@@ -10,9 +10,9 @@ namespace Generate_invoices_from_xml_data
         {
             // Load the template Word document
             WordDocument document = new WordDocument(Path.GetFullPath(@"Data/Template.docx"));
-            // To start each record at new page, so enable this property
+            // start each record at new page
             document.MailMerge.StartAtNewPage = true;
-            // Perform the mail merge for the group
+            // Perform mail merge using relational XML data
             document.MailMerge.ExecuteNestedGroup(GetRelationalData());
             // Save the result Word document.
             document.Save(Path.GetFullPath("../../../Output/Output.docx"));
@@ -20,9 +20,12 @@ namespace Generate_invoices_from_xml_data
             document.Close();
         }
         #region Helper Method
-        static MailMergeDataTable GetRelationalData()
+        /// <summary>
+        /// Retrieves relational invoice data from XML and converts it into a MailMergeDataTable.
+        /// </summary>
+        private static MailMergeDataTable GetRelationalData()
         {            
-            //Gets data from XML
+            // Load the XML data file
             Stream xmlStream = File.OpenRead(Path.GetFullPath(@"Data/InvoiceDetails.xml"));
             XmlDocument xmlDocument = new XmlDocument();
             xmlDocument.Load(xmlStream);
@@ -30,32 +33,34 @@ namespace Generate_invoices_from_xml_data
 
             ExpandoObject customerDetails = new ExpandoObject();
             GetDataAsExpandoObject((xmlDocument as XmlNode).LastChild, ref customerDetails);
-            // Treat customerDetails as a dictionary
+            // Convert dynamic object into dictionary
             IDictionary<string, object> customerDict = customerDetails as IDictionary<string, object>;
             // Get the "Invoices" list
             List<ExpandoObject> invoicesList = customerDict["Invoices"] as List<ExpandoObject>;
             // Take the first item in that list
             IDictionary<string, object> firstInvoiceGroup = invoicesList[0] as IDictionary<string, object>;
             // Get the "Invoice" list from that group
-            List<ExpandoObject>  invoices = firstInvoiceGroup["Invoice"] as List<ExpandoObject>;            
-            //Creates an instance of "MailMergeDataTable" by specifying mail merge group name and "IEnumerable" collection
+            List<ExpandoObject>  invoices = firstInvoiceGroup["Invoice"] as List<ExpandoObject>;
+            // Create MailMergeDataTable with group name "Invoices"
             MailMergeDataTable dataTable = new MailMergeDataTable("Invoices", invoices);
             return dataTable;
         }
         /// <summary>
         /// Gets the data as ExpandoObject.
         /// </summary>
-        /// <param name="reader">The reader.</param>
+        /// <param name="node">The current XML node being processed.</param>
+        /// <param name="dynamicObject">The dynamic object to populate with node data.</param>
         /// <returns></returns>
-        /// <exception cref="System.Exception">reader</exception>
-        /// <exception cref="XmlException">Unexpected xml tag  + reader.LocalName</exception>
         private static void GetDataAsExpandoObject(XmlNode node, ref ExpandoObject dynamicObject)
         {
             if (node.InnerText == node.InnerXml)
+                // Leaf node: store text value
                 dynamicObject.TryAdd(node.LocalName, node.InnerText);
             else
             {
+                // Handle child nodes
                 List<ExpandoObject> childObjects;
+                // If the tag already exists, reuse the list; otherwise create a new one
                 if ((dynamicObject as IDictionary<string, object>).ContainsKey(node.LocalName))
                     childObjects = (dynamicObject as IDictionary<string, object>)[node.LocalName] as List<ExpandoObject>;
                 else
@@ -63,11 +68,14 @@ namespace Generate_invoices_from_xml_data
                     childObjects = new List<ExpandoObject>();
                     dynamicObject.TryAdd(node.LocalName, childObjects);
                 }
+                // Create a new child object for the current node
                 ExpandoObject childObject = new ExpandoObject();
+                // Recursively process all child nodes
                 foreach (XmlNode childNode in (node as XmlNode).ChildNodes)
                 {
                     GetDataAsExpandoObject(childNode, ref childObject);
                 }
+                // Add the processed child object to the list
                 childObjects.Add(childObject);
             }
         }
