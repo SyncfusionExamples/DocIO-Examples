1309772b1ecdb6fb9c75e643db5337287f80e020
Kathiresan4347 <159137198+Kathiresan4347@users.noreply.github.com>
2025-12-29 18:57:05 +0530

    ES-999797-Changes Added

---
+++ HTML-conversions/Convert-HTML-with-CSS-Selctor-to-Word/.NET/Convert-HTML-to-Word/Program.cs
@@ -0,0 +1,25 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+
+namespace Convert_HTML_to_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Loads an existing Word document into DocIO instance.
+            using (FileStream fileStreamPath = new FileStream(Path.GetFullPath(@"Data/Input.html"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (WordDocument document = new WordDocument(fileStreamPath, FormatType.Html))
+                {
+                    //Creates file stream.
+                    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Result.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        //Saves the Word document to file stream.
+                        document.Save(outputFileStream, FormatType.Docx);
+                    }
+                }
+            }
+        }
+    }
+}
