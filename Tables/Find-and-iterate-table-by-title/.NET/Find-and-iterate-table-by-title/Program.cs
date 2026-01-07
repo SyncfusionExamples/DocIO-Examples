f4f5108d1a95c079ab17b8eef0b727715123f236
Kathiresan4347 <159137198+Kathiresan4347@users.noreply.github.com>
2026-01-07 18:20:53 +0530

    ES-992086-Changes added

---
+++ Tables/Find-and-iterate-table-by-title/.NET/Find-and-iterate-table-by-title/Program.cs
@@ -0,0 +1,47 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.Drawing;
+
+namespace Find_and_iterate_table_by_title
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {        
+            using (FileStream fileStream = new FileStream(Path.GetFullPath(@"Data/Template.docx"), FileMode.Open, FileAccess.ReadWrite))
+            {
+                //Opens an existing Word document.
+                using (WordDocument document = new WordDocument(fileStream, FormatType.Automatic))
+                {
+                    // Find the table with title.
+                    WTable table = document.FindItemByProperty(EntityType.Table, "Title", "Overview") as WTable;
+                    if (table != null)
+                    {
+                        // Iterate through the rows and cells of the table
+                        foreach (WTableRow row in table.Rows)
+                        {
+                            //Iterates through the cells of rows.
+                            foreach (WTableCell cell in row.Cells)
+                            {
+                                //Iterates through the paragraphs of the cell.
+                                foreach (WParagraph paragraph in cell.Paragraphs)
+                                {
+                                    //When the paragraph contains text Panda then apply green as back color to cell.
+                                    if (paragraph.Text.Contains("panda"))
+                                        cell.CellFormat.BackColor = Color.Coral;
+                                }
+                            }
+                        }
+                    }
+                    //Creates file stream.
+                    using (FileStream outputStream = new FileStream(Path.GetFullPath(@"Output/Result.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        //Saves the Word document to file stream.
+                        document.Save(outputStream, FormatType.Docx);
+                    }
+                }
+            }
+        }
+    }
+}
+
