2ec024fc2de127b5a1aab7e475cfc08f71c69a3c
GOPINATHSF4767 <gopinath.natesan@syncfusion.com>
2026-02-06 10:53:43 +0530

    Modified sample

--- Paragraphs/Apply_Style_for_TextRange_in_Paragraph/.NET/Apply_Style_for_TextRange_in_Paragraph/Program.cs
+++ Paragraphs/Apply_Style_for_TextRange_in_Paragraph/.NET/Apply_Style_for_TextRange_in_Paragraph/Program.cs
@@ -7,26 +7,32 @@ namespace Apply_Style_for_TextRange_in_Paragraph
     {
         static void Main(string[] args)
         {
-            // Create a new Word document
-            using (WordDocument document = new WordDocument(Path.GetFullPath(@"../../../Data/Template.docx")))
+            using (FileStream fileStream = new FileStream(Path.GetFullPath(@"Data/Input.docx"), FileMode.Open))
             {
-                WCharacterStyle customStyle = document.AddCharacterStyle("MyCustomStyle") as WCharacterStyle;
+                //Loads an existing Word document.
+                using (WordDocument wordDocument = new WordDocument(fileStream, FormatType.Automatic))
+                {
+                    WCharacterStyle customStyle = wordDocument.AddCharacterStyle("MyCustomStyle") as WCharacterStyle;
 
-                //Set style properties
-                customStyle.CharacterFormat.FontName = "Calibri";
-                customStyle.CharacterFormat.FontSize = 18;
-                customStyle.CharacterFormat.Bold = true;
-                customStyle.CharacterFormat.UnderlineStyle = Syncfusion.Drawing.UnderlineStyle.Single;
-                //Get text range
-                WTextRange textRange = null;
-                if (document.LastParagraph.ChildEntities.Count > 0)
-                    textRange = document.LastParagraph.ChildEntities[0] as WTextRange;
-                //Apply custom style
-                if (textRange != null)
-                    textRange.ApplyStyle("MyCustomStyle");
-
-                //Saves the Word document.
-                document.Save(Path.GetFullPath(@"../../../Output/Result.docx"), FormatType.Docx);
+                    //Set style properties
+                    customStyle.CharacterFormat.FontName = "Calibri";
+                    customStyle.CharacterFormat.FontSize = 18;
+                    customStyle.CharacterFormat.Bold = true;
+                    customStyle.CharacterFormat.UnderlineStyle = Syncfusion.Drawing.UnderlineStyle.Single;
+                    //Get text range
+                    WTextRange textRange = null;
+                    if (wordDocument.LastParagraph.ChildEntities.Count > 0)
+                        textRange = wordDocument.LastParagraph.ChildEntities[0] as WTextRange;
+                    //Apply custom style
+                    if (textRange != null)
+                        textRange.ApplyStyle("MyCustomStyle");
+                    //Creates file stream.
+                    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Result.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        //Saves the Word document to file stream.
+                        wordDocument.Save(outputFileStream, FormatType.Docx);
+                    }
+                }
             }
         }
     }
