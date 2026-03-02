9e41b5f8f9998fa0a6c98d82310071c04ba7e14e
GOPINATHSF4767 <gopinath.natesan@syncfusion.com>
2026-03-02 14:49:54 +0530

    Added sample

---
+++ Word-document/Identify-Word-document-is-Encrypted-or-not/.NET/Identify-Word-document-is-Encrypted-or-not/Program.cs
@@ -0,0 +1,37 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System;
+using System.IO;
+
+namespace Identify_Word_document_is_Encrypted_or_not
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creates a new Word document.
+            using (FileStream fileStreamPath = new FileStream(Path.GetFullPath(@"Data/Template.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                Console.WriteLine(IsEncrypted(fileStreamPath));
+            }
+        }
+        public static string IsEncrypted(FileStream fileStream)
+        {
+            try
+            {
+                //Open the existing Word document.
+                WordDocument document = new WordDocument(fileStream, FormatType.Docx);
+                return "Docuemnt is not encrypted.";
+            }
+            catch (Exception exception)
+            {
+                //Return if Word document is encrypted.
+                if (exception.Message == "Document is encrypted, password is needed to open the document")
+                {
+                    return exception.Message;
+                }
+            }
+            return "Docuemnt is not encrypted.";
+        }
+    }
+}
