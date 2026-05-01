091a574ec3e122712dfd72f8ab7b8f361dfb5245
GOPINATHSF4767 <gopinath.natesan@syncfusion.com>
2026-05-01 15:41:02 +0530

    Modified code

--- Sections/Import-Headers-and-Footers-from-Another-WordDocument/.NET/Import-Headers-and-Footers-from-Another-WordDocument/Program.cs
+++ Sections/Import-Headers-and-Footers-from-Another-WordDocument/.NET/Import-Headers-and-Footers-from-Another-WordDocument/Program.cs
@@ -12,24 +12,24 @@ namespace Import_Headers_and_Footers_from_Another_WordDocument
                 using (FileStream destinationStreamPath = new FileStream(Path.GetFullPath("Data/DestinationDocument.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
                 {
                     //Opens an source document from file system through constructor of WordDocument class
-                    using (WordDocument document = new WordDocument(sourceStreamPath, FormatType.Automatic))
+                    using (WordDocument sourceDocument = new WordDocument(sourceStreamPath, FormatType.Automatic))
                     {
                         //Opens the destination document 
                         using (WordDocument destinationDocument = new WordDocument(destinationStreamPath, FormatType.Docx))
                         {
 
-                            WSection section = document.Sections[0] as WSection;
+                            WSection sourceDocumentSection = sourceDocument.Sections[0] as WSection;
                             //Move all the items from one collection to another collection
                             for (int i = 0; i < destinationDocument.Sections.Count; i++)
                             {
-                                MoveItems(destinationDocument.Sections[i].HeadersFooters.EvenHeader.ChildEntities, section.HeadersFooters.Header.ChildEntities);
-                                MoveItems(destinationDocument.Sections[i].HeadersFooters.EvenFooter.ChildEntities, section.HeadersFooters.Footer.ChildEntities);
-                                MoveItems(destinationDocument.Sections[i].HeadersFooters.OddHeader.ChildEntities, section.HeadersFooters.Header.ChildEntities);
-                                MoveItems(destinationDocument.Sections[i].HeadersFooters.OddFooter.ChildEntities, section.HeadersFooters.Footer.ChildEntities);
-                                MoveItems(destinationDocument.Sections[i].HeadersFooters.FirstPageHeader.ChildEntities, section.HeadersFooters.Header.ChildEntities);
-                                MoveItems(destinationDocument.Sections[i].HeadersFooters.FirstPageFooter.ChildEntities, section.HeadersFooters.Footer.ChildEntities);
-                                destinationDocument.Sections[i].PageSetup.DifferentOddAndEvenPages = section.PageSetup.DifferentOddAndEvenPages;
-                                destinationDocument.Sections[i].PageSetup.DifferentFirstPage = section.PageSetup.DifferentFirstPage;
+                                MoveItems(destinationDocument.Sections[i].HeadersFooters.EvenHeader.ChildEntities, sourceDocumentSection.HeadersFooters.Header.ChildEntities);
+                                MoveItems(destinationDocument.Sections[i].HeadersFooters.EvenFooter.ChildEntities, sourceDocumentSection.HeadersFooters.Footer.ChildEntities);
+                                MoveItems(destinationDocument.Sections[i].HeadersFooters.OddHeader.ChildEntities, sourceDocumentSection.HeadersFooters.Header.ChildEntities);
+                                MoveItems(destinationDocument.Sections[i].HeadersFooters.OddFooter.ChildEntities, sourceDocumentSection.HeadersFooters.Footer.ChildEntities);
+                                MoveItems(destinationDocument.Sections[i].HeadersFooters.FirstPageHeader.ChildEntities, sourceDocumentSection.HeadersFooters.Header.ChildEntities);
+                                MoveItems(destinationDocument.Sections[i].HeadersFooters.FirstPageFooter.ChildEntities, sourceDocumentSection.HeadersFooters.Footer.ChildEntities);
+                                destinationDocument.Sections[i].PageSetup.DifferentOddAndEvenPages = sourceDocumentSection.PageSetup.DifferentOddAndEvenPages;
+                                destinationDocument.Sections[i].PageSetup.DifferentFirstPage = sourceDocumentSection.PageSetup.DifferentFirstPage;
                             }
                             using (FileStream outputStream = new FileStream(Path.GetFullPath("Output/Result.docx"), FileMode.Create))
                             {
@@ -43,13 +43,12 @@ namespace Import_Headers_and_Footers_from_Another_WordDocument
         /// <summary>
         /// Move all the items from one collection to another collection.
         /// </summary>
-        /// <param name="childEntities1"></param>
-        /// <param name="childEntities2"></param>
-        private static void MoveItems(EntityCollection childEntities1, EntityCollection childEntities2)
+        /// <param name="detinationDocumentEntityCollection">Destination collection</param>
+        /// <param name="sourceDocumentEntityCollection">Source collection</param>
+        private static void MoveItems(EntityCollection detinationDocumentEntityCollection, EntityCollection sourceDocumentEntityCollection)
         {
-            for (int i = 0; i < childEntities2.Count; i++)
-                childEntities1.Add(childEntities2[i].Clone());
+            for (int i = 0; i < sourceDocumentEntityCollection.Count; i++)
+                detinationDocumentEntityCollection.Add(sourceDocumentEntityCollection[i].Clone());
         }
     }
-    }
-
+}
