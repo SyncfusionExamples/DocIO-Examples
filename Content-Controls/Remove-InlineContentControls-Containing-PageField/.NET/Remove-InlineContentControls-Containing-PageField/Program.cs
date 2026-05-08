91fe3f32550ac7fe218f6f75d5aceb380cb69675
Karan-SF4772 <karan.nagarathinam@syncfusion.com>
2026-05-08 09:33:56 +0530

    Addressed the feedback

--- Content-Controls/Remove-InlineContentControls-Containing-PageField/.NET/Remove-InlineContentControls-Containing-PageField/Program.cs
+++ Content-Controls/Remove-InlineContentControls-Containing-PageField/.NET/Remove-InlineContentControls-Containing-PageField/Program.cs
@@ -7,46 +7,39 @@ namespace Remove_InlineContentControls_Containing_PageField
     {
         public static void Main(string[] args)
         {            
-            //Load existing Word document
+            // Load existing Word document
             using (WordDocument document = new WordDocument(Path.GetFullPath(@"Data/Input.docx"), FormatType.Docx))
             {
-                //Find Content control
-                List<Entity> entities = document.FindAllItemsByProperties(EntityType.InlineContentControl, null, null);
+                // Find all PAGE fields in the document
+                List<Entity> pageFields = document.FindAllItemsByProperty(EntityType.Field, "FieldType", "FieldPage");
                 // Iterate through the list of entities in reverse to safely remove items during iteration
-                for (int i = entities.Count - 1; i >= 0; i--)
+                for (int i = pageFields.Count - 1; i >= 0; i--)
                 {
-                    // Attempt to cast the current entity to an InlineContentControl
-                    InlineContentControl inLineContentControl = entities[i] as InlineContentControl;
-                    if (inLineContentControl != null)
-                    {                       
-                        // Iterate through the child items of the content control
-                        foreach (ParagraphItem entity in inLineContentControl.ParagraphItems)
+                    // Attempt to cast the current entity to an WField
+                    WField pageField = pageFields[i] as WField;
+                    if (pageField != null)
+                    {
+                        // Check the direct parent of PAGE field is InlineContentControl
+                        if (pageField.Owner is InlineContentControl)
                         {
-                            // Check if the entity is a PAGE field
-                            if (entity is WField field && field.FieldType == FieldType.FieldPage)
+                            InlineContentControl inlineContentControl = pageField.Owner as InlineContentControl;
+                            // If the content control is nested inside another content control
+                            if (inlineContentControl.Owner is InlineContentControl)
                             {
-                                // If the content control is nested inside another content control
-                                if (inLineContentControl.Owner is InlineContentControl)
-                                {
-                                    InlineContentControl owner = inLineContentControl.Owner as InlineContentControl;
-                                    // Remove the current content control from its parent content control
-                                    owner.ParagraphItems.Remove(inLineContentControl);
-                                }
-                                // If the content control is directly inside a paragraph
-                                else if (inLineContentControl.Owner is WParagraph)
-                                {
-                                    WParagraph parentParagraph = inLineContentControl.Owner as WParagraph;
-                                    // Remove the content control from the paragraph
-                                    parentParagraph.ChildEntities.Remove(inLineContentControl);
-                                }
-                                // Remove the content control from the main entity list
-                                entities.RemoveAt(i);
-                                // Exit the inner loop since the control has been removed
-                                break;
+                                InlineContentControl owner = inlineContentControl.Owner as InlineContentControl;
+                                // Remove the current content control from its parent content control
+                                owner.ParagraphItems.Remove(inlineContentControl);
+                            }
+                            // If the content control is directly inside a paragraph
+                            else if (inlineContentControl.Owner is WParagraph)
+                            {
+                                WParagraph parentParagraph = inlineContentControl.Owner as WParagraph;
+                                // Remove the content control from the paragraph
+                                parentParagraph.ChildEntities.Remove(inlineContentControl);
                             }
                         }
                     }
-                }
+                }               
                 // Save the document if needed
                 using (FileStream fileStream = new FileStream(@"../../../Output/Output.docx", FileMode.OpenOrCreate, FileAccess.ReadWrite))
                 {
