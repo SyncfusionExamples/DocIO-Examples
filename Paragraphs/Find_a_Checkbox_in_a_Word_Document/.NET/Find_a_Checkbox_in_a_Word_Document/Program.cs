d2dde85f5de8e7ce8e85197bb5b7d541deb073f4
GOPINATHSF4767 <gopinath.natesan@syncfusion.com>
2026-05-18 13:19:02 +0530

    Modified sample

--- Paragraphs/Find_a_Checkbox_in_a_Word_Document/.NET/Find_a_Checkbox_in_a_Word_Document/Program.cs
+++ Paragraphs/Find_a_Checkbox_in_a_Word_Document/.NET/Find_a_Checkbox_in_a_Word_Document/Program.cs
@@ -5,110 +5,50 @@ namespace Find_a_Checkbox_in_a_Word_Document
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            // Load the word document
-            using (FileStream fileStream = new FileStream("../../../Data/Template.docx", FileMode.Open, FileAccess.Read))
+            // Opens the input Word document from the specified path
+            using (FileStream fileStream = new FileStream(Path.GetFullPath("Data/Template.docx"), FileMode.Open, FileAccess.Read))
             {
+                // Loads the Word document into DocIO
                 using (WordDocument document = new WordDocument(fileStream, FormatType.Docx))
                 {
-                    foreach (WSection section in document.Sections)
+                    // Finds all Block Content Controls of type CheckBox in the document
+                    List<Entity> blockContentControls = document.FindAllItemsByProperty(EntityType.BlockContentControl, "ContentControlProperties.Type", ContentControlType.CheckBox.ToString());
+
+                    // Verifies if any block-level checkbox content controls are found
+                    if (blockContentControls != null)
                     {
-                        //Accesses the Body of section where all the contents in document are apart
-                        WTextBody sectionBody = section.Body;
-                        IterateTextBody(sectionBody);
-                        WHeadersFooters headersFooters = section.HeadersFooters;
-                        //Consider that OddHeader and OddFooter are applied to this document
-                        //Iterates through the TextBody of OddHeader and OddFooter
-                        IterateTextBody(headersFooters.OddHeader);
-                        IterateTextBody(headersFooters.OddFooter);
+                        foreach (Entity entity in blockContentControls)
+                        {
+                            // Cast the entity to BlockContentControl
+                            BlockContentControl blockContentControl = entity as BlockContentControl;
+                            // Unchecks the checkbox
+                            blockContentControl.ContentControlProperties.IsChecked = false;
+                        }
                     }
-                    using (FileStream outputStream = new FileStream("../../../Output/Result.docx", FileMode.Create, FileAccess.Write))
+
+                    // Finds all Inline Content Controls of type CheckBox in the document
+                    List<Entity> inlineContentControls = document.FindAllItemsByProperty(EntityType.InlineContentControl, "ContentControlProperties.Type", ContentControlType.CheckBox.ToString());
+
+                    // Verifies if any inline checkbox content controls are found
+                    if (inlineContentControls != null)
                     {
-                        //Saves the stream as Word file
+                        foreach (Entity entity in inlineContentControls)
+                        {
+                            // Cast the entity to InlineContentControl
+                            InlineContentControl inlineContentControl = entity as InlineContentControl;
+                            // Unchecks the checkbox
+                            inlineContentControl.ContentControlProperties.IsChecked = false;
+                        }
+                    }
+
+                    // Creates a file stream for the output document
+                    using (FileStream outputStream = new FileStream(Path.GetFullPath("Output/Result.docx"), FileMode.Create, FileAccess.Write))
+                    {
+                        // Saves the modified document with updated checkbox states
                         document.Save(outputStream, FormatType.Docx);
                     }
-                }
-            }
-        }
-        public static void IterateTextBody(WTextBody textBody)
-        {
-            //Iterates through each of the child items of WTextBody
-            for (int i = 0; i < textBody.ChildEntities.Count; i++)
-            {
-                //IEntity is the basic unit in DocIO DOM. 
-                //Accesses the body items (should be either paragraph, table or block content control) as IEntity
-                IEntity bodyItemEntity = textBody.ChildEntities[i];
-                //A Text body has 3 types of elements - Paragraph, Table and Block Content Control
-                //Decides the element type by using EntityType
-                switch (bodyItemEntity.EntityType)
-                {
-                    case EntityType.Paragraph:
-                        WParagraph paragraph = bodyItemEntity as WParagraph;
-                        //Processes the paragraph contents
-                        //Iterates through the paragraph's DOM
-                        IterateParagraph(paragraph.Items);
-                        break;
-                    case EntityType.Table:
-                        //Table is a collection of rows and cells
-                        //Iterates through table's DOM
-                        IterateTable(bodyItemEntity as WTable);
-                        break;
-                    case EntityType.BlockContentControl:
-                        BlockContentControl blockContentControl = bodyItemEntity as BlockContentControl;
-                        if (blockContentControl.ContentControlProperties.Type == ContentControlType.CheckBox)
-                            blockContentControl.ContentControlProperties.IsChecked = false;
-                        //Iterates to the body items of Block Content Control.
-                        IterateTextBody(blockContentControl.TextBody);
-                        break;
-                }
-            }
-        }
-        public static void IterateTable(WTable table)
-        {
-            //Iterates the row collection in a table
-            foreach (WTableRow row in table.Rows)
-            {
-                //Iterates the cell collection in a table row
-                foreach (WTableCell cell in row.Cells)
-                {
-                    //Table cell is derived from (also a) TextBody
-                    //Reusing the code meant for iterating TextBody
-                    IterateTextBody(cell);
-                }
-            }
-        }
-        public static void IterateParagraph(ParagraphItemCollection paraItems)
-        {
-            for (int i = 0; i < paraItems.Count; i++)
-            {
-                Entity entity = paraItems[i];
-                //A paragraph can have child elements such as text, image, hyperlink, symbols, etc.,
-                //Decides the element type by using EntityType
-                switch (entity.EntityType)
-                {
-                    case EntityType.CheckBox:
-                        if (entity is WCheckBox)
-                            (entity as WCheckBox).Checked = false;
-                        break;
-                    case EntityType.TextBox:
-                        //Iterates to the body items of textbox.
-                        WTextBox textBox = entity as WTextBox;
-                        IterateTextBody(textBox.TextBoxBody);
-                        break;
-                    case EntityType.Shape:
-                        //Iterates to the body items of shape.
-                        Shape shape = entity as Shape;
-                        IterateTextBody(shape.TextBody);
-                        break;
-                    case EntityType.InlineContentControl:
-                        //Iterates to the paragraph items of inline content control.
-                        InlineContentControl inlineContentControl = entity as InlineContentControl;
-                        if (inlineContentControl.ContentControlProperties.Type == ContentControlType.CheckBox)
-                            inlineContentControl.ContentControlProperties.IsChecked = false;
-                        IterateParagraph(inlineContentControl.ParagraphItems);
-                        break;
                 }
             }
         }
