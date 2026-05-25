f5350064955f4714b6f1dca745d3194bda645e10
GOPINATHSF4767 <gopinath.natesan@syncfusion.com>
2026-05-25 13:53:08 +0530

    Added sample

---
+++ Getting-Started/Blazor/BlazorApp-WASM-PWA/BlazorApp-WASM-PWA/FileUtils.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JSInterop;
+
+
+public static class FileUtils
+{
+    public static ValueTask<object> SaveAs(this IJSRuntime js, string filename, byte[] data)
+       => js.InvokeAsync<object>(
+            "saveAsFile",
+            filename,
+            Convert.ToBase64String(data));
+}
+
