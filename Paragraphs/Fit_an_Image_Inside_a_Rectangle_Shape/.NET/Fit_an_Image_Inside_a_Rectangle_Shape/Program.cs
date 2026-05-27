74f1c2fa79f5017ef740488265b4ec5e9befedcf
GOPINATHSF4767 <gopinath.natesan@syncfusion.com>
2026-05-27 14:05:33 +0530

    Modified sample

--- Paragraphs/Fit_an_Image_Inside_a_Rectangle_Shape/.NET/Fit_an_Image_Inside_a_Rectangle_Shape/Program.cs
+++ Paragraphs/Fit_an_Image_Inside_a_Rectangle_Shape/.NET/Fit_an_Image_Inside_a_Rectangle_Shape/Program.cs
@@ -1,5 +1,5 @@
-﻿using Syncfusion.DocIO.DLS;
-using Syncfusion.DocIO;
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
 
 
 namespace Fit_an_Image_Inside_a_Rectangle_Shape
@@ -8,33 +8,25 @@ namespace Fit_an_Image_Inside_a_Rectangle_Shape
     {
         static void Main(string[] args)
         {
-            //Create a new Word document
-            using (WordDocument document = new WordDocument())
+            // Open the input Word document as a file stream
+            using (FileStream inputStream = new FileStream(Path.GetFullPath(@"../../../Data/Template.docx"), FileMode.Open, FileAccess.Read))
             {
-                //Add a new section
-                WSection section = document.AddSection() as WSection;
-                //Add a new paragraph to the section.
-                WParagraph paragraph = section.AddParagraph() as WParagraph;
-                //Add a new rectangle shape
-                Shape rectangle = paragraph.AppendShape(AutoShapeType.Rectangle, 150, 100);
-                //Format the rectangle shape
-                rectangle.VerticalPosition = 72;
-                rectangle.HorizontalPosition = 72;
-                //Add a new paragraph to a rectangle shape
-                WParagraph para = rectangle.TextBody.AddParagraph() as WParagraph;
-                //Append the picture to the paragraph
-                WPicture picture = para.AppendPicture(File.ReadAllBytes("../../../Data/Mountain-200.jpg")) as WPicture;
-                //Resize the picture according to rectangle shape
-                picture.Width = rectangle.Width;
-                picture.Height = rectangle.Height;
-                picture.VerticalPosition = rectangle.VerticalPosition;
-                picture.HorizontalPosition = rectangle.HorizontalPosition;
-                //document.Settings.ResizeImageToFitInContainer = true;
-                //Creates file stream.
-                using (FileStream outputStream = new FileStream(Path.GetFullPath(@"../../../Output/Result.docx"), FileMode.Create))
+                // Load the Word document
+                using (WordDocument document = new WordDocument(inputStream, FormatType.Docx))
                 {
-                    //Saves the Word document to file stream.
-                    document.Save(outputStream, FormatType.Docx);
+                    Entity entity = document.FindItemByProperty(EntityType.Picture, "Title", "Product");
+                    WPicture picture = entity as WPicture;
+                    if(picture.OwnerParagraph.OwnerTextBody.Owner is WTextBox shape)
+                    {
+                        picture.Height = shape.TextBoxFormat.Height;
+                        picture.Width = shape.TextBoxFormat.Width;
+                    }
+                    //Creates file stream.
+                    using (FileStream outputStream = new FileStream(Path.GetFullPath(@"../../../Output/Result.docx"), FileMode.Create))
+                    {
+                        //Saves the Word document to file stream.
+                        document.Save(outputStream, FormatType.Docx);
+                    }
                 }
             }
         }
