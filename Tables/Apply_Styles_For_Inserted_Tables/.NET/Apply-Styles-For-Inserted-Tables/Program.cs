cbfb9e2d5cf316a50e093c882c85a3771fe756b7
GOPINATHSF4767 <gopinath.natesan@syncfusion.com>
2026-06-30 10:14:00 +0530

    Modified sample

--- Tables/Apply_Styles_For_Inserted_Tables/.NET/Apply-Styles-For-Inserted-Tables/Program.cs
+++ Tables/Apply_Styles_For_Inserted_Tables/.NET/Apply-Styles-For-Inserted-Tables/Program.cs
@@ -8,48 +8,39 @@ namespace Apply_Styles_For_Inserted_Tables
     {
         static void Main(string[] args)
         {
-            // Open an existing word document 
-            using (FileStream inputFileStream = new FileStream(Path.GetFullPath(@"Data/Template.docx"), FileMode.Open))
-            { 
-                // Opens an input Word template
-                using (WordDocument resultDocument = new WordDocument(inputFileStream, FormatType.Docx))
+            // Opens an input Word template
+            using (WordDocument resultDocument = new WordDocument(Path.GetFullPath(@"../../../Data/Template.docx")))
+            {
+                // Read HTML string from the file.
+                string html = File.ReadAllText(Path.GetFullPath(@"../../../Data/Table.html"));
+
+                // Insert HTML (table style is not applied automatically)
+                resultDocument.LastSection.Body.InsertXHTML(html);
+
+                // Append table manually - this one applies the table style from the template
+                IWTable table = resultDocument.LastSection.AddTable();
+                //Specify the total number of rows and columns. 
+                table.ResetCells(2, 6);
+
+                for (int rowIndex = 0; rowIndex < 2; rowIndex++)
+                    for (int columnIndex = 0; columnIndex < 6; columnIndex++)
+                        table.Rows[rowIndex].Cells[columnIndex].AddParagraph().Text = (rowIndex * columnIndex).ToString();
+
+                //Finds all the table in the Word document
+                List<Entity> tableList = resultDocument.FindAllItemsByProperty(EntityType.Table, "EntityType", EntityType.Table.ToString());
+                foreach (var item in tableList)
                 {
-                    // Read HTML string from the file.
-                    string html = File.ReadAllText(Path.GetFullPath(@"Data/Table.html"));
-
-                    // Insert HTML (table style is not applied automatically)
-                    resultDocument.LastSection.Body.InsertXHTML(html);
-
-                    // Append table manually - this one applies the table style from the template
-                    WTable table = resultDocument.LastSection.AddTable() as WTable;
-                    WTableRow row = table.AddRow();
-                    row.AddCell();
-                    row.AddCell();
-                    row.AddCell();
-                    row.AddCell();
-                    row.AddCell();
-                    row.AddCell();
-                    table.AddRow();
-
-                    for (int rowIndex = 0; rowIndex < 2; rowIndex++)
-                        for (int columnIndex = 0; columnIndex < 6; columnIndex++)
-                            table.Rows[rowIndex].Cells[columnIndex].AddParagraph().Text = (rowIndex * columnIndex).ToString();
-
-                    //Finds all the table in the Word document
-                    List<Entity> tableList = resultDocument.FindAllItemsByProperty(EntityType.Table, "EntityType", EntityType.Table.ToString());
-                    foreach (var item in tableList)
-                    {
-                        WTable tableInDocument = item as WTable;
-                        //Apply table style "TableGrid" to the table
-                        tableInDocument.ApplyStyle(BuiltinTableStyle.TableGrid);
-                    }
-                    // Save the document to output file
-                    using (FileStream outputStream = new FileStream(Path.GetFullPath(@"Output/Result.docx"), FileMode.Create))
-                    {
-                        resultDocument.Save(outputStream, FormatType.Docx);
-                    }
+                    WTable tableInDocument = item as WTable;
+                    //Apply table style "TableGrid" to the table
+                    tableInDocument.ApplyStyle(BuiltinTableStyle.TableGrid);
                 }
-            } 
-        }
+                // Save the document to output file
+                using (FileStream outputStream = new FileStream(Path.GetFullPath(@"../../../Output/Result.docx"), FileMode.Create))
+                {
+                    resultDocument.Save(outputStream, FormatType.Docx);
+                }
+            }
+        } 
     }
 }
+
