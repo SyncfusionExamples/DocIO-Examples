6c0e706edf73f4ea32695b4c51cb7cc472ef32d6
AtchayaSekar28 <atchaya.sekar@syncfusion.com>
2026-07-16 00:33:06 +0530

    1041234-Added GitHub sample for Extract Text in DocIO Examples

---
+++ Word-document/Extract-Text-From-Word-Document/.NET/Extract-Text-From-Word-Document/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+
+// Loads a template document
+WordDocument document = new WordDocument(Path.GetFullPath(@"Data/Template.docx"));
+// Gets the document text
+string text = document.GetText();
+// Prints the extracted text to the console
+Console.WriteLine(text);
+// Dispose the document instance 
+document.Close();
