e236c9dd121be7cdaaa6e8f258a8c091fa9f2bc5
GOPINATHSF4767 <gopinath.natesan@syncfusion.com>
2026-07-27 12:08:22 +0530

    Added samples

---
+++ Word-to-Markdown-conversion/Convert-Word-to-Markdown_with_Encoding/.NET/Convert-Word-to-Markdown_with_Encoding/Program.cs
@@ -0,0 +1,21 @@
+﻿using Syncfusion.DocIO.DLS;
+using System.IO;
+using System.Text;
+
+namespace Convert_Word_to_Markdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Open an existing Word document.
+            using (WordDocument document = new WordDocument(Path.GetFullPath(@"Data/Input.docx")))
+            {
+                //Set the encoding values.
+                document.SaveOptions.MarkdownSaveOptions.Encoding = Encoding.ASCII;
+                //Save the document as a Markdown file.
+                document.Save(Path.GetFullPath(@"Output/Output.md"));
+            }
+        }
+    }
+}
---
+++ Word-to-Markdown-conversion/Get_Markdown_document/.NET/Get_Markdown_document/Program.cs
@@ -0,0 +1,23 @@
+﻿using Syncfusion.DocIO.DLS;
+using Syncfusion.Office.Markdown;
+using System.IO;
+
+namespace Convert_Word_to_Markdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Open an existing Word document.
+            using (WordDocument document = new WordDocument(Path.GetFullPath(@"Data/Input.docx")))
+            {
+                // Convert the Word document to Markdown.
+                MarkdownDocument markdownDocument = document.GetMarkdownDocument();
+                // Save or process the Markdown document as needed.
+                markdownDocument.Save(Path.GetFullPath(@"Output/Output.md"));
+                // Dispose the Markdown document.
+                markdownDocument.Dispose();
+            }
+        }
+    }
+}
