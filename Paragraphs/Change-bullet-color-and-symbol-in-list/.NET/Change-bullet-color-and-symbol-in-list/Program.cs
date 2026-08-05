9d0b2bb82515c22cbee7d86975861750a2334f09
SushmaPrinceEdwin <sushma.princeedwin@syncfusion.com>
2026-08-05 22:16:33 +0530

    Addressed the feedback

--- Paragraphs/Change-bullet-color-and-symbol-in-list/.NET/Change-bullet-color-and-symbol-in-list/Program.cs
+++ Paragraphs/Change-bullet-color-and-symbol-in-list/.NET/Change-bullet-color-and-symbol-in-list/Program.cs
@@ -1,36 +1,25 @@
 ﻿using Syncfusion.DocIO;
 using Syncfusion.DocIO.DLS;
-using System.IO;
 using Syncfusion.Drawing;
 
-namespace Change_bullet_color_and_symbol_in_list
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        //Load the input Word document.
+        using (WordDocument document = new WordDocument(@"Data/Input.docx", FormatType.Docx))
         {
-            //Open the file as a stream.
-            using (FileStream inputStream = new FileStream(Path.GetFullPath(@"Data/Input.docx"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            {
-                //Load the file stream into a Word document.
-                using (WordDocument document = new WordDocument(inputStream, FormatType.Docx))
-                {
-                    //Access the list style in a Word document.
-                    ListStyle style = document.ListStyles[0];
-                    WListLevel levelOne = style.Levels[0];
-                    //Define the character and pattern for level 1.
-                    levelOne.PatternType = ListPatternType.Bullet;
-                    levelOne.BulletCharacter = "\u0076";
-                    levelOne.CharacterFormat.FontName = "Wingdings";
-					levelOne.CharacterFormat.TextColor = Color.Red;
-                    //Create a file stream.
-                    using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Sample.docx"), FileMode.Create, FileAccess.ReadWrite))
-                    {
-                        //Save the Word document to the file stream.
-                        document.Save(outputFileStream, FormatType.Docx);
-                    }
-                }
-            }
+            //Access the list style in a Word document.
+            ListStyle style = document.ListStyles[0];
+            WListLevel levelOne = style.Levels[0];
+            //Define the character and pattern for level 1.
+            levelOne.PatternType = ListPatternType.Bullet;
+            levelOne.BulletCharacter = "\u0076";
+            levelOne.CharacterFormat.FontName = "Wingdings";
+            levelOne.CharacterFormat.TextColor = Color.Red;
+            //Save the Word document.
+            document.Save(@"Output/Sample.docx", FormatType.Docx);
         }
     }
 }
+
