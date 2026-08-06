14c5b6407e89afe88022f506de88d29db1f13cab
SushmaPrinceEdwin <sushma.princeedwin@syncfusion.com>
2026-08-06 11:41:39 +0530

    Addressed the feedback

--- Paragraphs/Change-bullet-color-and-symbol-in-list/.NET/Change-bullet-color-and-symbol-in-list/Program.cs
+++ Paragraphs/Change-bullet-color-and-symbol-in-list/.NET/Change-bullet-color-and-symbol-in-list/Program.cs
@@ -2,24 +2,19 @@
 using Syncfusion.DocIO.DLS;
 using Syncfusion.Drawing;
 
-class Program
+
+//Load the input Word document.
+using (WordDocument document = new WordDocument(@"Data/Input.docx", FormatType.Docx))
 {
-    static void Main(string[] args)
-    {
-        //Load the input Word document.
-        using (WordDocument document = new WordDocument(@"Data/Input.docx", FormatType.Docx))
-        {
-            //Access the list style in a Word document.
-            ListStyle style = document.ListStyles[0];
-            WListLevel levelOne = style.Levels[0];
-            //Define the character and pattern for level 1.
-            levelOne.PatternType = ListPatternType.Bullet;
-            levelOne.BulletCharacter = "\u0076";
-            levelOne.CharacterFormat.FontName = "Wingdings";
-            levelOne.CharacterFormat.TextColor = Color.Red;
-            //Save the Word document.
-            document.Save(@"Output/Sample.docx", FormatType.Docx);
-        }
-    }
+    //Access the list style in a Word document.
+    ListStyle style = document.ListStyles[0];
+    WListLevel levelOne = style.Levels[0];
+    //Define the character and pattern for level 1.
+    levelOne.PatternType = ListPatternType.Bullet;
+    levelOne.BulletCharacter = "\u0076";
+    levelOne.CharacterFormat.FontName = "Wingdings";
+    levelOne.CharacterFormat.TextColor = Color.Red;
+    //Save the Word document.
+    document.Save(@"Output/Sample.docx", FormatType.Docx);
 }
 
