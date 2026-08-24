ce1b17e46524ec9f4a294ca13f3966e68a0ef290
SushmaSF4796 <sushma.princeedwin@syncfusion.com>
2026-08-24 15:16:51 +0530

    Added changes

---
+++ Paragraphs/Convert-bullet-to-numbered-list/.NET/Convert-bullet-to-numbered-list/Program.cs
@@ -0,0 +1,16 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+
+string inputFile = @"../../../Input.docx";
+string outputFile = @"../../../Output.docx";
+
+WordDocument wordDocument = new WordDocument(inputFile, FormatType.Docx);
+// Replace {list-id} with the bullet list's id you want to target.
+List<Entity> bullets = wordDocument.FindAllItemsByProperty(EntityType.Paragraph,"ListFormat.ListType","Bulleted");
+
+foreach (Entity entity in bullets)
+    (entity as WParagraph).ListFormat.ApplyDefNumberedStyle();
+
+wordDocument.Save(outputFile);
+wordDocument.Close();
+
