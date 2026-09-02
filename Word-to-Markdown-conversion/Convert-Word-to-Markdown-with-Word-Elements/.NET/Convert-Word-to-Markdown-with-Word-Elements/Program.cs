ad8496dd3b9f1640462146ff3d6c69b59cd54b8b
ATCHAYASEKAR <atchaya.sekar@syncfusion.com>
2026-09-02 17:12:02 +0530

    Added Word to Markdown Conversion with Preserved Word Elements Sample

---
+++ Word-to-Markdown-conversion/Convert-Word-to-Markdown-with-Word-Elements/.NET/Convert-Word-to-Markdown-with-Word-Elements/Program.cs
@@ -0,0 +1,23 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIORenderer;
+using System.IO;
+
+namespace Convert_Word_to_Markdown_with_Word_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Open an existing Word document.
+            using (WordDocument document = new WordDocument(Path.GetFullPath(@"Data/Input.docx")))
+            {
+                // Initialize the DocIORenderer to preserve Word elements as images.
+                using DocIORenderer docIORenderer = new DocIORenderer();
+                //Save the document as a Markdown file.
+                document.Save(Path.GetFullPath(@"Output/Output.md"));
+            }
+        }
+    }
+}
+
