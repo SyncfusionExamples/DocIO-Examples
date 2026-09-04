8ccb547371969f2666222257ee8499a2f19db490
ATCHAYASEKAR <atchaya.sekar@syncfusion.com>
2026-09-04 15:45:33 +0530

    Added the sample for embedding font support in word

---
+++ Word-document/Embedding-Fonts/.NET/Embedding-Fonts/Embedding-Fonts/Program.cs
@@ -0,0 +1,24 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIORenderer;
+using System.IO;
+
+namespace Embedding_Fonts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WordDocument document = new WordDocument(Path.GetFullPath(@"Data/Input.docx")))
+            {
+                //Initialize the DocIORenderer.
+                using DocIORenderer renderer = new DocIORenderer();
+                //Enable the flag to embed complete TrueType/OpenType fonts used in the document.
+                document.SaveOptions.EmbedFonts = true;
+                //Save the Word document
+                document.Save(Path.GetFullPath(@"Output/Output.md"));
+                document.Close();
+            }
+        }
+    }
+}
